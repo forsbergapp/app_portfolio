06bf8e4d47207a7400e4a31a11ee9758d93892c1
fapwork <fapwork@proton.me>
2022-02-12 00:49:12 +0100

    subdomain management (removed vhost) and renaming of parameters, email external template, email logging fix, async buggfix

--- service/db/api/user_account_like/user_account_like.service.js
+++ service/db/api/user_account_like/user_account_like.service.js
@@ -2,7 +2,7 @@ const {pool, oracledb, oracle_options} = require ("../../config/database");
 
 module.exports = {
 	likeUser: (id, id_like, callBack) => {
-		if (process.env.SERVER_DB_USE == 1) {
+		if (process.env.SERVICE_DB_USE == 1) {
 			pool.query(
 			`INSERT INTO user_account_like(
 							user_account_id, user_account_id_like, date_created)
@@ -18,7 +18,7 @@ module.exports = {
 					return callBack(null, results);
 				}	
 			);
-		}else if (process.env.SERVER_DB_USE==2){
+		}else if (process.env.SERVICE_DB_USE==2){
 			async function execute_sql(err, result){
 				try{
 				const pool2 = await oracledb.getConnection();
@@ -49,7 +49,7 @@ module.exports = {
 		}
 	},
 	unlikeUser: (id, id_unlike, callBack) => {
-		if (process.env.SERVER_DB_USE == 1) {
+		if (process.env.SERVICE_DB_USE == 1) {
 			pool.query(
 			`DELETE FROM user_account_like
 				WHERE  user_account_id = ?
@@ -65,7 +65,7 @@ module.exports = {
 					return callBack(null, results);
 				}	
 			);
-		}else if (process.env.SERVER_DB_USE==2){
+		}else if (process.env.SERVICE_DB_USE==2){
 			async function execute_sql(err, result){
 				try{
 				const pool2 = await oracledb.getConnection();
