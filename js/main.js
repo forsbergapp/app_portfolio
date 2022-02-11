e8dcb75f9cebafba1e482f53890ce04c9cc71396
fapwork <fapwork@proton.me>
2022-02-11 01:06:00 +0100

    reading apps and parameters from database, adjustments parameter tables

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 var global_module_type;
 var global_app_id = 0;
-var global_host;
-var global_auth_token_url;
+var global_service_auth_token_url;
+var global_service_geolocation;
+var global_service_gps_ip;
+var global_rest_url_base    = 'https://' + location.hostname + '/service/db/api/';
+var global_rest_app;
+var global_rest_app_parameter = 'app_parameter/';
+var global_rest_app_log;
 var global_rest_client_id;
 var global_rest_client_secret;
 var global_rest_dt;
-var global_service_geolocation;
-var global_service_gps_ip;
-var global_user_gps_place;
-var global_rest_url_base    = 'https://' + location.hostname + '/service/db/api/';
-var global_rest_app_log;
-var global_rest_app_globals = global_rest_url_base + 'app/mainglobals';
 
 function toggle_switch(){
     if(document.getElementById('toggle_checkbox').checked)
@@ -39,7 +38,7 @@ function app_log(app_module, app_module_type, app_module_request, app_module_res
 				 '"user_gps_longitude": "' + user_gps_longitude + '"' +
 				'}';
 
-	fetch(global_rest_app_log,
+	fetch(global_rest_url_base + global_rest_app_log,
 		{
 		method: 'POST',
 		headers: {
@@ -65,7 +64,7 @@ function app_log(app_module, app_module_type, app_module_request, app_module_res
 function get_gps_from_ip(){
     var status;
     var json;
-	fetch(global_service_gps_ip + '?app_id=' + global_app_id,
+	fetch(global_service_geolocation + global_service_gps_ip + '?app_id=' + global_app_id,
         {method: 'GET',
         headers: {
 			'Content-Type': 'application/json',
@@ -79,13 +78,15 @@ function get_gps_from_ip(){
         .then(function(result) {
             if (status==200){
                 json = JSON.parse(result);
-                global_user_gps_latitude  = json.geoplugin_latitude;
-                global_user_gps_longitude = json.geoplugin_longitude;
-                global_user_gps_place     = json.geoplugin_city + ', ' + 
-                                            json.geoplugin_regionName + ', ' + 
-                                            json.geoplugin_countryName;
-                app_log('INIT', global_module_type, location.hostname, global_user_gps_place, '', 
-                        global_user_gps_latitude, global_user_gps_longitude); 
+                app_log('INIT', 
+                        global_module_type, 
+                        location.hostname, 
+                        json.geoplugin_city + ', ' + 
+                        json.geoplugin_regionName + ', ' + 
+                        json.geoplugin_countryName, 
+                        '', 
+                        json.geoplugin_latitude, 
+                        json.geoplugin_longitude); 
             }
             else
                 alert('Error: get_gps_from_ip: ' + result);
@@ -93,11 +94,44 @@ function get_gps_from_ip(){
 
 	return null;
 }
+function get_apps() {
+	var status;
+    var json;
+    fetch(global_rest_url_base + global_rest_app,
+    {method: 'GET',
+     headers: {
+			'Authorization': 'Bearer ' + global_rest_dt
+        }
+    })
+      .then(function(response) {
+            status = response.status;
+            return response.text();
+      })
+      .then(function(result) {
+          if (status == 200){
+            json = JSON.parse(result);
+            let html='';
+            for (var i = 0; i < json.data.length; i++) {
+                if (json.data[i].id !==0){
+                    html +=
+                    `<div class='app_link' onclick='window.open("${json.data[i].url}");'>
+                            <div class='app_logo_div'><img class='app_logo' src='${json.data[i].logo}' /></div>
+                            <div class='app_name'>${json.data[i].app_name}</div>
+                    </div>`;
+                }
+            }
+            document.getElementById('apps').innerHTML = html;
+          }
+          else
+            alert('Error: get_apps: ' + result);
+        });
+	return null;
+}
 
 function get_token() {
 	var status;
     var json;
-    fetch(global_auth_token_url + '?app_id=0&app_user_id=',
+    fetch(global_service_auth_token_url + '?app_id=' + global_app_id + '&app_user_id=',
     {method: 'POST',
      headers: {
         'Authorization': 'Basic ' + btoa(global_rest_client_id + ':' + global_rest_client_secret)
@@ -114,6 +148,7 @@ function get_token() {
                 global_rest_at = json.token_at;
                 global_rest_dt = json.token_dt;
                 get_gps_from_ip();
+                get_apps();
             }
           else
             alert('Error: get_token: ' + result);
@@ -121,10 +156,10 @@ function get_token() {
 	return null;
 }
 
-function get_globals() {
+function get_parameters() {
     var status;
     var json;
-    fetch(global_rest_app_globals,
+    fetch(global_rest_url_base + global_rest_app_parameter + global_app_id,
       {method: 'GET'})
         .then(function(response) {
             status = response.status;
@@ -133,41 +168,36 @@ function get_globals() {
         .then(function(result) {
             if (status==200){
                 json = JSON.parse(result);
-                if (json.success === 1){    
-                    global_host	                = json.host;
-                    global_auth_token_url       = 'https://' + global_host + json.auth_token_url;
-                    global_rest_client_id	    = json.rest_client_id;
-                    global_rest_client_secret   = json.rest_client_secret;
-                    global_service_geolocation 	= 'https://' + global_host + json.service_geolocation;
-                    global_service_gps_ip    	= global_service_geolocation + json.service_gps_ip;
-                    global_rest_app_log         = global_rest_url_base + json.rest_app_log;
-
-                    document.getElementById('app_email').href='mailto:' + json.email;
-                    document.getElementById('app_email').innerHTML=json.email;
-                    get_token();
+                for (var i = 0; i < json.data.length; i++) {
+                    if (json.data[i].parameter_name=='APP_REST_CLIENT_ID')
+                        global_rest_client_id = json.data[i].parameter_value;
+                    if (json.data[i].parameter_name=='APP_REST_CLIENT_SECRET')
+                        global_rest_client_secret = json.data[i].parameter_value;
+                    if (json.data[i].parameter_name=='SERVICE_AUTH_TOKEN_URL')
+                        global_service_auth_token_url = 'https://' + location.hostname + json.data[i].parameter_value;
+                    if (json.data[i].parameter_name=='REST_APP')
+                        global_rest_app = json.data[i].parameter_value;
+                    if (json.data[i].parameter_name=='REST_APP_LOG')
+                        global_rest_app_log = json.data[i].parameter_value;
+                    if (json.data[i].parameter_name=='COPYRIGHT')
+                        document.getElementById('copyright').innerHTML=json.data[i].parameter_value;
+                    if (json.data[i].parameter_name=='EMAIL'){
+                        document.getElementById('app_email').href='mailto:' + json.data[i].parameter_value;
+                        document.getElementById('app_email').innerHTML=json.data[i].parameter_value;    
+                    }
+                    if (json.data[i].parameter_name=='SERVICE_GEOLOCATION')
+                        global_service_geolocation = 'https://' + location.hostname + json.data[i].parameter_value;
+                    if (json.data[i].parameter_name=='SERVICE_GPS_IP')
+                        global_service_gps_ip = json.data[i].parameter_value;
                 }
+                get_token();
             }
             else
-                alert('Error: get_globals: ' + result);
+                alert('Error: get_parameters: ' + result);
         });
     return null;
 }
 
-function open_app(app_no){
-    switch (app_no){
-        case 1:{
-            window.open ("https://timetables." + global_host);	
-            break;
-        }
-        case 2:{
-            window.open ("https://propertymanagement." + global_host);
-            break;
-        }
-        default:
-            break;
-    }
-    return null;
-}
 function countdown(remaining) {
     if(remaining <= 0)
         location.reload(true);
@@ -177,11 +207,11 @@ function countdown(remaining) {
 function init_maintenance(){
     global_module_type='MAINTENANCE';
     countdown(60);
-    get_globals();
+    get_parameters();
 }
 
 function init(){
     global_module_type='INIT';
     document.getElementById("toggle_checkbox").checked = true;
-    get_globals();
+    get_parameters();
 }
