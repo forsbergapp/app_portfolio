1331b40ab217078707f8b4af2c0c1bbea06fe176
fapwork <fapwork@proton.me>
2022-02-23 19:18:35 +0100

    added pwa service worker that caches files, reads pwa parameters from db, favicon.ico fix

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(
         styleSrc: ["'self'", "'unsafe-inline'", 'fonts.googleapis.com', 'use.fontawesome.com', 'api.mapbox.com'],
         fontSrc: ["self", 'fonts.gstatic.com', 'use.fontawesome.com'],
         imgSrc: ["*", 'data:', 'blob:'],
-        connectSrc: ["'self'", 'api.mapbox.com', 'events.mapbox.com', 'www.facebook.com', 'graph.facebook.com'],
+        connectSrc: ["*"],
         childSrc: ["'self'", 'blob:'],
         objectSrc: ["'self'", 'data:'],
         frameSrc: ["'self'", 'data:', 'accounts.google.com', 'www.facebook.com'],
@@ -112,34 +112,104 @@ app.use('/app1/css',express.static(__dirname + '/app1/css'));
 app.use('/app1/js',express.static(__dirname + '/app1/js'));
 app.use('/app1/info',express.static(__dirname + '/app1/info'));
 app.use('/app1/images',express.static(__dirname + '/app1/images'));
-
+//app 1 pwa service worker, placed in root
+app.get("/sw.js",function (req, res,next) {
+  if (req.headers.host.substring(0,req.headers.host.indexOf('.')) == 'app1') {
+      res.type('application/javascript');
+      res.setHeader('Service-Worker-Allowed', '/')
+      res.status(200);
+      return res.sendFile(__dirname + "/app1/sw.js");
+  }
+  else
+    next();
+});
 //app 1 progressive webapp menifest
-app.get("/app1/manifest.json",function (req, res,next) {
-    res.type('text/plain')
-    res.send(`{
-                "short_name": "${process.env.APP1_PWA_SHORT_NAME}",
-                "name": "${process.env.APP1_PWA_NAME}",
-                "description": "${process.env.APP1_PWA_DESCRIPTION}",
-                "start_url": "${process.env.APP1_PWA_START_URL}",
-                "display": "${process.env.APP1_PWA_DISPLAY}",
-                "background_color": "${process.env.APP1_PWA_BACKGROUND_COLOR}",
-                "theme_color": "${process.env.APP1_PWA_THEME_COLOR}",
-                "orientation": "${process.env.APP1_PWA_ORIENTATION}",
-                "icons": [
-                  {
-                    "src": "${process.env.APP1_PWA_ICONS1_SRC}",
-                    "type": "${process.env.APP1_PWA_ICONS1_TYPE}",
-                    "sizes": "${process.env.APP1_PWA_ICONS1_SIZES}"
-                  },
-                  {
-                    "src": "${process.env.APP1_PWA_ICONS2_SRC}",
-                    "type": "${process.env.APP1_PWA_ICONS2_TYPE}",
-                    "sizes": "${process.env.APP1_PWA_ICONS2_SIZES}"
-                  }
-                ],
-                "scope": "${process.env.APP1_PWA_SCOPE}"
-              }`
-      );
+app.get("/app1/manifest.json",function (req, res, next) {
+    const { getParameters } = require ("./service/db/api/app_parameter/app_parameter.service");
+    getParameters(process.env.APP1_ID,(err, results) =>{
+      if (err) {
+        console.log(err);
+        return res.status(500).send({
+          success: 0,
+          data: err
+        });
+      }
+      else {
+        let json = JSON.parse(JSON.stringify(results));
+        let pwa_short_name;
+        let pwa_name;
+        let pwa_description;
+        let pwa_start_url;
+        let pwa_display;
+        let pwa_background_color;
+        let pwa_theme_color;
+        let pwa_orientation;
+        let pwa_icons1_src;
+        let pwa_icons1_type;
+        let pwa_icons1_sizes;
+        let pwa_icons2_src;
+        let pwa_icons2_type;
+        let pwa_icons2_sizes;
+        let pwa_scope;
+        for (var i = 0; i < json.length; i++) {
+          if (json[i].parameter_name=='PWA_SHORT_NAME')
+            pwa_short_name = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_NAME')
+            pwa_name = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_DESCRIPTION')
+            pwa_description = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_START_URL')
+            pwa_start_url = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_DISPLAY')
+            pwa_display = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_BACKGROUND_COLOR')
+            pwa_background_color = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_THEME_COLOR')
+            pwa_theme_color = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_ORIENTATION')
+            pwa_orientation = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_ICONS1_SRC')
+            pwa_icons1_src = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_ICONS1_TYPE')
+            pwa_icons1_type = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_ICONS1_SIZES')
+            pwa_icons1_sizes = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_ICONS2_SRC')
+            pwa_icons2_src = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_ICONS2_TYPE')
+            pwa_icons2_type = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_ICONS2_SIZES')
+            pwa_icons2_sizes = json[i].parameter_value;
+          if (json[i].parameter_name=='PWA_SCOPE')
+            pwa_scope = json[i].parameter_value;
+        }
+        res.type('text/plain');
+        res.send(`{
+                    "short_name": "${pwa_short_name}",
+                    "name": "${pwa_name}",
+                    "description": "${pwa_description}",
+                    "start_url": "${pwa_start_url}",
+                    "display": "${pwa_display}",
+                    "background_color": "${pwa_background_color}",
+                    "theme_color": "${pwa_theme_color}",
+                    "orientation": "${pwa_orientation}",
+                    "icons": [
+                      {
+                        "src": "${pwa_icons1_src}",
+                        "type": "${pwa_icons1_type}",
+                        "sizes": "${pwa_icons1_sizes}"
+                      },
+                      {
+                        "src": "${pwa_icons2_src}",
+                        "type": "${pwa_icons2_type}",
+                        "sizes": "${pwa_icons2_sizes}"
+                      }
+                    ],
+                    "scope": "${pwa_scope}"
+                  }`
+                );
+      }
+    });
 });
 //app 1 show profile directly from url
 app.get('/:user', function(req, res,next) {
@@ -148,6 +218,8 @@ app.get('/:user', function(req, res,next) {
       req.params.user !== '' && 
       req.params.user!=='robots.txt' &&
       req.params.user!=='manifest.json' &&
+      req.params.user!=='favicon.ico' &&
+      req.params.user!=='sw.js' &&
       req.params.user!=='css' &&
       req.params.user!=='images' &&
       req.params.user!=='js' &&
@@ -163,7 +235,7 @@ app.get('/:user', function(req, res,next) {
             // return app 1
             const { getApp } = require("./app1/app");
             res.setHeader('Content-Type', 'text/html');
-            const app = getApp()
+            const app = getApp(process.env.APP1_ID)
             .then(function(app_result){
               return res.send(app_result);
             });
@@ -188,6 +260,31 @@ app.use('/app0/js',express.static(__dirname + '/app0/js'));
 app.get('/robots.txt', function (req, res) {
   res.type('text/plain');
   res.send("User-agent: *\nDisallow: /");
+});
+app.get('/favicon.ico', function (req, res) {
+  switch (req.headers.host.substring(0,req.headers.host.indexOf('.'))){
+    case '':
+    case 'www':{
+      res.sendFile(__dirname + "/app0/images/favicon.ico");
+      break;
+    }
+    case 'app0':{
+      res.sendFile(__dirname + "/app0/images/favicon.ico");
+      break;
+    }
+    case 'app1':{
+      res.sendFile(__dirname + "/app1/images/favicon.ico");
+      break;
+    }
+    case 'app2':{
+      res.sendFile(__dirname + "/app2/images/favicon.ico");
+      break;
+    }
+    default:{
+      res.sendFile(__dirname + "/app0/images/favicon.ico");
+      break; 
+    }
+  }
 });
 
 //config root url
@@ -214,7 +311,7 @@ app.get('/',function (req, res) {
       //app 1 generates startup html with some data from database
       const { getApp } = require("./app1/app");
       res.setHeader('Content-Type', 'text/html');
-      const app = getApp(1)
+      const app = getApp(process.env.APP1_ID)
       .then(function(app_result){
         return res.send(app_result);
       });
