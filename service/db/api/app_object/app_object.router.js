789159a50d67d963be940adc9abed1436c4f24c4
fapwork <fapwork@proton.me>
2022-03-07 13:43:42 +0100

    logg fixes, more logs in user_account, added router log using SERVICE_LOG_ENABLE_ROUTER

--- service/db/api/app_object/app_object.router.js
+++ service/db/api/app_object/app_object.router.js
@@ -1,6 +1,10 @@
-const { getObjects } = require ("./app_object.controller");
 const router = require("express").Router();
+const { getObjects } = require ("./app_object.controller");
 const { checkDataToken } = require("../../../auth/auth.controller");
-
+const { createLogAppRI } = require("../../../log/log.service");
+router.use((req,res,next)=>{
+    createLogAppRI(req, res, req.query.id, __appfilename, __appfunction, __appline, req.body);
+    next();
+})
 router.get("/:lang_code",  checkDataToken, getObjects);
 module.exports = router;
--- service/db/api/language/locale/locale.router.js
+++ service/db/api/language/locale/locale.router.js
@@ -1,6 +1,10 @@
-const { getLocales } = require ("./locale.controller");
 const router = require("express").Router();
+const { getLocales } = require ("./locale.controller");
 const { checkDataToken } = require("../../../../auth/auth.controller");
-
+const { createLogAppRI } = require("../../../../log/log.service");
+router.use((req,res,next)=>{
+    createLogAppRI(req, res, req.query.id, __appfilename, __appfunction, __appline, req.body);
+    next();
+})
 router.get("/:lang_code",  checkDataToken, getLocales);
 module.exports = router;
