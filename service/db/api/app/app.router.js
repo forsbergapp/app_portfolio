6d14e04b97a51ed9798db3549bf8de355d2db2cf
fapwork <fapwork@proton.me>
2022-03-07 13:43:42 +0100

    logg fixes, more logs in user_account, added router log using SERVICE_LOG_ENABLE_ROUTER

--- service/db/api/app/app.router.js
+++ service/db/api/app/app.router.js
@@ -1,6 +1,10 @@
 const { getApp } = require ("./app.controller");
 const router = require("express").Router();
 const { checkDataToken } = require("../../../auth/auth.controller");
-
+const { createLogAppRI } = require("../../../log/log.service");
+router.use((req,res,next)=>{
+    createLogAppRI(req, res, req.query.id, __appfilename, __appfunction, __appline, req.body);
+    next();
+})
 router.get("/",  checkDataToken, getApp);
 module.exports = router;
