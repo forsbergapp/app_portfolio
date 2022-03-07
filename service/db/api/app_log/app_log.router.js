186740b4d7a886a8df1d9c8d097fe2d7b3638d1b
fapwork <fapwork@proton.me>
2022-03-07 13:43:42 +0100

    logg fixes, more logs in user_account, added router log using SERVICE_LOG_ENABLE_ROUTER

--- service/db/api/app_log/app_log.router.js
+++ service/db/api/app_log/app_log.router.js
@@ -1,7 +1,11 @@
-const { getLogs,createLog } = require ("./app_log.controller");
 const router = require("express").Router();
+const { getLogs,createLog } = require ("./app_log.controller");
 const { checkDataToken } = require("../../../auth/auth.controller");
-
+const { createLogAppRI } = require("../../../log/log.service");
+router.use((req,res,next)=>{
+    createLogAppRI(req, res, req.query.id, __appfilename, __appfunction, __appline, req.body);
+    next();
+})
 router.get("/",  checkDataToken, getLogs);
 router.post("/", checkDataToken, createLog);
 module.exports = router;
--- service/db/api/app_timetables_user_setting_view/app_timetables_user_setting_view.router.js
+++ service/db/api/app_timetables_user_setting_view/app_timetables_user_setting_view.router.js
@@ -1,6 +1,10 @@
-const { insertUserSettingView} = require ("./app_timetables_user_setting_view.controller");
 const router = require("express").Router();
+const { insertUserSettingView} = require ("./app_timetables_user_setting_view.controller");
 const { checkDataToken } = require("../../../auth/auth.controller");
-
+const { createLogAppRI } = require("../../../log/log.service");
+router.use((req,res,next)=>{
+    createLogAppRI(req, res, req.query.id, __appfilename, __appfunction, __appline, req.body);
+    next();
+})
 router.post("/", checkDataToken, insertUserSettingView);
 module.exports = router;
