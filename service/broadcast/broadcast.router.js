b6e51aef9d39c54cf26284706fc26e79d37cb42c
fapwork <fapwork@proton.me>
2022-03-23 02:15:46 +0100

    dashboard admin ui, app stat page, list connected users, app log with map integration, filter by app_id

--- service/broadcast/broadcast.router.js
+++ service/broadcast/broadcast.router.js
@@ -1,7 +1,8 @@
-const { getBroadcast, getConnected} = require ("./broadcast.controller");
+const { getBroadcast, getConnected, sendBroadcast} = require ("./broadcast.controller");
 const { checkAdmin} = require ("../auth/admin/admin.controller");
 const router = require("express").Router();
 
 router.get("/",getBroadcast);
-router.get("/connected",checkAdmin, getConnected);
+router.get("/connected", checkAdmin, getConnected);
+router.post("/",checkAdmin, sendBroadcast);
 module.exports = router;
