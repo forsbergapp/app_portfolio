6de1f3fd4f8ae10562f2ba728622ec670b236404
fapwork <fapwork@proton.me>
2022-04-19 23:04:51 +0200

    APB-8 Admin, page SERVER LOGS filesearch with fileselect, settings enabled log variables visible, buggfixes reading logfiles, logfiles saved with leading 0 for month and day.

--- service/log/log.controller.js
+++ service/log/log.controller.js
@@ -1,4 +1,4 @@
-const { getParameters, getLogs} = require ("./log.service");
+const { getParameters, getLogs, getFiles} = require ("./log.service");
 module.exports = {
 	getParameters: (req, res) => {
 		getParameters((err, results) =>{
@@ -17,13 +17,24 @@ module.exports = {
 	getLogs: (req, res) => {
 		getLogs(req.query, (err, results) =>{
 			if (err) {
-				return res.status(500).send({
-					success: 0,
-					data: err
-				});
+				return res.status(500).send(
+                    err
+                );
 			}
 			return res.status(200).send(
-				'[' + results + ']'
+				results
+			);
+		});
+	},
+	getFiles: (req, res) => {
+		getFiles((err, results) =>{
+			if (err) {
+				return res.status(500).send(
+                    err
+                );
+			}
+			return res.status(200).send(
+				results
 			);
 		});
 	}
