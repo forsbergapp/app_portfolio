76422043ffc1433f70d142e155172cc27fefac63
appportfolio <forsbergapp@protonmail.com>
2022-06-19 22:13:16 +0200

    APB-24: shows user accounts app in profile in app0, bugfix displaying private profile

--- service/db/api/user_account_app/user_account_app.controller.js
+++ service/db/api/user_account_app/user_account_app.controller.js
@@ -1,4 +1,4 @@
-const { createUserAccountApp} = require ("./user_account_app.service");
+const { createUserAccountApp, getUserAccountApps} = require ("./user_account_app.service");
 
 module.exports = {
 	
@@ -15,5 +15,19 @@ module.exports = {
 				items: Array(results)
 			});
 		});
-	}
+	},
+	getUserAccountApps: (req, res) => {
+		getUserAccountApps(req.query.app_id, req.params.user_account_id, (err,results) => {
+			if (err) {
+				return res.status(500).send(
+					err
+				);
+			}
+			return res.status(200).json({
+				count: results.length,
+				success: 1,
+				items: results
+			});
+		});
+	},
 }
--- service/db/api/user_account_app/user_account_app.router.js
+++ service/db/api/user_account_app/user_account_app.router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { createUserAccountApp} = require ("./user_account_app.controller");
+const { createUserAccountApp, getUserAccountApps} = require ("./user_account_app.controller");
 const { checkAccessToken } = require("../../../auth/auth.controller");
 const { createLogAppRI } = require("../../../log/log.service");
 router.use((req,res,next)=>{
@@ -7,4 +7,5 @@ router.use((req,res,next)=>{
     next();
 })
 router.post("/", checkAccessToken, createUserAccountApp);
+router.get("/:user_account_id", checkAccessToken, getUserAccountApps);
 module.exports = router;
