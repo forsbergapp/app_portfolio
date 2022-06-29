9fa9414b899769899642729e5c51d0d142cb72c9
fapwork <fapwork@proton.me>
2022-06-29 16:37:00 +0200

    APB-27	fixed URIError: Failed to decode ..., added messages 20309 and 20310 and controls input text in all text input fields to avoid TypeError: Cannot read properties of undefined (reading 'query')... so no server path will be exposed, doublequote and backslash not allowed, bugfix app1 url parameter

--- scripts/ar.sql
+++ scripts/ar.sql
@@ -997,6 +997,8 @@ INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20
 INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20306','الرمز غير صالح');
 INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20307','الملف المطبوع سري');
 INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20308','حجم الملف كبير جدًا');
+INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20309','النص غير صالح');
+INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20310','نص طويل جدا');
 INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20400','لم يتم العثور على السجل');
 INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20401','فشل تحديث إعدادات المستخدم');
 INSERT INTO message_translation (language_id, message_code, text) VALUES (25,'20402','فشل تحديث المستخدم');
