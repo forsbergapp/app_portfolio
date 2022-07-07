c76aaad2c61be8858cd20272e7eb03adb5f4d1e1
appportfolio <forsbergapp@protonmail.com>
2022-07-07 23:37:01 +0200

    APB-103 showing profile in url for app0, consolidated and simplified app request in app.get, maintenance managed by forms service

--- apps/app0/client.js
+++ apps/app0/client.js
@@ -1,46 +1,61 @@
 const { read_app_files, get_module_with_init } = require("../");
 module.exports = {
-    getApp:(app_id, params, gps_lat, gps_long, gps_place) => {
+    getApp:(app_id, username, gps_lat, gps_long, gps_place) => {
         return new Promise(function (resolve, reject){
-            const files = [
-                ['APP', __dirname + '/src/index.html'],
-                ['<AppCommonHead/>', __dirname + '/../common/src/head.html'],
-                ['<AppCommonBody/>', __dirname + '/../common/src/body.html'],
-                ['<AppCommonProfileDetail/>', __dirname + '/../common/src/profile_detail.html'], //Profile tag in common body
-
-                ['<AppHead/>', __dirname + '/src/head.html'],
-                ['<AppUserAccount/>', __dirname + '/src/user_account.html'],
-                ['<AppToggle/>', __dirname + '/src/toogle.html'],
-                ['<AppBackground/>', __dirname + '/src/background.html'],
-                ['<AppDialogues/>', __dirname + '/src/dialogues.html'],
-                ['<AppProfileInfo/>', __dirname + '/src/profile_info.html'],   /*Profile tag in common body*/
-                ['<AppWindowInfo/>', __dirname + '/src/window_info.html']
-              ];
-              read_app_files(app_id, files, (err, app)=>{
-                if (err)
-                    reject(err);
-                else{
-                    //Profile tag not used in common body
-                    app = app.replace(
-                        '<AppProfileTop/>',
-                        '');   
-                    get_module_with_init(app_id, 
-                                         'app_exception',
-                                         null,
-                                         true,
-                                         null,
-                                         gps_lat,
-                                         gps_long,
-                                         gps_place,
-                                         app, (err, app_init) =>{
-                        if (err)
-                            reject(err);
-                        else{
-                            resolve(app_init);
-                        }
-                    })
-                }
-            })           
+            function main(app_id){
+                const files = [
+                    ['APP', __dirname + '/src/index.html'],
+                    ['<AppCommonHead/>', __dirname + '/../common/src/head.html'],
+                    ['<AppCommonBody/>', __dirname + '/../common/src/body.html'],
+                    ['<AppCommonProfileDetail/>', __dirname + '/../common/src/profile_detail.html'], //Profile tag in common body
+    
+                    ['<AppHead/>', __dirname + '/src/head.html'],
+                    ['<AppUserAccount/>', __dirname + '/src/user_account.html'],
+                    ['<AppToggle/>', __dirname + '/src/toogle.html'],
+                    ['<AppBackground/>', __dirname + '/src/background.html'],
+                    ['<AppDialogues/>', __dirname + '/src/dialogues.html'],
+                    ['<AppProfileInfo/>', __dirname + '/src/profile_info.html'],   /*Profile tag in common body*/
+                    ['<AppWindowInfo/>', __dirname + '/src/window_info.html']
+                  ];
+                  read_app_files(app_id, files, (err, app)=>{
+                    if (err)
+                        reject(err);
+                    else{
+                        //Profile tag not used in common body
+                        app = app.replace(
+                            '<AppProfileTop/>',
+                            '');   
+                        get_module_with_init(app_id, 
+                                             'app_exception',
+                                             null,
+                                             true,
+                                             null,
+                                             gps_lat,
+                                             gps_long,
+                                             gps_place,
+                                             app, (err, app_init) =>{
+                            if (err)
+                                reject(err);
+                            else{
+                                resolve(app_init);
+                            }
+                        })
+                    }
+                }) 
+            }
+            if (username!=null){
+                const {getProfileUser} = require("../../service/db/api/user_account/user_account.service");
+                getProfileUser(app_id, null, username, null, (err,result)=>{
+                    if (result)
+                        main(app_id);
+                    else{
+                        //return 0 meaning redirect to /
+                        resolve (0);
+                    }
+                })
+            }
+            else
+                main(app_id);          
         })
     }
 }
--- apps/app2/server.js
+++ apps/app2/server.js
@@ -3,50 +3,22 @@ const APP2_ID = 2;
 app.use('/app2',express.static(__dirname + '/apps/app2'));
 
 app.get('/favicon.ico', function (req, res, next) {
-    switch (req.headers.host.substring(0,req.headers.host.indexOf('.'))){
-      case 'app2':{
-        res.sendFile(__dirname + "/apps/app2/images/favicon.ico");
-        break;
-      }
-      default:{
-        next();
-      }
-    }
-  });
+  if (req.headers.host.substring(0,req.headers.host.indexOf('.')) == 'app2')
+    res.sendFile(__dirname + "/apps/app2/images/favicon.ico");
+  else
+    next();  
+});
   
-  app.get('/',function (req, res, next) {
+app.get('/',function (req, res, next) {
+  if (req.headers.host.substring(0,req.headers.host.indexOf('.')) == 'app2'){
     //redirect from http to https
     if (req.protocol=='http')
       return res.redirect('https://' + req.headers.host);
-    //redirect naked domain to www
-    if (((req.headers.host.split('.').length - 1) == 1) &&
-        req.headers.host.indexOf('localhost')==-1)
-      return res.redirect('https://www.' + req.headers.host);
-    switch (req.headers.host.substring(0,req.headers.host.indexOf('.'))){
-      case 'app2':{
-        const { getParameter} = require ("./service/db/api/app_parameter/app_parameter.service");
-        getParameter(process.env.MAIN_APP_ID,'SERVER_MAINTENANCE', (err, db_SERVER_MAINTENANCE)=>{
-          if (err)
-            createLogAppSE(APP2_ID, __appfilename, __appfunction, __appline, err);      
-          else{
-              if (db_SERVER_MAINTENANCE==1){
-                const { getMaintenance} = require("./service/forms/forms.controller");
-                getMaintenance(req, res, APP2_ID,(err, app_result)=>{
-                    return res.send(app_result);
-                })
-              }
-              else{
-                const { getForm} = require("./service/forms/forms.controller");
-                getForm(req, res, APP2_ID, null,(err, app_result)=>{
-                    return res.send(app_result);
-                })
-              }
-          }
-        })
-        break;
-      }
-      default:{
-        next();
-      }
-    }
-  });
+    const { getForm} = require("./service/forms/forms.controller");
+    getForm(req, res, APP2_ID, null,(err, app_result)=>{
+        return res.send(app_result);
+    })
+  }
+  else
+    next();
+});
