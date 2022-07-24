89d73477a1818e92cb4216dfb803e2e8ffc4f4e0
appportfolio <app.portfolio@protonmail.com>
2022-07-24 02:05:57 +0200

    APB-173 consolidated database sql call in all *.service.js files to one function in database.js

--- service/db/api/app1_user_setting_like/app1_user_setting_like.service.js
+++ service/db/api/app1_user_setting_like/app1_user_setting_like.service.js
@@ -1,116 +1,56 @@
-const {oracledb, get_pool} = require ("../../config/database");
-const { createLogAppSE } = require("../../../../service/log/log.controller");
+const {execute_db_sql} = require ("../../config/database");
 module.exports = {
 	likeUserSetting: (app_id, id, id_like, callBack) => {
+		let sql;
+		let parameters;
 		if (process.env.SERVICE_DB_USE == 1) {
-			get_pool(app_id).query(
-			`INSERT INTO ${process.env.SERVICE_DB_DB1_NAME}.app1_user_setting_like(
-							user_account_id, app1_user_setting_id, date_created)
-				VALUES(?,?, SYSDATE()) `,
-				[
-				id,
-				id_like
-				],
-				(error, results, fields) => {
-					if (error){
-						createLogAppSE(app_id, __appfilename, __appfunction, __appline, error);
-						return callBack(error);
-					}
-					return callBack(null, results);
-				}	
-			);
+			sql = `INSERT INTO ${process.env.SERVICE_DB_DB1_NAME}.app1_user_setting_like(
+								user_account_id, app1_user_setting_id, date_created)
+					VALUES(?,?, SYSDATE()) `;
+			parameters = [id,
+						  id_like
+						 ];
 		}else if (process.env.SERVICE_DB_USE==2){
-			async function execute_sql(err, result){
-				let pool2;
-				try{
-				pool2 = await oracledb.getConnection(get_pool(app_id));
-				const result = await pool2.execute(
-					`INSERT INTO ${process.env.SERVICE_DB_DB2_NAME}.app1_user_setting_like(
-									user_account_id, app1_user_setting_id, date_created)
-						VALUES(:user_account_id,:app1_user_setting_id, SYSDATE) `,
-					{
-						user_account_id: id,
-						app1_user_setting_id: id_like
-					},
-					(err,result) => {
-						if (err) {
-							createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-							return callBack(err);
-						}
-						else{
-							return callBack(null, result);
-						}
-					});
-				}catch (err) {
-					createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-					return callBack(err.message);
-				} finally {
-					if (pool2) {
-						try {
-							await pool2.close(); 
-						} catch (err) {
-							createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-						}
-					}
-				}
-			}
-			execute_sql();
+			sql = `INSERT INTO ${process.env.SERVICE_DB_DB2_NAME}.app1_user_setting_like(
+								user_account_id, app1_user_setting_id, date_created)
+					VALUES(:user_account_id,:app1_user_setting_id, SYSDATE) `;
+			parameters = {
+							user_account_id: id,
+							app1_user_setting_id: id_like
+						};
 		}
+		execute_db_sql(app_id, app_id, sql, parameters, null, (err, result)=>{
+			if (err)
+				return callBack(err, null);
+			else
+				return callBack(null, result);
+		});
 	},
 	unlikeUserSetting: (app_id, id, id_unlike, callBack) => {
+		let sql;
+		let parameters;
 		if (process.env.SERVICE_DB_USE == 1) {
-			get_pool(app_id).query(
-			`DELETE FROM ${process.env.SERVICE_DB_DB1_NAME}.app1_user_setting_like
-			  WHERE user_account_id = ?
-				AND app1_user_setting_id = ? `,
-				[
-				id,
-				id_unlike
-				],
-				(error, results, fields) => {
-					if (error){
-						createLogAppSE(app_id, __appfilename, __appfunction, __appline, error);
-						return callBack(error);
-					}
-					return callBack(null, results);
-				}	
-			);
+			sql = `DELETE FROM ${process.env.SERVICE_DB_DB1_NAME}.app1_user_setting_like
+					WHERE user_account_id = ?
+					AND app1_user_setting_id = ? `;
+			parameters = [
+							id,
+							id_unlike
+							];
 		}else if (process.env.SERVICE_DB_USE==2){
-			async function execute_sql(err, result){
-				let pool2;
-				try{
-				pool2 = await oracledb.getConnection(get_pool(app_id));
-				const result = await pool2.execute(
-					`DELETE FROM ${process.env.SERVICE_DB_DB2_NAME}.app1_user_setting_like
-					  WHERE user_account_id = :user_account_id
-						AND app1_user_setting_id = :app1_user_setting_id `,
-					{
-						user_account_id: id,
-						app1_user_setting_id: id_unlike
-					},
-					(err,result) => {
-						if (err) {
-							createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-							return callBack(err);
-						}
-						else{
-							return callBack(null, result);
-						}
-					});
-				}catch (err) {
-					createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-					return callBack(err.message);
-				} finally {
-					if (pool2) {
-						try {
-							await pool2.close(); 
-						} catch (err) {
-							createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-						}
-					}
-				}
-			}
-			execute_sql();
+			sql = `DELETE FROM ${process.env.SERVICE_DB_DB2_NAME}.app1_user_setting_like
+					WHERE user_account_id = :user_account_id
+					AND app1_user_setting_id = :app1_user_setting_id `;
+			parameters = {
+							user_account_id: id,
+							app1_user_setting_id: id_unlike
+						 };
 		}
+		execute_db_sql(app_id, app_id, sql, parameters, null, (err, result)=>{
+			if (err)
+				return callBack(err, null);
+			else
+				return callBack(null, result);
+		});
 	},
 };
--- service/db/api/message_translation/message_translation.service.js
+++ service/db/api/message_translation/message_translation.service.js
@@ -1,117 +1,87 @@
-const {oracledb, get_pool} = require ("../../config/database");
-const { createLogAppSE } = require("../../../../service/log/log.controller");
+const {execute_db_sql} = require ("../../config/database");
 module.exports = {
 	getMessage: (code, app_id, lang_code, callBack) => {
+		let sql;
+    	let parameters;
 		if (process.env.SERVICE_DB_USE==1){
-			get_pool(app_id).query(
-				`SELECT m.code,
-						m.message_level_id,
-						m.message_type_id,
-						mt.language_id,
-						l.lang_code,
-						mt.text,
-						am.app_id
-				   FROM ${process.env.SERVICE_DB_DB1_NAME}.message m,
-						${process.env.SERVICE_DB_DB1_NAME}.message_translation mt,
-						${process.env.SERVICE_DB_DB1_NAME}.app_message am,
-						${process.env.SERVICE_DB_DB1_NAME}.language l
-				WHERE mt.language_id = l.id
-				  AND mt.message_code = m.code
-				  AND am.message_code = m.code
-				  AND am.app_id = ?
-				  AND m.code = ?
-				  AND (l.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-						OR (l.lang_code = 'en'
-							AND NOT EXISTS(SELECT NULL
-											FROM  ${process.env.SERVICE_DB_DB1_NAME}.message_translation mt1,
-												  ${process.env.SERVICE_DB_DB1_NAME}.language l1
-											WHERE mt1.message_code = mt.message_code
-											  AND l1.id = mt1.language_id
-											  AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-										)
-							)
-						)`,
-				[app_id,
-				 code,
-				 lang_code,
-				 lang_code,
-				 lang_code,
-				 lang_code,
-				 lang_code,
-				 lang_code
-				 ],
-				(error, results, fields) => {
-					if (error){
-						createLogAppSE(app_id, __appfilename, __appfunction, __appline, error);
-						return callBack(error);
-					}
-					return callBack(null, results[0]);
-				}
-			);
+			sql = `SELECT 	m.code,
+							m.message_level_id,
+							m.message_type_id,
+							mt.language_id,
+							l.lang_code,
+							mt.text,
+							am.app_id
+					FROM ${process.env.SERVICE_DB_DB1_NAME}.message m,
+							${process.env.SERVICE_DB_DB1_NAME}.message_translation mt,
+							${process.env.SERVICE_DB_DB1_NAME}.app_message am,
+							${process.env.SERVICE_DB_DB1_NAME}.language l
+					WHERE mt.language_id = l.id
+					AND mt.message_code = m.code
+					AND am.message_code = m.code
+					AND am.app_id = ?
+					AND m.code = ?
+					AND (l.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+							OR (l.lang_code = 'en'
+								AND NOT EXISTS(SELECT NULL
+												FROM  ${process.env.SERVICE_DB_DB1_NAME}.message_translation mt1,
+													${process.env.SERVICE_DB_DB1_NAME}.language l1
+												WHERE mt1.message_code = mt.message_code
+												AND l1.id = mt1.language_id
+												AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+											)
+								)
+							)`;
+			parameters = [	app_id,
+							code,
+							lang_code,
+							lang_code,
+							lang_code,
+							lang_code,
+							lang_code,
+							lang_code
+							];
 		}
 		else if (process.env.SERVICE_DB_USE==2){
-			async function execute_sql(err, result){
-				let pool2;
-				try{
-				pool2 = await oracledb.getConnection(get_pool(app_id));
-				const result = await pool2.execute(
-					`SELECT m.code "code",
+			sql = `SELECT 	m.code "code",
 							m.message_level_id "message_level_id",
 							m.message_type_id "message_type_id",
 							mt.language_id "language_id",
 							l.lang_code "lang_code",
 							mt.text "text",
 							am.app_id "app_id"
-					   FROM ${process.env.SERVICE_DB_DB2_NAME}.message m,
-					   		${process.env.SERVICE_DB_DB2_NAME}.message_translation mt,
-					   		${process.env.SERVICE_DB_DB2_NAME}.app_message am,
+					FROM ${process.env.SERVICE_DB_DB2_NAME}.message m,
+							${process.env.SERVICE_DB_DB2_NAME}.message_translation mt,
+							${process.env.SERVICE_DB_DB2_NAME}.app_message am,
 							${process.env.SERVICE_DB_DB2_NAME}.language l
-					  WHERE mt.language_id = l.id
+					WHERE mt.language_id = l.id
 						AND mt.message_code = m.code
 						AND am.message_code = m.code
 						AND am.app_id = :app_id
-					    AND m.code = :code
+						AND m.code = :code
 						AND (l.lang_code IN (:lang_code, 
 											SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), 
 											SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
 						OR (l.lang_code = 'en'
 							AND NOT EXISTS(SELECT NULL
-											 FROM ${process.env.SERVICE_DB_DB2_NAME}.message_translation mt1,
-												  ${process.env.SERVICE_DB_DB2_NAME}.language l1
-										    WHERE mt1.message_code = mt.message_code
-											  AND l1.id = mt1.language_id
-											  AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
+											FROM ${process.env.SERVICE_DB_DB2_NAME}.message_translation mt1,
+												${process.env.SERVICE_DB_DB2_NAME}.language l1
+											WHERE mt1.message_code = mt.message_code
+											AND l1.id = mt1.language_id
+											AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
 											)
 							)
-						)`,
-					{
-						app_id: app_id,
-						code: code,
-						lang_code: lang_code
-					},
-					(err,result) => {
-						if (err) {
-							createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-							return callBack(err);
-						}
-						else{
-							return callBack(null, result.rows[0]);
-						}
-					});
-				}catch (err) {
-					createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-					return callBack(err.message);
-				} finally {
-					if (pool2) {
-						try {
-							await pool2.close(); 
-						} catch (err) {
-							createLogAppSE(app_id, __appfilename, __appfunction, __appline, err);
-						}
-					}
-				}
-			}
-			execute_sql();
+						)`;
+			parameters = {
+							app_id: app_id,
+							code: code,
+							lang_code: lang_code
+						 };
 		}
+		execute_db_sql(app_id, app_id, sql, parameters, null, (err, result)=>{
+			if (err)
+				return callBack(err, null);
+			else
+				return callBack(null, result[0]);
+		});
 	}
 };
