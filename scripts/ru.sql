31b954b9b05799cac40e5a15c736a8581f13f534
fapwork <fapwork@proton.me>
2022-07-25 21:28:40 +0200

    APB-162 tables for screenshots bugfix db script, app category and translations

--- scripts/ru.sql
+++ scripts/ru.sql
@@ -1,6 +1,17 @@
 --Oracle, uncomment:
 --SET DEFINE OFF;
 --
+-- Dumping data for table `app_category_translation`
+--
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(1, 453, 'дело');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(2, 453, 'Связь');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(3, 453, 'Образование');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(4, 453, 'Финансировать');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(5, 453, 'Дом и дом');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(6, 453, 'Продуктивность');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(7, 453, 'Шоппинг');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(8, 453, 'Игра');
+--
 -- Dumping data for table `app_object_item_translation`
 --
 INSERT INTO app_object_item_translation (app_id, object_name, object_item_name, language_id, text) VALUES (0,'DIALOGUE','USERNAME',453,'Имя пользователя');
