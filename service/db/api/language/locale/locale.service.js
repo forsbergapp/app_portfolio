a743c073d2c11ad1be8463e6f6d5326d349c5e02
fapwork <fapwork@proton.me>
2022-08-04 16:11:04 +0200

    APB-33 country and languages translations updated for most languages saved in separate sql files, only english contains countries and languges translated separately, sql for language list bugfix, all languages returns 531 translated locales or default english if not translated and shortest locale

--- service/db/api/language/locale/locale.service.js
+++ service/db/api/language/locale/locale.service.js
@@ -4,87 +4,102 @@ module.exports = {
     let sql;
     let parameters;
     if (process.env.SERVICE_DB_USE == 1) {
-      sql = `SELECT   CONCAT(l.lang_code, CASE 
+      sql = `SELECT   CONCAT(l2.lang_code, CASE 
                                           WHEN c.country_code IS NOT NULL THEN 
                                             CONCAT('-', c.country_code) 
                                           ELSE 
                                             '' 
                                           END) locale, 
-                                          CONCAT(UPPER(SUBSTR(CONCAT(language_translation.text, 
-                                            CASE 
-                                            WHEN ct.text IS NOT NULL THEN 
-                                              CONCAT(' (', CONCAT(ct.text,')')) 
-                                            ELSE 
-                                              '' 
-                                            END),1,1)),SUBSTR(CONCAT(language_translation.text, 
-                                                                      CASE 
-                                                                      WHEN ct.text IS NOT NULL THEN 
-                                                                        CONCAT(' (', CONCAT(ct.text,')')) 
-                                                                      ELSE 
-                                                                        '' 
-                                                                      END),2)) text
-                                  FROM  ${process.env.SERVICE_DB_DB1_NAME}.language l,
-                                  ${process.env.SERVICE_DB_DB1_NAME}.language_translation,
-                                  ${process.env.SERVICE_DB_DB1_NAME}.locale loc
-                                  LEFT OUTER JOIN ${process.env.SERVICE_DB_DB1_NAME}.country c
-                                  ON c.id= loc.country_id
-                                  LEFT OUTER JOIN ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct
-                                  ON ct.country_id = loc.country_id
-                                  WHERE l.id = loc.language_id
-                                  AND language_translation.language_id = l.id
-                                  AND language_translation.language_translation_id IN 
-                                  (SELECT id 
-                                  FROM ${process.env.SERVICE_DB_DB1_NAME}.language l2
-                                  WHERE (l2.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                  OR (l2.lang_code = 'en'
-                                  AND NOT EXISTS(SELECT NULL
-                                          FROM  ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt1,
+                      CONCAT(UPPER(SUBSTR(CONCAT(lt.text, 
+                            CASE 
+                            WHEN ct.text IS NOT NULL THEN 
+                              CONCAT(' (', CONCAT(ct.text,')')) 
+                            ELSE 
+                              '' 
+                            END),1,1)),SUBSTR(CONCAT(lt.text, 
+                                                      CASE 
+                                                      WHEN ct.text IS NOT NULL THEN 
+                                                        CONCAT(' (', CONCAT(ct.text,')')) 
+                                                      ELSE 
+                                                        '' 
+                                                      END),2)) text
+              FROM ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt,
+                   ${process.env.SERVICE_DB_DB1_NAME}.language l,
+                   ${process.env.SERVICE_DB_DB1_NAME}.language l2,
+                   ${process.env.SERVICE_DB_DB1_NAME}.locale loc,
+                   ${process.env.SERVICE_DB_DB1_NAME}.country c,
+                   ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct,
+                   ${process.env.SERVICE_DB_DB1_NAME}.language c2
+             WHERE lt.language_translation_id = l.id
+               AND loc.language_id = lt.language_id
+               AND l2.id = lt.language_id
+               AND c.id = loc.country_id
+               AND ct.country_id = c.id
+               AND c2.id = ct.language_id
+               AND (l.lang_code = (SELECT MIN(l1.lang_code)
+                                     FROM ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt1,
+                                          ${process.env.SERVICE_DB_DB1_NAME}.language l1
+                                    WHERE l1.id  = lt1.language_translation_id
+                                      AND lt1.language_id = lt.language_id
+                                      AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+                                  )
+                    OR (l.lang_code = 'en'
+                          AND NOT EXISTS(SELECT NULL
+                                           FROM ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt1,
                                                 ${process.env.SERVICE_DB_DB1_NAME}.language l1
                                           WHERE l1.id  = lt1.language_translation_id
-                                          AND lt1.language_id = l.id
-                                          AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                      )
-                                  )
-                                  )
-                                  )
-                                  AND ct.language_id IN 
-                                  (SELECT id 
-                                  FROM language l2
-                                  WHERE (l2.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                  OR (l2.lang_code = 'en'
-                                  AND NOT EXISTS(SELECT NULL
-                                          FROM  ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct1,
-                                                ${process.env.SERVICE_DB_DB1_NAME}.language l1
-                                          WHERE l1.id = ct1.language_id
-                                          AND  ct1.country_id = c.id
-                                          AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                      )
-                                  )
-                                  )
-                                  )
-                                  UNION
-                                  SELECT  l.lang_code locale,
-                                  CONCAT(UPPER(SUBSTR(lt.text,1,1)), SUBSTR(lt.text,2)) text
-                                  FROM  ${process.env.SERVICE_DB_DB1_NAME}.language l,
-                                  ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt
-                                  WHERE lt.language_id = l.id
-                                  AND INSTR(l.lang_code,'-') = 0
-                                  AND lt.language_translation_id IN 
-                                  (SELECT id 
-                                  FROM ${process.env.SERVICE_DB_DB1_NAME}.language l2
-                                  WHERE (l2.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                  OR (l2.lang_code = 'en'
-                                  AND NOT EXISTS(SELECT NULL
-                                                  FROM  ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt1,
-                                                        ${process.env.SERVICE_DB_DB1_NAME}.language l1
-                                                  WHERE l1.id  = lt1.language_translation_id
-                                                  AND lt1.language_id = l.id
-                                                  AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                              )
-                                          )
+                                            AND lt1.language_id = lt.language_id
+                                            AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
                                         )
+                        )
+                  )
+              AND (c2.lang_code = (SELECT MIN(l1.lang_code)
+                                     FROM ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct1,
+                                          ${process.env.SERVICE_DB_DB1_NAME}.language l1
+                                    WHERE l1.id  = ct1.language_id
+                                      AND ct1.country_id = c.id
+                                      AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
                                   )
-                                  ORDER BY 2 `;
+                    OR (c2.lang_code = 'en'
+                          AND NOT EXISTS(SELECT NULL
+                                           FROM ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct1,
+                                                ${process.env.SERVICE_DB_DB1_NAME}.language l1
+                                          WHERE l1.id  = ct1.language_id
+                                            AND ct1.country_id = c.id
+                                            AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+                                        )
+                       )
+                  )
+            UNION
+            SELECT l2.lang_code locale,
+                   CONCAT(UPPER(SUBSTR(lt.text,1,1)), SUBSTR(lt.text,2)) text
+              FROM ${process.env.SERVICE_DB_DB1_NAME}.language l,
+                   ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt,
+                   ${process.env.SERVICE_DB_DB1_NAME}.language l2
+             WHERE lt.language_translation_id = l.id
+               AND INSTR(l2.lang_code,'-') = 0
+               AND l2.id = lt.language_id
+               AND (l.lang_code = (SELECT MIN(l3.lang_code)
+                                     FROM ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt3,
+                                          ${process.env.SERVICE_DB_DB1_NAME}.language l3
+                                    WHERE l3.id  = lt3.language_translation_id
+                                      AND lt3.language_id = lt.language_id
+                                      AND l3.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+                                  )
+                      OR (l.lang_code = 'en'
+                          AND NOT EXISTS(SELECT NULL
+                                          FROM ${process.env.SERVICE_DB_DB1_NAME}.language_translation lt4,
+                                               ${process.env.SERVICE_DB_DB1_NAME}.language l4
+                                          WHERE l4.id  = lt4.language_translation_id
+                                          AND lt4.language_id = lt.language_id
+                                          AND l4.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+                                        )
+                        )
+                    )
+               AND  EXISTS(SELECT NULL
+                             FROM locale loc
+                            WHERE loc.language_id = lt.language_id)
+            ORDER BY 2 `;
       parameters = [lang_code,
                     lang_code,
                     lang_code,
