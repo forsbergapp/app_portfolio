aabaeef760dc48853650daf83bac164beeedd1c8
appportfolio <forsbergapp@protonmail.com>
2022-08-26 19:37:45 +0200

    APB-48 added more columns to app2_setting, moved app2 objects in import.sql to app2/scripts and with schemaname, app2/scripts/*.sql with app2_setting, app2_setting_type and app2_setting_translation records

---
+++ apps/app2/scripts/ar.sql
@@ -0,0 +1,31 @@
+--
+-- Dumping data for table app2_setting_translation
+--
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (3,25,'لا شيء');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (4,25,'يوم الجمعة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (5,25,'يوم السبت');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (6,25,'الأحد');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (7,25,'مجموعات عشرة أيام');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (26,25,'اساسي');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (27,25,'حنفي');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (28,25,'منتصف الليل');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (29,25,'زاوية / 60 من الليل (موصى به)');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (30,25,'1/7 الليل');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (31,25,'لا تعديل');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (32,25,'12 ساعة بدون لاحقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (33,25,'24 ساعة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (34,25,'12 ساعة مع لاحقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (40,25,'لا شيء');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (41,25,'عشر دقائق');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (42,25,'خمس عشرة دقيقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (43,25,'عشرون دقيقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (44,25,'خمس وعشرون دقيقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (45,25,'ثلاثون دقيقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (46,25,'الساعة القادمة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (47,25,'الساعة القادمة + خمسة عشر دقيقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (48,25,'الساعة القادمة + ثلاثون دقيقة');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (49,25,'لا');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (50,25,'الفجر والمغرب');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (51,25,'امساك و المغرب');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (52,25,'الفجر والعشاء');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (53,25,'إمساك وعشاء');
---
+++ apps/app2/scripts/no.sql
@@ -0,0 +1,31 @@
+--
+-- Dumping data for table app2_setting_translation
+--
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (3,393,'Ingen');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (4,393,'Fredag');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (5,393,'Lørdag');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (6,393,'Søndag');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (7,393,'10 dagers grupper');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (26,393,'Standard');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (27,393,'Hanafi');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (28,393,'Midt på natten');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (29,393,'Vinkel/60. natt (anbefalt)');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (30,393,'1/7 av natten');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (31,393,'Ingen justering');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (32,393,'12 timer uten suffiks');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (33,393,'24 timer i døgnet');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (34,393,'12 timer med suffiks');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (40,393,'Ingen');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (41,393,'10 minutter');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (42,393,'15 minutter');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (43,393,'20 minutter');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (44,393,'25 minutter');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (45,393,'30 minutter');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (46,393,'Neste time');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (47,393,'Neste time + 15 minutter');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (48,393,'Neste time + 30 minutter');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (49,393,'Nei');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (50,393,'Fajr & Mahgrib');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (51,393,'Imsak & Mahgrib');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (52,393,'Fajr & Isha');
+INSERT INTO app_portfolio.app2_setting_translation (app2_setting_id, language_id, text) VALUES (53,393,'Imsak & Isha');
