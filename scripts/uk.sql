9c7fdb182df6e843fc3ff079ca9e23dfac254d2c
appportfolio <app.portfolio@protonmail.com>
2022-09-03 00:34:02 +0200

    APB-33 added ukranian language

---
+++ scripts/uk.sql
@@ -0,0 +1,135 @@
+--Oracle, uncomment:
+--SET DEFINE OFF;
+--
+-- app_category_translation
+--
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(1, 564, 'Бізнес');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(2, 564, 'Комунікації');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(3, 564, 'Освіта');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(4, 564, 'Фінанси');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(5, 564, 'Будинок і дім');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(6, 564, 'Продуктивність');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(7, 564, 'Шопінг');
+INSERT INTO app_portfolio.app_category_translation(app_category_id, language_id, text) VALUES(8, 564, 'Гра');
+--
+-- app_object_item_translation
+--
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','USERNAME',564,"Ім'я користувача");
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','PASSWORD',564,'Пароль');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','BIO',564,'біографія');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','EMAIL',564,'Електронна пошта');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','NEW_EMAIL',564,'Нова електронна пошта');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','PASSWORD_CONFIRM',564,'Підтвердження пароля');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','PASSWORD_REMINDER',564,'Нагадування пароля');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','NEW_PASSWORD_CONFIRM',564,'Підтвердження нового пароля');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','NEW_PASSWORD',564,'Новий пароль');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (0,'COMMON','CONFIRM_QUESTION',564,'Ти впевнений?');
+
+
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_ASR',564,'полудень');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_ASR_IQAMAT',564,'Iqamat');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_CALTYPE_GREGORIAN',564,'Григоріанський');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_CALTYPE_HIJRI',564,'Хіджра');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_DAY',564,'День');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_DHUHR',564,'полудень');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_DHUHR_IQAMAT',564,'Iqamat');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_FAJR',564,'Світанок');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_FAJR_IQAMAT',564,'Iqamat');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_IMSAK',564,'Imsak');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_ISHA',564,'ніч');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_ISHA_IQAMAT',564,'Iqamat');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_MAGHRIB',564,'Захід сонця');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_MAGHRIB_IQAMAT',564,'Iqamat');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_MIDNIGHT',564,'опівночі');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_NOTES',564,'Примітки');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_SUNRISE',564,'Схід сонця');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_SUNSET',564,'Захід сонця');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_ASR',564,'Asr');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_ASR_IQAMAT',564,'Ікамат');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_DHUHR',564,'Dhuhr');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_DHUHR_IQAMAT',564,'Ікамат');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_FAJR',564,'Fajr');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_FAJR_IQAMAT',564,'Ікамат');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_IMSAK',564,'Imsak');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_ISHA',564,'Isha');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_ISHA_IQAMAT',564,'Ікамат');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_MAGHRIB',564,'Maghrib');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_MAGHRIB_IQAMAT',564,'Ікамат');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_MIDNIGHT',564,'Midnight');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_SUNRISE',564,'Shorook');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_TRANSLITERATION_SUNSET',564,'Sunset');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','COLTITLE_WEEKDAY',564,'будній день');
+INSERT INTO app_portfolio.app_object_item_translation (app_object_item_app_object_app_id, app_object_item_app_object_object_name, app_object_item_object_item_name, language_id, text) VALUES (2,'REPORT','TIMETABLE_TITLE',564,'розклад');
+--
+-- app_object_translation
+--
+INSERT INTO app_portfolio.app_object_translation (app_object_app_id, app_object_object_name, language_id, text) VALUES (2,'APP_DESCRIPTION',564,'Прогресивна веб-програма (PWA) односторінкова програма (SPA)');
+INSERT INTO app_portfolio.app_object_translation (app_object_app_id, app_object_object_name, language_id, text) VALUES (3,'APP_DESCRIPTION',564,'Проста програма для відображення моделі даних і способів інтеграції програми в платформу програми');
+--
+-- message_translation
+--
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20100',"Ім'я користувача 5 - 100 символів");
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20101',"Недійсне ім'я користувача");
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20102','Біо максимум 100 символів');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20103','Електронна пошта не більше 100 символів');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20104','Нагадування максимум 100 символів');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20105','Недійсна електронна адреса');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20106','Пароль 10 - 100 символів');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20107',"Ім'я користувача, пароль і електронна адреса є обов'язковими");
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20200','Електронна пошта вже існує');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20201','Провайдер вже існує');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20203',"Ім'я користувача вже існує");
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20204','Занадто довге значення');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20300',"Ім'я користувача або пароль не знайдено");
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20301','Пароль не той');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20302','Ви не можете видалити останні налаштування користувача');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20303',"Будь ласка, введіть ім'я користувача");
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20304','Будь ласка, введіть пароль');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20305','Користувача не знайдено');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20306','Код недійсний');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20307','Тип файлу заборонений');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20308','Розмір файлу завеликий');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20309','Текст недійсний');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20310','Надто довгий текст');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20400','Запис не знайдено');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20401','Недійсний пароль');
+INSERT INTO app_portfolio.message_translation (language_id, message_code, text) VALUES (564,'20500','Відсутня широта або довгота');
+--
+-- setting_translation
+--
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (3,564,'Жодного');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (4,564,"П'ятниця");
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (5,564,'Субота');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (6,564,'неділя');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (7,564,'Групи 10 днів');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (26,564,'Стандартний');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (27,564,'ханафітський');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (28,564,'Серед ночі');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (29,564,'Ракурс/60-та ночі (рекомендовано)');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (30,564,'1/7 ночі');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (31,564,'Без регулювання');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (32,564,'12-годинний без суфікса');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (33,564,'24 години');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (34,564,'12-годинний із суфіксом');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (40,564,'Жодного');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (41,564,'10 хвилин');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (42,564,'15 хвилин');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (43,564,'20 хвилин');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (44,564,'25 хвилин');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (45,564,'30 хвилин');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (46,564,'Наступна година');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (47,564,'Наступна година + 15 хвилин');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (48,564,'Наступна година + 30 хвилин');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (49,564,'Ні');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (50,564,'Fajr & Mahgrib');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (51,564,'Imsak & Mahgrib');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (52,564,'Fajr & Isha');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (53,564,'Imsak & Isha');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (532,564,'Зліва направо');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (533,564,'Справа наліво');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (534,564,'Транслітерація');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (535,564,'Транслітерація, переклад');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (536,564,'Переклад, транслітерація');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (537,564,'Переклад');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (543,564,'Григоріанський');
+INSERT INTO app_portfolio.setting_translation (setting_id, language_id, text) VALUES (544,564,'Хіджра');
