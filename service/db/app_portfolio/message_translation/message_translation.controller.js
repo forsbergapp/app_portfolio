10d5857b291ab624b7d02ee9631d931b7e4d7ada
fapwork <fapwork@proton.me>
2022-09-02 23:18:45 +0200

    APB-220 adding data app_id parameter fetching message so app uses correct pool but fetching message from other app, admin uses own getmessage_admin function, handling error in fetching error message, bugfix create log declaration, obsolete comments removal, app2 bugfix window size profile report view, buggfix sql: changed column name

--- service/db/app_portfolio/message_translation/message_translation.controller.js
+++ service/db/app_portfolio/message_translation/message_translation.controller.js
@@ -2,10 +2,7 @@ const { getMessage } = require ("./message_translation.service");
 
 module.exports = {
 	getMessage: (req, res) => {
-		var code = req.params.code;
-		var app_id = req.query.app_id; 
-		var lang_code = req.query.lang_code;
-		getMessage(code, app_id, lang_code, (err, results) =>{
+		getMessage(req.params.code, req.query.data_app_id, req.query.app_id, req.query.lang_code, (err, results) =>{
 			if (err) {
 				return res.status(500).send({
 					success: 0,
