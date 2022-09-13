dc6499edd331ed74e60163f9013b8145f62bb984
appportfolio <app.portfolio@protonmail.com>
2022-09-13 22:00:35 +0200

    APB-231 all translations use same logic for lang_code with COALESCE and MAX functions in mysql and oracle

--- service/db/app_portfolio/app/app.service.js
+++ service/db/app_portfolio/app/app.service.js
@@ -17,48 +17,34 @@ module.exports = {
 							ON aot.app_object_app_id = a.id
 						AND aot.app_object_object_name = 'APP_DESCRIPTION'
 							AND aot.language_id IN (SELECT id 
-													FROM ${process.env.SERVICE_DB_DB1_NAME}.language l
-													WHERE (l.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-															OR (l.lang_code = 'en'
-																AND NOT EXISTS(SELECT NULL
-																				FROM ${process.env.SERVICE_DB_DB1_NAME}.app_object_translation aot1,
-																					 ${process.env.SERVICE_DB_DB1_NAME}.language l1
-																				WHERE l1.id  = aot1.language_id
-																				AND aot1.app_object_app_id  = aot.app_object_app_id
-																				AND aot1.app_object_object_name = aot.app_object_object_name
-																				AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-																			)
-															)
-														)
-												)
+													  FROM ${process.env.SERVICE_DB_DB1_NAME}.language l
+													  WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+																		     FROM ${process.env.SERVICE_DB_DB1_NAME}.app_object_translation aot1,
+																				  ${process.env.SERVICE_DB_DB1_NAME}.language l1
+																		    WHERE l1.id  = aot1.language_id
+  																			  AND aot1.app_object_app_id  = aot.app_object_app_id
+																			  AND aot1.app_object_object_name = aot.app_object_object_name
+																			  AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+																		  )
+												   )
 						LEFT OUTER JOIN ${process.env.SERVICE_DB_DB1_NAME}.app_category_translation act
 							ON act.app_category_id = a.app_category_id
 							AND act.language_id IN (SELECT id 
 													  FROM ${process.env.SERVICE_DB_DB1_NAME}.language l
-													  WHERE (l.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-															OR (l.lang_code = 'en'
-																AND NOT EXISTS(SELECT NULL
-																				FROM ${process.env.SERVICE_DB_DB1_NAME}.app_category_translation act1,
-																					 ${process.env.SERVICE_DB_DB1_NAME}.language l1
-																				WHERE l1.id  = act1.language_id
-																				AND act1.app_category_id  = act.app_category_id
-																				AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-																			)
-															)
-														)
-							)
+													  WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+																			 FROM ${process.env.SERVICE_DB_DB1_NAME}.app_category_translation act1,
+																				  ${process.env.SERVICE_DB_DB1_NAME}.language l1
+																			WHERE l1.id  = act1.language_id
+																			  AND act1.app_category_id  = act.app_category_id
+																			  AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+																		  )
+													)
 					WHERE (a.id = COALESCE(?, a.id)
 						OR 
 						? = 0)
 					AND a.enabled = 1
 					ORDER BY 1 `;
 			parameters = [	lang_code,
-							lang_code,
-							lang_code,
-							lang_code,
-							lang_code,
-							lang_code,
-							lang_code,
 							lang_code,
 							lang_code,
 							lang_code,
@@ -79,40 +65,28 @@ module.exports = {
 							  ON aot.app_object_app_id = a.id
 							 AND aot.app_object_object_name = 'APP_DESCRIPTION'
 							 AND aot.language_id IN (SELECT id 
-														FROM ${process.env.SERVICE_DB_DB2_NAME}.language l
-														WHERE (l.lang_code IN (:lang_code, 
-																			SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), 
-																			SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-																OR (l.lang_code = 'en'
-																	AND NOT EXISTS(SELECT NULL
-																					FROM ${process.env.SERVICE_DB_DB2_NAME}.app_object_translation aot1,
-																						${process.env.SERVICE_DB_DB2_NAME}.language l1
-																					WHERE l1.id  = aot1.language_id
-																					AND aot1.app_object_app_id  = aot.app_object_app_id
-																					AND aot1.app_object_object_name = aot.app_object_object_name
-																					AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-																				)
-																)
-															)
+													   FROM ${process.env.SERVICE_DB_DB2_NAME}.language l
+													  WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+																			 FROM ${process.env.SERVICE_DB_DB2_NAME}.app_object_translation aot1,
+																				  ${process.env.SERVICE_DB_DB2_NAME}.language l1
+																			WHERE l1.id  = aot1.language_id
+ 																			  AND aot1.app_object_app_id  = aot.app_object_app_id
+																			  AND aot1.app_object_object_name = aot.app_object_object_name
+																			  AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
+																		  )
 													)
 							LEFT OUTER JOIN ${process.env.SERVICE_DB_DB2_NAME}.app_category_translation act
 							  ON act.app_category_id = a.app_category_id
 							 AND act.language_id IN (SELECT id 
 													   FROM ${process.env.SERVICE_DB_DB2_NAME}.language l
-													  WHERE (l.lang_code IN (:lang_code, 
-																			  SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), 
-																			  SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-															OR (l.lang_code = 'en'
-																AND NOT EXISTS(SELECT NULL
-																				 FROM ${process.env.SERVICE_DB_DB2_NAME}.app_category_translation act1,
-																					  ${process.env.SERVICE_DB_DB2_NAME}.language l1
-																				WHERE l1.id  = act1.language_id
-																				AND act1.app_category_id  = act.app_category_id
-																				AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-																			)
-															)
+													  WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+																			 FROM ${process.env.SERVICE_DB_DB2_NAME}.app_category_translation act1,
+																				  ${process.env.SERVICE_DB_DB2_NAME}.language l1
+																			WHERE l1.id  = act1.language_id
+																			  AND act1.app_category_id  = act.app_category_id
+																			  AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
+																		  )
 													)
-							)
 					WHERE (id= NVL(:id, id)
 						OR 
 						:id = 0)
