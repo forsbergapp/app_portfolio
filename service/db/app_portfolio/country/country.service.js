636af340944a88fba576b9c08ba3ba61814146b6
fapwork <fapwork@proton.me>
2022-09-13 22:00:35 +0200

    APB-231 all translations use same logic for lang_code with COALESCE and MAX functions in mysql and oracle

--- service/db/app_portfolio/country/country.service.js
+++ service/db/app_portfolio/country/country.service.js
@@ -4,62 +4,51 @@ module.exports = {
                 let sql;
                 let parameters;
                 if (process.env.SERVICE_DB_USE == 1) {
-                        sql = `SELECT    c.id,
-                                        c.country_code,
-                                        c.flag_emoji,
-                                        ct.text,
-                                        cg.group_name
-                                FROM    ${process.env.SERVICE_DB_DB1_NAME}.country  c,
-                                        ${process.env.SERVICE_DB_DB1_NAME}.country_group cg, 
-                                        ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct,
-                                        ${process.env.SERVICE_DB_DB1_NAME}.language l
+                        sql = `SELECT c.id,
+                                      c.country_code,
+                                      c.flag_emoji,
+                                      ct.text,
+                                      cg.group_name
+                                 FROM ${process.env.SERVICE_DB_DB1_NAME}.country c,
+                                      ${process.env.SERVICE_DB_DB1_NAME}.country_group cg,
+                                      ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct
                                 WHERE ct.country_id = c.id
-                                AND   cg.id = c.country_group_id
-                                AND   l.id = ct.language_id
-                                AND (l.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                        OR (l.lang_code = 'en'
-                                                AND NOT EXISTS(SELECT NULL
-                                                                FROM ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct1,
-                                                                        ${process.env.SERVICE_DB_DB1_NAME}.language l1
-                                                                WHERE ct1.country_id = ct.country_id
-                                                                AND l1.id = ct1.language_id
-                                                                AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
-                                                        )
-                                                )
-                                        )
+                                  AND cg.id = c.country_group_id
+                                  AND ct.language_id IN (SELECT id 
+                                                           FROM ${process.env.SERVICE_DB_DB1_NAME}.language l
+                                                          WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+                                                                                 FROM ${process.env.SERVICE_DB_DB1_NAME}.country_translation ct1,
+                                                                                      ${process.env.SERVICE_DB_DB1_NAME}.language l1
+                                                                                WHERE ct1.country_id = ct.country_id
+                                                                                  AND l1.id = ct1.language_id
+                                                                                  AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
+                                                                              )
+                                                         )
                                 ORDER BY 5, 4`;
                         parameters = [  lang_code,
                                         lang_code,
-                                        lang_code,
-                                        lang_code,
-                                        lang_code,
                                         lang_code];
                 } else if (process.env.SERVICE_DB_USE == 2) {
-                        sql = `SELECT    c.id "id",
-                                        c.country_code "country_code",
-                                        c.flag_emoji "flag_emoji",
-                                        ct.text "text",
-                                        cg.group_name "group_name"
-                                FROM    ${process.env.SERVICE_DB_DB2_NAME}.country  c,
-                                        ${process.env.SERVICE_DB_DB2_NAME}.country_group cg,
-                                        ${process.env.SERVICE_DB_DB2_NAME}.country_translation ct,
-                                        ${process.env.SERVICE_DB_DB2_NAME}.language l
+                        sql = `SELECT c.id "id",
+                                      c.country_code "country_code",
+                                      c.flag_emoji "flag_emoji",
+                                      ct.text "text",
+                                      cg.group_name "group_name"
+                                 FROM ${process.env.SERVICE_DB_DB2_NAME}.country  c,
+                                      ${process.env.SERVICE_DB_DB2_NAME}.country_group cg,
+                                      ${process.env.SERVICE_DB_DB2_NAME}.country_translation ct
                                 WHERE ct.country_id = c.id
-                                AND   cg.id = c.country_group_id
-                                AND   l.id = ct.language_id
-                                AND (l.lang_code IN (:lang_code, 
-                                                SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), 
-                                                SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-                                        OR (l.lang_code = 'en'
-                                                AND NOT EXISTS(SELECT NULL
-                                                                FROM ${process.env.SERVICE_DB_DB2_NAME}.country_translation ct1,
-                                                                ${process.env.SERVICE_DB_DB2_NAME}.language l1
-                                                                WHERE ct1.country_id = ct.country_id
-                                                                AND l1.id = ct1.language_id
-                                                                AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-                                                                )
-                                                )
-                                        )
+                                  AND cg.id = c.country_group_id
+                                  AND ct.language_id IN (SELECT id 
+                                                           FROM ${process.env.SERVICE_DB_DB2_NAME}.language l
+                                                          WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+                                                                                 FROM ${process.env.SERVICE_DB_DB2_NAME}.country_translation ct1,
+                                                                                      ${process.env.SERVICE_DB_DB2_NAME}.language l1
+                                                                                WHERE ct1.country_id = ct.country_id
+                                                                                  AND l1.id = ct1.language_id
+                                                                                  AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
+                                                                              )
+                                                        )
                                 ORDER BY 5, 4`;
                         parameters = {
                                         lang_code: lang_code
