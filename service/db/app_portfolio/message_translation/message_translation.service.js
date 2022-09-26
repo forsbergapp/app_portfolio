d2d11b095132ad65417ac8943edf1c1dcd5508dc
fapwork <fapwork@proton.me>
2022-09-26 13:38:07 +0200

    APB-238 setting empty lists if error, bugfix get message admin sql, returning 404 for no records found for lists app_log, connected, server log, pm2 log, get files lov, parameter type lov, list titles css fixes, dialogue message css max height and scroll autos to handle long technical messages, when error set empty list for app and app parameter, bugfix connected filter

--- service/db/app_portfolio/message_translation/message_translation.service.js
+++ service/db/app_portfolio/message_translation/message_translation.service.js
@@ -76,34 +76,31 @@ module.exports = {
 		let sql;
     	let parameters;
 		if (process.env.SERVICE_DB_USE==1){
-			sql = `SELECT m.code "code",
-						  m.message_level_id "message_level_id",
-						  m.message_type_id "message_type_id",
-						  mt.language_id "language_id",
-						  l.lang_code "lang_code",
-						  mt.text "text",
-						  am.app_id "app_id"
-				     FROM ${process.env.SERVICE_DB_DB2_NAME}.message m,
-						  ${process.env.SERVICE_DB_DB2_NAME}.message_translation mt,
-						  ${process.env.SERVICE_DB_DB2_NAME}.app_message am,
-						  ${process.env.SERVICE_DB_DB2_NAME}.language l
-				    WHERE mt.language_id = l.id
-  					  AND mt.message_code = m.code
-					  AND am.message_code = m.code
-					  AND am.app_id = :app_id
-					  AND m.code = :code
-					  AND l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
-										   FROM ${process.env.SERVICE_DB_DB2_NAME}.message_translation mt1,
-  											    ${process.env.SERVICE_DB_DB2_NAME}.language l1
-										  WHERE mt1.message_code = mt.message_code
-										    AND l1.id = mt1.language_id
-										    AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
+			sql = `SELECT m.code,
+						  m.message_level_id,
+						  m.message_type_id,
+						  mt.language_id,
+						  l.lang_code,
+						  mt.text,
+						  am.app_id
+					 FROM ${process.env.SERVICE_DB_DB1_NAME}.message m,
+						  ${process.env.SERVICE_DB_DB1_NAME}.message_translation mt,
+						  ${process.env.SERVICE_DB_DB1_NAME}.app_message am,
+						  ${process.env.SERVICE_DB_DB1_NAME}.language l
+					 WHERE mt.language_id = l.id
+					   AND mt.message_code = m.code
+					   AND am.message_code = m.code
+					   AND am.app_id = ?
+					   AND m.code = ?
+					   AND l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+											FROM ${process.env.SERVICE_DB_DB1_NAME}.message_translation mt1,
+												 ${process.env.SERVICE_DB_DB1_NAME}.language l1
+										   WHERE mt1.message_code = mt.message_code
+											 AND l1.id = mt1.language_id
+											 AND l1.lang_code IN (?, SUBSTRING_INDEX(?,'-',2), SUBSTRING_INDEX(?,'-',1))
 										)`;
 			parameters = [	app_id,
 							code,
-							lang_code,
-							lang_code,
-							lang_code,
 							lang_code,
 							lang_code,
 							lang_code
