84e6d5550f4377ccc9e50c01685a229da9941872
fapwork <fapwork@proton.me>
2022-09-27 15:22:55 +0200

    APB-235 improving cohesion and coupling design, displayday, displaymonth and displayyear use same promise resolve syntax, using style.display instead of style.visibility, divs changed to block, simplified navigation left and right logic for timetables, auth control not checking service/regional frequent used functions and display null when error for these to avoid messages on open url, added spinner on paper div and adjust dialogue_loading spinner size

--- service/regional/regional.controller.js
+++ service/regional/regional.controller.js
@@ -1,14 +1,25 @@
 const { getTimezoneOffset, getGregorian} = require ("./regional.service");
 module.exports = {
 	getTimezoneOffset: (req, res) => {
-        return res.json(
-            getTimezoneOffset(req.query.timezone)
-        )
-    
+        try {
+            return res.json(
+                getTimezoneOffset(req.query.timezone)    
+            )
+        } catch (error) {
+            return res.send(
+                null
+            )
+        };
     },
 	getGregorian: (req, res) => {
-        return res.json(
-            getGregorian(JSON.parse(req.query.hijridate), req.query.adjustment)
-        )
+        try {
+            return res.json(
+                getGregorian(JSON.parse(req.query.hijridate), req.query.adjustment)    
+            )
+        } catch (error) {
+            return res.send(
+                null
+            )
+        }
 	}
 };
