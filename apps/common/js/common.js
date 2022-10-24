e0b251e74d7c0a3e720473e44b514f9e48983015
fapwork <fapwork@proton.me>
2022-10-24 18:54:51 +0200

    APB-236 replaced slow service/regional with commons/js/regional.js now timetables is client only execution

--- apps/common/js/common.js
+++ apps/common/js/common.js
@@ -5,7 +5,6 @@
     BROADCAST
     GPS
     MAP
-    REGIONAL
     COUNTRY & CITIES
     QR
     PROFILE
@@ -1524,25 +1523,6 @@ async function map_update(longitude, latitude, zoom, text_place, timezone_text =
 }
 
 /*----------------------- */
-/* REGIONAL               */
-/*----------------------- */
-async function getTimezoneOffset(timezone){
-    return new Promise(function (resolve, reject){
-        common_fetch(`${window.global_service_regional}${window.global_service_regional_timezoneoffset}?timezone=${timezone}`,
-                     'GET', 0, null, null, null, (err, timezoneoffset) =>{
-                            resolve (timezoneoffset);
-        })
-    })
-}
-async function HijriToGreg(hijridate, adjustment){
-    return new Promise(function (resolve, reject){
-        common_fetch(`${window.global_service_regional}${window.global_service_regional_gregorian}?hijridate=${JSON.stringify(hijridate)}&adjustment=${adjustment}`,
-                     'GET', 0, null, null, null, (err, gregorian) =>{
-                            resolve (JSON.parse(gregorian));
-        })
-    })
-}
-/*----------------------- */
 /* COUNTRY & CITIES       */
 /*----------------------- */
 async function get_cities(countrycode, callBack){
@@ -3013,9 +2993,6 @@ function set_globals(parameters){
     window.global_service_geolocation_gps_timezone;
     window.global_service_geolocation_gps_place;
     window.global_service_geolocation_gps_ip;
-    window.global_service_regional;
-    window.global_service_regional_timezoneoffset;
-    window.global_service_regional_gregorian;
     window.global_service_report;
     window.global_service_worldcities;
         
@@ -3311,9 +3288,6 @@ async function init_common(parameters, callBack){
                 case 'SERVICE_MAP_JUMPTO'                   :{window.global_service_map_jumpto = parseInt(parameter_value);break;}
                 case 'SERVICE_MAP_POPUP_OFFSET'             :{window.global_service_map_popup_offset = parseInt(parameter_value);break;}
                 case 'SERVICE_MAP_STYLE'                    :{window.global_service_map_style = parameter_value;break;}
-                case 'SERVICE_REGIONAL'                     :{window.global_service_regional = parameter_value;break;}
-                case 'SERVICE_REGIONAL_TIMEZONEOFFSET'      :{window.global_service_regional_timezoneoffset = parameter_value;break;}
-                case 'SERVICE_REGIONAL_GREGORIAN'           :{window.global_service_regional_gregorian = parameter_value;break;}
                 case 'SERVICE_REPORT'                       :{window.global_service_report = parameter_value;break;}
                 case 'SERVICE_WORLDCITIES'                  :{window.global_service_worldcities = parameter_value;break;}
             }
--- apps/common/js/regional.js
+++ apps/common/js/regional.js
@@ -1,6 +1,4 @@
-
-module.exports = {
-    getGregorian: (arabicDate, adjustment)=>{
+async function getGregorian (arabicDate, adjustment){
         //http://www.islamicity.org
         MHK = false;
 
@@ -157,46 +155,45 @@ module.exports = {
 	english_date[0] = y;
 
 	return english_date;
-    },
+}
+//https://github.com/mobz/get-timezone-offset
+//It returns the timezone offset in minutes for any IANA timezone name 
+//for any valid javascript date in the past, present and future.
+//used for sending offset to prayTimes.getTimes
+//function getTimezoneOffset( tz_str, date ) {
+async function getTimezoneOffset(tz_str){
     //https://github.com/mobz/get-timezone-offset
-    //It returns the timezone offset in minutes for any IANA timezone name 
-    //for any valid javascript date in the past, present and future.
-    //used for sending offset to prayTimes.getTimes
-    //function getTimezoneOffset( tz_str, date ) {
-    getTimezoneOffset: (tz_str)=>{
-        //https://github.com/mobz/get-timezone-offset
-        function parseDate( date_str ) {
-            var us_re = /(\d+).(\d+).(\d+),?\s+(\d+).(\d+)(.(\d+))?/;
-            date_str = date_str.replace(/[\u200E\u200F]/g, '');
-            var date_a = us_re.exec( date_str );
-            return [].slice.call(us_re.exec( date_str ), 1)
-                .map( Math.floor );
-        }
-        //https://github.com/mobz/get-timezone-offset
-        function diffMinutes( d1, d2 ) {
-            var day = d1[1] - d2[1];
-            var hour = d1[3] - d2[3];
-            var min = d1[4] - d2[4];
-            if( day > 15 ) day = -1;
-            if( day < -15 ) day = 1;
-            return 60 * ( 24 * day + hour ) + min;
-        }
-
-        var now = new Date();
-        var locale = 'en-us';
-        var format_options = {timeZone: 'UTC',
-                            hour12: false,
-                            year: 'numeric',
-                            month: 'numeric',
-                            day: 'numeric',
-                            hour: 'numeric',
-                            minute: 'numeric'};
-
-        var utc_f = new Intl.DateTimeFormat(locale, format_options );
-        format_options.timeZone = tz_str;
-        var loc_f = new Intl.DateTimeFormat(locale, format_options );
-        return -1 * diffMinutes(
-            parseDate( utc_f.format( now )),
-            parseDate( loc_f.format( now ))) / 60;
+    function parseDate( date_str ) {
+        var us_re = /(\d+).(\d+).(\d+),?\s+(\d+).(\d+)(.(\d+))?/;
+        date_str = date_str.replace(/[\u200E\u200F]/g, '');
+        var date_a = us_re.exec( date_str );
+        return [].slice.call(us_re.exec( date_str ), 1)
+            .map( Math.floor );
     }
+    //https://github.com/mobz/get-timezone-offset
+    function diffMinutes( d1, d2 ) {
+        var day = d1[1] - d2[1];
+        var hour = d1[3] - d2[3];
+        var min = d1[4] - d2[4];
+        if( day > 15 ) day = -1;
+        if( day < -15 ) day = 1;
+        return 60 * ( 24 * day + hour ) + min;
+    }
+
+    var now = new Date();
+    var locale = 'en-us';
+    var format_options = {timeZone: 'UTC',
+                        hour12: false,
+                        year: 'numeric',
+                        month: 'numeric',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric'};
+
+    var utc_f = new Intl.DateTimeFormat(locale, format_options );
+    format_options.timeZone = tz_str;
+    var loc_f = new Intl.DateTimeFormat(locale, format_options );
+    return -1 * diffMinutes(
+        parseDate( utc_f.format( now )),
+        parseDate( loc_f.format( now ))) / 60;
 }
