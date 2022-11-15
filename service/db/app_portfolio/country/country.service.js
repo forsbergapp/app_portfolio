f4cded8dd640dceff6c18143cd0438a49ad3683d
appportfolio <forsbergapp@protonmail.com>
2022-11-15 22:20:59 +0100

    APB-300 consolidated lang_code search in SQL and created common function get_locale, bugfix getStatCountAdmin removed COALESCE in ORDER BY

--- service/db/app_portfolio/country/country.service.js
+++ service/db/app_portfolio/country/country.service.js
@@ -1,56 +1,34 @@
-const {execute_db_sql, get_schema_name} = require ("../../common/common.service");
+const {execute_db_sql, get_schema_name, get_locale} = require ("../../common/common.service");
 module.exports = {
         getCountries: (app_id, lang_code, callBack) => {
-                let sql;
-                let parameters;
-                if (process.env.SERVICE_DB_USE == 1) {
-                        sql = `SELECT c.id,
-                                      c.country_code,
-                                      c.flag_emoji,
-                                      ct.text,
-                                      cg.group_name
-                                 FROM ${get_schema_name()}.country c,
-                                      ${get_schema_name()}.country_group cg,
-                                      ${get_schema_name()}.country_translation ct
-                                WHERE ct.country_id = c.id
-                                  AND cg.id = c.country_group_id
-                                  AND ct.language_id IN (SELECT id 
-                                                           FROM ${get_schema_name()}.language l
-                                                          WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
-                                                                                 FROM ${get_schema_name()}.country_translation ct1,
-                                                                                      ${get_schema_name()}.language l1
-                                                                                WHERE ct1.country_id = ct.country_id
-                                                                                  AND l1.id = ct1.language_id
-                                                                                  AND l1.lang_code IN (:lang_code, SUBSTRING_INDEX(:lang_code,'-',2), SUBSTRING_INDEX(:lang_code,'-',1))
-                                                                              )
-                                                         )
-                                ORDER BY 5, 4`;
-                } else if (process.env.SERVICE_DB_USE == 2) {
-                        sql = `SELECT c.id "id",
-                                      c.country_code "country_code",
-                                      c.flag_emoji "flag_emoji",
-                                      ct.text "text",
-                                      cg.group_name "group_name"
-                                 FROM ${get_schema_name()}.country  c,
-                                      ${get_schema_name()}.country_group cg,
-                                      ${get_schema_name()}.country_translation ct
-                                WHERE ct.country_id = c.id
-                                  AND cg.id = c.country_group_id
-                                  AND ct.language_id IN (SELECT id 
-                                                           FROM ${get_schema_name()}.language l
-                                                          WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
-                                                                                 FROM ${get_schema_name()}.country_translation ct1,
-                                                                                      ${get_schema_name()}.language l1
-                                                                                WHERE ct1.country_id = ct.country_id
-                                                                                  AND l1.id = ct1.language_id
-                                                                                  AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-                                                                              )
-                                                        )
-                                ORDER BY 5, 4`;
-                }
+               let sql;
+               let parameters;
+               sql = `SELECT c.id "id",
+                              c.country_code "country_code",
+                              c.flag_emoji "flag_emoji",
+                              ct.text "text",
+                              cg.group_name "group_name"
+                         FROM ${get_schema_name()}.country  c,
+                              ${get_schema_name()}.country_group cg,
+                              ${get_schema_name()}.country_translation ct
+                        WHERE ct.country_id = c.id
+                          AND cg.id = c.country_group_id
+                          AND ct.language_id IN (SELECT id 
+                                                   FROM ${get_schema_name()}.language l
+                                                  WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+                                                                         FROM ${get_schema_name()}.country_translation ct1,
+                                                                              ${get_schema_name()}.language l1
+                                                                        WHERE ct1.country_id = ct.country_id
+                                                                          AND l1.id = ct1.language_id
+                                                                          AND l1.lang_code IN (:lang_code1, :lang_code2, :lang_code3)
+                                                                      )
+                                                 )
+                         ORDER BY 5, 4`;
                 parameters = {
-                        lang_code: lang_code
-                     };
+                              lang_code1: get_locale(lang_code, 1),
+                              lang_code2: get_locale(lang_code, 2),
+                              lang_code3: get_locale(lang_code, 3)
+                             };
                 execute_db_sql(app_id, sql, parameters, null, 
                                __appfilename, __appfunction, __appline, (err, result)=>{
 			if (err)
--- service/db/app_portfolio/setting/setting.service.js
+++ service/db/app_portfolio/setting/setting.service.js
@@ -1,69 +1,41 @@
-const {execute_db_sql, get_schema_name} = require ("../../common/common.service");
+const {execute_db_sql, get_schema_name, get_locale} = require ("../../common/common.service");
 module.exports = {
 	getSettings: (app_id, lang_code, setting_type_name, callBack) => {
     let sql;
     let parameters;
     if (typeof setting_type_name=='undefined' ||setting_type_name=='' ||setting_type_name==null)
           setting_type_name = null;
-    if (process.env.SERVICE_DB_USE == 1) {
-      sql = `SELECT st.setting_type_name,
-                    s.id,
-                    s.data,
-                    s.data2,
-                    s.data3,
-                    s.data4,
-                    s.data5,
-                    COALESCE(str.text, s.description) text
-               FROM ${get_schema_name()}.setting_type st,
-                    ${get_schema_name()}.setting s
-                    LEFT OUTER JOIN(SELECT str.setting_id,
-                                           str.text
-                                      FROM ${get_schema_name()}.setting_translation str,
-                                           ${get_schema_name()}.language l
-                                      WHERE l.id = str.language_id
-                                        AND l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
-                                                              FROM ${get_schema_name()}.setting_translation str1,
-                                                                   ${get_schema_name()}.language l1
-                                                            WHERE l1.id  = str1.language_id
-                                                              AND str1.setting_id = str.setting_id
-                                                              AND l1.lang_code IN (:lang_code, SUBSTRING_INDEX(:lang_code,'-',2), SUBSTRING_INDEX(:lang_code,'-',1))
-                                                           )
-                                     )  str
+     sql = `SELECT st.setting_type_name "setting_type_name",
+                   s.id "id",
+                   s.data "data",
+                   s.data2 "data2",
+                   s.data3 "data3",
+                   s.data4 "data4",
+                   s.data5 "data5",
+                   COALESCE(str.text, s.description) "text"
+             FROM ${get_schema_name()}.setting_type st,
+                  ${get_schema_name()}.setting s
+             LEFT OUTER JOIN(SELECT str.setting_id,
+                                    str.text
+                               FROM ${get_schema_name()}.setting_translation str,
+                                    ${get_schema_name()}.language l
+                              WHERE l.id = str.language_id
+                                AND l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+                                                     FROM ${get_schema_name()}.setting_translation str1,
+                                                          ${get_schema_name()}.language l1
+                                                    WHERE l1.id  = str1.language_id
+                                                      AND str1.setting_id = str.setting_id
+                                                      AND l1.lang_code IN (:lang_code1, :lang_code2, :lang_code3)
+                                                  )
+                         )  str
                     ON str.setting_id = s.id
-              WHERE st.setting_type_name = COALESCE(:setting_type_name, st.setting_type_name)
-                AND s.setting_type_id = st.id  
-            ORDER BY 1, 2 `;
-    }else if (process.env.SERVICE_DB_USE==2){
-      sql = `SELECT st.setting_type_name "setting_type_name",
-                    s.id "id",
-                    s.data "data",
-                    s.data2 "data2",
-                    s.data3 "data3",
-                    s.data4 "data4",
-                    s.data5 "data5",
-                    COALESCE(str.text, s.description) "text"
-               FROM ${get_schema_name()}.setting_type st,
-                    ${get_schema_name()}.setting s
-                    LEFT OUTER JOIN(SELECT str.setting_id,
-                                           str.text
-                                      FROM ${get_schema_name()}.setting_translation str,
-                                           ${get_schema_name()}.language l
-                                     WHERE l.id = str.language_id
-                                       AND l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
-                                                             FROM ${get_schema_name()}.setting_translation str1,
-                                                                  ${get_schema_name()}.language l1
-                                                            WHERE l1.id  = str1.language_id
-                                                              AND str1.setting_id = str.setting_id
-                                                              AND l1.lang_code IN (:lang_code, SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,2)-1), SUBSTR(:lang_code, 0,INSTR(:lang_code,'-',1,1)-1))
-                                                          )
-                                   )  str
-                  ON str.setting_id = s.id
-              WHERE st.setting_type_name = COALESCE(:setting_type_name, st.setting_type_name)
-                AND s.setting_type_id = st.id  
-              ORDER BY 1, 2`;
-     }
+            WHERE st.setting_type_name = COALESCE(:setting_type_name, st.setting_type_name)
+              AND s.setting_type_id = st.id  
+          ORDER BY 1, 2`;
      parameters = {
-                    lang_code: lang_code,
+                    lang_code1: get_locale(lang_code, 1),
+                    lang_code2: get_locale(lang_code, 2),
+                    lang_code3: get_locale(lang_code, 3),
                     setting_type_name: setting_type_name
                    };
      execute_db_sql(app_id, sql, parameters, null, 
