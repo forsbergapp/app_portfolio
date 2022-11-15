a1d5026330686fda230bf8a9182154d9116de136
appportfolio <app.portfolio@protonmail.com>
2022-11-15 01:24:35 +0100

    APB-300 mysql with connection syntax, configured parameters to use same bind variable names and consolidated all sql to one common sql with some exceptions for different SQL, all NVL replaced by COALESCE, replaced mariadb npm module with mysql

--- service/db/app_portfolio/app2_user_setting_view/app2_user_setting_view.service.js
+++ service/db/app_portfolio/app2_user_setting_view/app2_user_setting_view.service.js
@@ -1,49 +1,31 @@
-const {execute_db_sql} = require ("../../common/common.service");
+const {execute_db_sql, get_schema_name} = require ("../../common/common.service");
 module.exports = {
 	insertUserSettingView: (app_id, data, callBack) => {
 		let sql;
 		let parameters;
-		if (process.env.SERVICE_DB_USE == 1) {
-			sql = `INSERT INTO ${process.env.SERVICE_DB_DB1_NAME}.app2_user_setting_view(
-								user_account_id,
-								app2_user_setting_id,
-								client_ip,
-								client_user_agent,
-								client_longitude,
-								client_latitude,
-								date_created)
-					VALUES(?,?,?,?,?,?, CURRENT_TIMESTAMP) `;
-			parameters = [	data.user_account_id,
-							data.app2_user_setting_id,
-							data.client_ip,
-							data.client_user_agent,
-							data.client_longitude,
-							data.client_latitude];
-		}else if (process.env.SERVICE_DB_USE==2){
-			sql = `INSERT INTO ${process.env.SERVICE_DB_DB2_NAME}.app2_user_setting_view(
-								user_account_id,
-								app2_user_setting_id,
-								client_ip,
-								client_user_agent,
-								client_longitude, 
-								client_latitude, 
-								date_created)
-					VALUES( :user_account_id,
-							:app2_user_setting_id,
-							:client_ip,
-							:client_user_agent,
-							:client_longitude,
-							:client_latitude,
-							CURRENT_TIMESTAMP) `;
-			parameters = {
-							user_account_id: data.user_account_id,
-							app2_user_setting_id: data.app2_user_setting_id,
-							client_ip: data.client_ip,
-							client_user_agent: data.client_user_agent,
-							client_longitude: data.client_longitude,
-							client_latitude: data.client_latitude
-						 };
-		}
+		sql = `INSERT INTO ${get_schema_name()}.app2_user_setting_view(
+							user_account_id,
+							app2_user_setting_id,
+							client_ip,
+							client_user_agent,
+							client_longitude, 
+							client_latitude, 
+							date_created)
+				VALUES( :user_account_id,
+						:app2_user_setting_id,
+						:client_ip,
+						:client_user_agent,
+						:client_longitude,
+						:client_latitude,
+						CURRENT_TIMESTAMP) `;
+		parameters = {
+						user_account_id: data.user_account_id,
+						app2_user_setting_id: data.app2_user_setting_id,
+						client_ip: data.client_ip,
+						client_user_agent: data.client_user_agent,
+						client_longitude: data.client_longitude,
+						client_latitude: data.client_latitude
+						};
 		execute_db_sql(app_id, sql, parameters, null, 
 			           __appfilename, __appfunction, __appline, (err, result)=>{
 			if (err)
--- service/db/app_portfolio/user_account_view/user_account_view.service.js
+++ service/db/app_portfolio/user_account_view/user_account_view.service.js
@@ -1,33 +1,19 @@
-const {execute_db_sql} = require ("../../common/common.service");
+const {execute_db_sql, get_schema_name} = require ("../../common/common.service");
 module.exports = {
 	insertUserAccountView: (app_id, data, callBack) => {
 		let sql;
 		let parameters;
-		if (process.env.SERVICE_DB_USE == 1) {
-			sql = `INSERT INTO ${process.env.SERVICE_DB_DB1_NAME}.user_account_view(
-								user_account_id, user_account_id_view, client_ip, client_user_agent, client_longitude, client_latitude, date_created)
-					VALUES(?,?,?,?,?,?, CURRENT_TIMESTAMP) `;
-			parameters = [
-							data.user_account_id,
-							data.user_account_id_view,
-							data.client_ip,
-							data.client_user_agent,
-							data.client_longitude,
-							data.client_latitude
-						 ];
-		}else if (process.env.SERVICE_DB_USE==2){
-			sql = `INSERT INTO ${process.env.SERVICE_DB_DB2_NAME}.user_account_view(
-								user_account_id, user_account_id_view, client_ip, client_user_agent, client_longitude, client_latitude, date_created)
-					VALUES(:user_account_id,:user_account_id_view,:client_ip,:client_user_agent,:client_longitude,:client_latitude, CURRENT_TIMESTAMP) `;
-			parameters = {
-							user_account_id: data.user_account_id,
-							user_account_id_view: data.user_account_id_view,
-							client_ip: data.client_ip,
-							client_user_agent: data.client_user_agent,
-							client_longitude: data.client_longitude,
-							client_latitude: data.client_latitude
-						};
-		}
+		sql = `INSERT INTO ${get_schema_name()}.user_account_view(
+							user_account_id, user_account_id_view, client_ip, client_user_agent, client_longitude, client_latitude, date_created)
+				VALUES(:user_account_id,:user_account_id_view,:client_ip,:client_user_agent,:client_longitude,:client_latitude, CURRENT_TIMESTAMP) `;
+		parameters = {
+						user_account_id: data.user_account_id,
+						user_account_id_view: data.user_account_id_view,
+						client_ip: data.client_ip,
+						client_user_agent: data.client_user_agent,
+						client_longitude: data.client_longitude,
+						client_latitude: data.client_latitude
+					};
 		execute_db_sql(app_id, sql, parameters, null, 
 			           __appfilename, __appfunction, __appline, (err, result)=>{
 			if (err)
