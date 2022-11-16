8e265d3e53f490ebbec2eabc187360c702f12285
fapwork <fapwork@proton.me>
2022-11-16 11:41:38 +0100

    APB-300 consolidated SQL, removed CONVERT USING UTF8, LIMIT sql, SELECT * FROM, user event number of days check is done now in javascript, bugfix activateuser

--- service/db/app_portfolio/app2_theme/app2_theme.service.js
+++ service/db/app_portfolio/app2_theme/app2_theme.service.js
@@ -3,51 +3,27 @@ module.exports = {
 	getThemes: (app_id,callBack) => {
 		let sql;
 		let parameters;
-		if (process.env.SERVICE_DB_USE == 1) {
-			sql = `SELECT t.id,
-						  t.title,
-						  t.author,
-						  t.author_url,
-						  CONVERT(t.image_preview USING UTF8) image_preview,
-						  t.image_preview_url,
-						  CONVERT(t.image_background USING UTF8) image_background,
-						  t.image_background_url,
-						  CONVERT(t.image_header USING UTF8) image_header,
-						  t.image_header_url,
-						  CONVERT(t.image_footer USING UTF8) image_footer,
-						  t.image_footer_url,
-						  t.premium,
-						  tc.title category,
-						  tt.title type
-					 FROM ${get_schema_name()}.app2_theme t,
-						  ${get_schema_name()}.app2_theme_category tc,
-						  ${get_schema_name()}.app2_theme_type tt
-					WHERE tc.id = t.app2_theme_category_id
-					  AND tt.id = t.app2_theme_type_id 
-					ORDER BY tt.title, t.id`;
-		}else if (process.env.SERVICE_DB_USE==2){
-			sql = `SELECT t.id "id",
-						  t.title "title",
-						  t.author "author",
-						  t.author_url "author_url",
-						  t.image_preview "image_preview",
-						  t.image_preview_url "image_preview_url",
-						  t.image_background "image_background",
-						  t.image_background_url "image_background_url",
-						  t.image_header "image_header",
-						  t.image_header_url "image_header_url",
-						  t.image_footer "image_footer",
-						  t.image_footer_url "image_footer_url",
-						  t.premium "premium",
-						  tc.title "category",
-						  tt.title "type"
-					 FROM ${get_schema_name()}.app2_theme t,
-						  ${get_schema_name()}.app2_theme_category tc,
-						  ${get_schema_name()}.app2_theme_type tt
-					WHERE tc.id = t.app2_theme_category_id
-					  AND tt.id = t.app2_theme_type_id
-					ORDER BY tt.title, t.id`;
-		}
+		sql = `SELECT t.id "id",
+						t.title "title",
+						t.author "author",
+						t.author_url "author_url",
+						t.image_preview "image_preview",
+						t.image_preview_url "image_preview_url",
+						t.image_background "image_background",
+						t.image_background_url "image_background_url",
+						t.image_header "image_header",
+						t.image_header_url "image_header_url",
+						t.image_footer "image_footer",
+						t.image_footer_url "image_footer_url",
+						t.premium "premium",
+						tc.title "category",
+						tt.title "type"
+					FROM ${get_schema_name()}.app2_theme t,
+						${get_schema_name()}.app2_theme_category tc,
+						${get_schema_name()}.app2_theme_type tt
+				WHERE tc.id = t.app2_theme_category_id
+					AND tt.id = t.app2_theme_type_id
+				ORDER BY tt.title, t.id`;
 		parameters = {};
 		execute_db_sql(app_id, sql, parameters, null, 
 			           __appfilename, __appfunction, __appline, (err, result)=>{
