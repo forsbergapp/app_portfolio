1a567909db23e23ff85757bf6c790772af23d4e3
appportfolio <app.portfolio@protonmail.com>
2022-11-28 15:08:52 +0100

    APB-296 renamed scripts and added database uninstall scripts, updated README with support for 4 databases and unicode settings when running language scripts, updated .gitignore

---
+++ scripts/7.db_2_uninstall.sql
@@ -0,0 +1,10 @@
+drop user app_portfolio cascade;
+drop user app1;
+drop user app2;
+drop user app3;
+drop user app_admin;
+drop role role_app1;
+drop role role_app2;
+drop role role_app3;
+drop role role_app_admin;
+drop role role_app_dba;
