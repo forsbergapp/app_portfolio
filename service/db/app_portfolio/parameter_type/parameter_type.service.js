aa169a9724aaeb0cd590e5311065c730ed86af9d
appportfolio <forsbergapp@protonmail.com>
2022-11-29 20:21:16 +0100

    APB-270 reads parameter_type_translation text in admin and in lov, app_category sql optimize query

--- service/db/app_portfolio/parameter_type/parameter_type.service.js
+++ service/db/app_portfolio/parameter_type/parameter_type.service.js
@@ -1,14 +1,30 @@
-const {execute_db_sql, get_schema_name} = require ("../../common/common.service");
+const {execute_db_sql, get_schema_name, get_locale} = require ("../../common/common.service");
 module.exports = {
-	getParameterType: (app_id, id, callBack) => {
+	getParameterType: (app_id, id, lang_code, callBack) => {
 		let sql;
     	let parameters;
-		sql = `SELECT id "id",
-					  parameter_type_name "parameter_type_name"
-				 FROM ${get_schema_name()}.parameter_type
+		sql = `SELECT pt.id "id",
+					  pt.parameter_type_name "parameter_type_name",
+					  ptt.text "parameter_type_text"
+				 FROM ${get_schema_name()}.parameter_type pt
+				 LEFT OUTER JOIN ${get_schema_name()}.parameter_type_translation ptt
+				   ON ptt.parameter_type_id = pt.id
+				  AND ptt.language_id IN (SELECT id 
+											FROM ${get_schema_name()}.language l
+										   WHERE l.lang_code = (SELECT COALESCE(MAX(l1.lang_code),'en')
+																  FROM ${get_schema_name()}.parameter_type_translation ptt1,
+																	   ${get_schema_name()}.language l1
+																 WHERE l1.id  = ptt1.language_id
+																   AND ptt1.parameter_type_id  = pt.id
+																   AND l1.lang_code IN (:lang_code1, :lang_code2, :lang_code3)
+																 )
+											)
 				WHERE id = COALESCE(:id, id)
 				ORDER BY 1`;
-		parameters = {id: id};
+		parameters = {lang_code1: get_locale(lang_code, 1),
+					  lang_code2: get_locale(lang_code, 2),
+					  lang_code3: get_locale(lang_code, 3),
+					  id: id};
 		execute_db_sql(app_id, sql, parameters, true, 
 			           __appfilename, __appfunction, __appline, (err, result)=>{
 			if (err)
