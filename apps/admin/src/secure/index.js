e36c21bc950033d9af658740eecc29fa5980488f
fapwork <fapwork@proton.me>
2022-12-06 02:16:21 +0100

    APB-285 get_module_with_init added system_admin parameter to control login as system admin or user from db, menu checks window.global_system_admin, moved menu contents, not showing database for system_admin_only==1 since no db available, menu_secure and admin_secure now loaded when logged in, renamed admin src files, showing user acccount and user preferences part 1

--- apps/admin/src/secure/index.js
+++ apps/admin/src/secure/index.js
@@ -1,19 +1,21 @@
 const fs = require("fs");
 const { read_app_files, get_module_with_init } = require("../../../");
 module.exports = {
-    getAdminSecure:(app_id, user_account_id, gps_lat, gps_long, gps_place) => {    
+    getAdminSecure:(app_id, system_admin, user_account_id, gps_lat, gps_long, gps_place) => {    
         return new Promise(function (resolve, reject){
             const files = [
                 ['APP', __dirname + '/index.html'],
                 ['<AppHeadJS/>', __dirname + '/admin.js'],
                 ['<AppHeadCSS/>', __dirname + '/admin.css'],
-                ['<AppDashboard/>', __dirname + '/dashboard.html']
+                ['<AppAdminMainHTML/>', __dirname + '/main.html'],
+                ['<AppAdminDialogueHTML/>', __dirname + '/dialogue.html']
             ];
             read_app_files('', files, (err, app)=>{
                 if (err)
                     reject(err);
                 else{
                     get_module_with_init(app_id, 
+                                         system_admin,
                                          user_account_id,
                                         'admin_logoff_app',
                                         true, //close eventsource and create new as logged in
