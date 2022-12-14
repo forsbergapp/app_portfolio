77da65690a7e88df7d54b55d05f9456ba9ce1be1
appportfolio <app.portfolio@protonmail.com>
2022-12-14 02:22:11 +0100

    APB-115 implemented system admin and admin login part 1, system admin using old admin token and uses system admin functions, new env: SERVER_DB_START, SERVER_APP_START, SERVICE_AUTH_ENABLE_DBLOG, SERVER_SERVICE_GEOLOCATION_URL_GPS_IP, SERVER_SERVICE_GEOLOCATION_URL_GPS_PLACE

--- service/log/log.router.js
+++ service/log/log.router.js
@@ -1,8 +1,8 @@
 const { getParameters, getLogs, getFiles, getPM2Logs} = require ("./log.controller");
 const router = require("express").Router();
-const { checkAccessTokenAdmin} = require ("../auth/auth.controller");
-router.get("/parameters", checkAccessTokenAdmin, getParameters);
-router.get("/logs", checkAccessTokenAdmin, getLogs);
-router.get("/files", checkAccessTokenAdmin, getFiles);
-router.get("/pm2logs", checkAccessTokenAdmin, getPM2Logs);
+const { checkAdmin} = require ("../auth/admin/admin.controller");
+router.get("/parameters", checkAdmin, getParameters);
+router.get("/logs", checkAdmin, getLogs);
+router.get("/files", checkAdmin, getFiles);
+router.get("/pm2logs", checkAdmin, getPM2Logs);
 module.exports = router;
