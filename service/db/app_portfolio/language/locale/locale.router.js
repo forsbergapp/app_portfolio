e9a192b12f3005c521a82d5ae535e370de4d1590
fapwork <fapwork@proton.me>
2022-12-17 18:11:35 +0100

    APB-115 rewritten log functions to use promise, created setLogVariables, createLogServerI (with statusMessage) and createLogServerE, serverlog before access control and check_request,  check_request sets statuscode, createLogServerI res.on with 'finish' event called to get correct statuscode, verbose logic moved to log service

--- service/db/app_portfolio/language/locale/locale.router.js
+++ service/db/app_portfolio/language/locale/locale.router.js
@@ -3,8 +3,9 @@ const { getLocales } = require ("./locale.controller");
 const { checkDataToken } = require("../../../../auth/auth.controller");
 const { createLogAppRI } = require("../../../../log/log.controller");
 router.use((req,res,next)=>{
-    createLogAppRI(req, res, __appfilename, __appfunction, __appline, req.body);
-    next();
+    createLogAppRI(req, res, __appfilename, __appfunction, __appline, req.body).then(function(){
+		next();
+	})
 })
 router.get("/:lang_code", checkDataToken, getLocales);
 router.get("/admin/:lang_code", checkDataToken, getLocales);
