8ceb9530e5e800c2edb512c0559d32754334297f
appportfolio <app.portfolio@protonmail.com>
2022-12-18 01:33:39 +0100

    APB-115 added dashboard to system admin part 1, admin can only send INFO and CHAT broadcast from DASHBOARD, system admin can send MAINTENANCE too and switch to maintenance, ClientCheckMaintenance saves interval in client array record

--- service/broadcast/broadcast.router.js
+++ service/broadcast/broadcast.router.js
@@ -1,4 +1,4 @@
-const { BroadcastConnect, BroadcastSend, ConnectedList, ConnectedListSystemAdmin, ConnectedCount, ConnectedUpdate, ConnectedCheck} = require ("./broadcast.controller");
+const { BroadcastConnect, BroadcastSendSystemAdmin, BroadcastSendAdmin, ConnectedList, ConnectedListSystemAdmin, ConnectedCount, ConnectedUpdate, ConnectedCheck} = require ("./broadcast.controller");
 const { checkAccessTokenAdmin} = require ("../auth/auth.controller");
 const { checkAdmin} = require ("../auth/admin/admin.controller");
 const { checkDataToken } = require("../auth/auth.controller");
@@ -6,9 +6,9 @@ const router = require("express").Router();
 
 router.get("/SystemAdmin/connected", checkAdmin, ConnectedListSystemAdmin);
 router.patch("/SystemAdmin/update_connected", checkAdmin, ConnectedUpdate);
-router.post("/SystemAdmin/Send",checkAdmin, BroadcastSend);
+router.post("/SystemAdmin/Send",checkAdmin, BroadcastSendSystemAdmin);
 
-router.post("/Admin/Send",checkAccessTokenAdmin, BroadcastSend);
+router.post("/Admin/Send",checkAccessTokenAdmin, BroadcastSendAdmin);
 router.get("/Admin/connected", checkAccessTokenAdmin, ConnectedList);
 router.get("/Admin/connected/count", checkAccessTokenAdmin, ConnectedCount);
 
