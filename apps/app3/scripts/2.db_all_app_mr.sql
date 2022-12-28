cf78c99df3463f2bc3a4c9f4038d44f804df4f46
appportfolio <forsbergapp@protonmail.com>
2022-12-28 15:51:30 +0100

    APB-327 moved app data in 4.db_all_insert_data.sql =>apps/app[app_id]/scripts/1.db_all_app.sql and in 5.db_all*.sql => apps/app[app_id]/scripts/2.db_all_app*.sql, updated installation doc

---
+++ apps/app3/scripts/2.db_all_app_mr.sql
@@ -0,0 +1,4 @@
+--
+-- app_object_translation
+--
+INSERT INTO app_portfolio.app_object_translation (app_object_app_id, app_object_object_name, language_id, text) VALUES (3,'APP_DESCRIPTION',338,'कागदपत्रांचे सादरीकरण');
