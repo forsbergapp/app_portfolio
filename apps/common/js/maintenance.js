7d6d07c753cc419e1435de087fb88d6685077856
fapwork <fapwork@proton.me>
2023-03-13 17:52:51 +0100

    APB-370 added prefix common on all items in common. Reorganized css by declaring sections in same order for all apps, bugfix icon user_edit dialogue, removed unused create_map_popup_text() in app2, moved profile info cloud html in app1 to common.

--- apps/common/js/maintenance.js
+++ apps/common/js/maintenance.js
@@ -4,9 +4,9 @@ function app_exception(){
 }
 function init(parameters){
     document.title = common.ICONS['app_maintenance'];
-    document.getElementById('broadcast_close').innerHTML = common.ICONS['app_broadcast_close'];
-    document.getElementById('maintenance_footer').innerHTML= common.APP_SPINNER;
-    document.getElementById('maintenance_message').innerHTML = common.ICONS['app_maintenance'];;
+    document.getElementById('common_broadcast_close').innerHTML = common.ICONS['app_broadcast_close'];
+    document.getElementById('common_maintenance_footer').innerHTML= common.APP_SPINNER;
+    document.getElementById('common_maintenance_message').innerHTML = common.ICONS['app_maintenance'];;
     
     common.COMMON_GLOBAL['app_id'] = parameters.app_id;
     common.COMMON_GLOBAL['user_account_id'] = '';
