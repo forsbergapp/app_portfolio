f7702b0d424cdff6264e328f941790856d73feba
appportfolio <forsbergapp@protonmail.com>
2023-03-14 16:10:53 +0100

    APB-363 removed createLog from router and controller not used, removed createLogAdmin from controller not used. Moved control of ENABLE_DBLOG to service.

--- service/db/app_portfolio/app_log/app_log.router.js
+++ service/db/app_portfolio/app_log/app_log.router.js
@@ -1,5 +1,4 @@
-const { getLogsAdmin,createLog, getStatUniqueVisitorAdmin} = await import("./app_log.controller.js");
-const { checkDataToken } = await import(`file://${process.cwd()}/service/auth/auth.controller.js`);
+const { getLogsAdmin, getStatUniqueVisitorAdmin} = await import("./app_log.controller.js");
 const { createLogAppRI } = await import(`file://${process.cwd()}/service/log/log.controller.js`);
 const { checkAccessTokenAdmin} = await import(`file://${process.cwd()}/service/auth/auth.controller.js`);
 const {Router} = await import('express');
@@ -13,6 +12,5 @@ router.use((req,res,next)=>{
 	})
 })
 router.get("/admin",  checkAccessTokenAdmin, getLogsAdmin);
-router.post("/", checkDataToken, createLog);
 router.get("/admin/stat/uniquevisitor", checkAccessTokenAdmin, getStatUniqueVisitorAdmin);
 export{router};
