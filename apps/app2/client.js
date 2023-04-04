bf64686916c1e05eef2eb22d2bf103f27867ab10
appportfolio <app.portfolio@protonmail.com>
2023-04-04 21:57:35 +0200

    APB-384 reads only hexvalues used as icons and declares font from Fontawesome module and icon class in css, removes loading complete Fontawesome css, fixes icons app_align_right wrong character and app_log syntax error, fixes css selector for icons in menu 8 and font height in menu 10 in admin app, fixes stat row 2 width css in app2 and stat row 1 width in common css, fixes icon sizes app2 settings image

--- apps/app2/client.js
+++ apps/app2/client.js
@@ -143,7 +143,6 @@ function getApp(app_id, username, gps_lat, gps_long, gps_place){
                 ['APP', process.cwd() + '/apps/app2/src/index.html'],
                 ['<AppCommonHead/>', process.cwd() + '/apps/common/src/head.html'],
                 ['<AppCommonHeadMap/>', process.cwd() + '/apps/common/src/head_map.html'],
-                ['<AppCommonHeadFontawesome/>', process.cwd() + '/apps/common/src/head_fontawesome.html'],
                 ['<AppCommonHeadFonts/>', process.cwd() + '/apps/common/src/head_fonts.html'],
                 ['<AppCommonBody/>', process.cwd() + '/apps/common/src/body.html'],
                 ['<AppCommonBodyMaintenance/>', process.cwd() + '/apps/common/src/body_maintenance.html'],
