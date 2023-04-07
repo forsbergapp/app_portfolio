33871a9795a63cf2d22e5878845a4c03d2bff4a7
appportfolio <forsbergapp@protonmail.com>
2023-04-07 19:14:04 +0200

    APB-428 changes order of css, font css and javascript in head of app html to this order: 1.app js, 2.app css, 3.common css, 4. common js, app 1: will now display css animation at startup and less block render at startup, changes responsive sizes for start dialogues to be the same and places buttons on top and changes close icon to home icon. Moves dialogue start position up 1%. Sets classes on font links in common with rel disabled value that set rel=stylesheet when app.init() finished. Removes background color on dialogue_loading. Disables css transition in css and enables them at startup to avoid startup render issues. Removes DOMContentLoaded for startup javascript not needed.

--- apps/app1/client.js
+++ apps/app1/client.js
@@ -6,7 +6,7 @@ function getApp(app_id, username, gps_lat, gps_long, gps_place){
             const files = [
                 ['APP', process.cwd() + '/apps/app1/src/index.html'],
                 ['<AppCommonHead/>', process.cwd() + '/apps/common/src/head.html'],
-                ['<AppCommonHeadFonts/>', process.cwd() + '/apps/common/src/head_fonts.html'],
+                ['<AppCommonFonts/>', process.cwd() + '/apps/common/src/fonts.html'],
                 ['<AppCommonBody/>', process.cwd() + '/apps/common/src/body.html'],
                 ['<AppCommonBodyMaintenance/>', process.cwd() + '/apps/common/src/body_maintenance.html'],
                 ['<AppCommonBodyBroadcast/>', process.cwd() + '/apps/common/src/body_broadcast.html'],
