fb2fac795fd5feb77e8b23b160ad29949ee48a3f
fapwork <fapwork@proton.me>
2023-04-15 21:40:16 +0200

    APB-455 removes localstorage usage in app2 used to display scan mobile message

--- apps/app2/js/app.js
+++ apps/app2/js/app.js
@@ -689,12 +689,8 @@ const show_dialogue = (dialogue, file = '') => {
                 if (common.mobile())
                     return null;
                 else{
-                    //show once and store variable in localstorage
-                    if (!localStorage.scan_open_mobile) {
-                        localStorage.setItem('scan_open_mobile', true);
-                        document.getElementById('dialogue_scan_open_mobile').style.visibility = 'visible';
-                        common.create_qr('scan_open_mobile_qrcode', common.getHostname());
-                    };
+                    document.getElementById('dialogue_scan_open_mobile').style.visibility = 'visible';
+                    common.create_qr('scan_open_mobile_qrcode', common.getHostname());
                 }
                 break;
             }
