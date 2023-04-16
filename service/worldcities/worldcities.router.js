1f33464d0b761bcad16cde4062fb5b21ef9f8dcb
appportfolio <forsbergapp@protonmail.com>
2023-04-16 02:03:21 +0200

    APB-454 consolidates routers to one file, log getParameters renamed to getLogParameters, corrects missing logging for worldcities router, moves app2 routes to app2 server.js, and moves rest api app2 file directories to /apps/app2/service/db keeping endpoints as before, fixes a space in name theme class switching theme in app1

--- service/worldcities/worldcities.router.js
+++
@@ -1,6 +0,0 @@
-const { getCities} = await import('./worldcities.controller.js');
-const { checkDataToken } = await import(`file://${process.cwd()}/service/auth/auth.controller.js`);
-const {Router} = await import('express');
-const router = Router();
-router.get("/:country", checkDataToken, getCities);
-export {router};
