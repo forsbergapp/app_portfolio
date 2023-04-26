9988c665ebc0805c727a723c910f236d8a8826d1
appportfolio <app.portfolio@protonmail.com>
2023-04-26 20:22:10 +0200

    APB-480 adds random social records when installing demo data from admin, selects 50% sample of demo users, all records are created one by one, uninstall demo data fixes async issues, removes old demo scripts with bulk inserts, updates docs and renames install and uninstall scripts

--- service/db/admin/admin.controller.js
+++ service/db/admin/admin.controller.js
@@ -96,7 +96,6 @@ const demo_add = async (req, res)=> {
 		let records_user_account_app = 0;
 		let records_user_account_app_setting = 0;
 		let password_encrypted = hashSync(req.body.demo_password, genSaltSync(10));
-		//loop demo users and create them and update with id
 		const create_users = async (demo_user) =>{
 			return await new Promise((resolve, reject)=>{
 				const create_update_id = (demo_user)=>{
@@ -162,12 +161,13 @@ const demo_add = async (req, res)=> {
 				  })
 			});
 		}
+		//create all users first and update with id
 		let result_create_user = await create_users();
+		//create user settings
 		for (let demo_user of demo_users){
 			for (let i = 0; i < demo_user.settings.length; i++){
 				let user_setting_app_id = demo_user.settings[i].app_id;
 				let result_createUserAccountApp = await create_user_account_app(user_setting_app_id, demo_user.id);
-				delete demo_user.settings[i].app_id;
 				let settings_header_image;
 				//use file in settings or if missing then use filename same as demo username
 				if (demo_user.settings[i].image_header_image_img)
@@ -184,110 +184,191 @@ const demo_add = async (req, res)=> {
 				//month 20001-20022
 				demo_user.settings[i].design_theme_month_id = Math.floor(20001 + Math.random() * 22);
 				demo_user.settings[i].design_theme_year_id = 30001;
+				let settings_no_app_id = JSON.parse(JSON.stringify(demo_user.settings[i]));
+				delete settings_no_app_id.app_id;
 				let json_data_user_setting = `{
 					"description": "${demo_user.settings[i].description}",
-					"settings_json": ${JSON.stringify(demo_user.settings[i])},
+					"settings_json": ${JSON.stringify(settings_no_app_id)},
 					"user_account_id": ${demo_user.id}
 					}`;	
 				let result_createUserSetting = await create_setting(user_setting_app_id, JSON.parse(json_data_user_setting));
-				/* to be implemented and logic reviewed first:
-				let social_types = ['LIKE', 'VIEW', 'VIEW_ANONYMOUS', 'FOLLOWER', 'SETTINGS_LIKE', 'SETTINGS_VIEW', 'SETTINGS_VIEW_ANONYMOUS'];
-				social_types.ForEach(social_type => {
-					//select new random sample for each social type
-					let random_users1 = [];
-					let random_users2 = [];
-					//loop until two groups both have 20 samples with unique users in each sample
-					while (random_users1.length <= 20 && random_users2.length <= 20){
-						let random_array_index1 = Math.floor(1 + Math.random() * insert_record_user.length - 1 )
-						let random_array_index2 = Math.floor(1 + Math.random() * insert_record_user.length - 1 )
-						if (random_users1.length <20 && !random_users1.includes(random_array_index1) )
-							random_users1.push(insert_record_user[random_array_index1])
-						if (random_users2.length <20 && !random_users2.includes(random_array_index2))
-							random_users2.push(insert_record_user[random_array_index2])
+			}
+		}
+		let records_user_account_like = 0;
+		let records_user_account_view = 0;
+		let records_user_account_follow = 0;
+		let records_user_account_setting_like = 0;
+		let records_user_account_setting_view = 0;
+		//create social records
+		let social_types = ['LIKE', 'VIEW', 'VIEW_ANONYMOUS', 'FOLLOWER', 'SETTINGS_LIKE', 'SETTINGS_VIEW', 'SETTINGS_VIEW_ANONYMOUS'];
+		const create_likeuser = async (app_id, id, id_like ) =>{
+			return new Promise((resolve, reject) => {
+				likeUser(app_id, id, id_like, (err,results) => {
+					if (err)
+						reject(err);
+					else{
+						if (results.affectedRows == 1)
+							records_user_account_like++;
+						resolve(results);
 					}
-					const check_finished = () =>{
-						if (total_records==social_type.length * random_users1.length * random_users1.length)
-							return res.status(200).json({
-								count_created: total_records
-							});
+				})
+			})
+		}
+		const create_user_account_view = async (app_id, json_data ) =>{
+			return new Promise((resolve, reject) => {
+				insertUserAccountView(app_id, json_data, (err,results) => {
+					if (err)
+						reject(err);
+					else{
+						if (results.affectedRows == 1)
+							records_user_account_view++;
+						resolve(results);
 					}
-					random_users1.foreach(user1 =>{
-						random_users2.foreach(user2 => {
-							if (social_type == 'LIKE')
-								likeUser(req.query.app_id, user1, user2, (err,results_likeUser) => {
-									total_records++;
-									check_finished();
-								})
-							if (social_type == 'VIEW'){
-								insertUserAccountView(req.query.app_id,JSON.parse(
-																			`{"user_account_id": ${user1},
-																			"user_account_id_view": ${user2},
-																			"client_ip": null,
-																			"client_user_agent": null,
-																			"client_longitude": null,
-																			"client_latitude": null
-																		}`), (err,results_insertUserAccountView) => {
-									total_records++;
-									check_finished();
-								})
-							}
-							if (social_type == 'VIEW_ANONYMOUS')
-							insertUserAccountView(req.query.app_id, JSON.parse(
-																		`{ "user_account_id": ${user1},
-																		"user_account_id_view": null,
-																		"client_ip": null,
-																		"client_user_agent": null,
-																		"client_longitude": null,
-																		"client_latitude": null
-																		}`), (err,results_insertUserAccountView) => {
-								total_records++;
-								check_finished();
-							})
-
-							if (social_type == 'FOLLOWER')
-								followUser(req.query.app_id, user1, user2, (err,results_followUser) => {
-									total_records++;
-									check_finished();
-								})
-							if (social_type == 'SETTINGS_LIKE')
-								getUserSettingsByUserId(req.query.app_id, user1, (err,results_settings) => {
-									likeUserSetting(req.query.app_id, user2, results_settings.id, (err,results_likeUserSetting) => {
-										total_records++;
-										check_finished();
-									})
-								})
-							if (social_type == 'SETTINGS_VIEW' || social_type == 'SETTINGS_VIEW_ANONYMOUS'){
-								getUserSettingsByUserId(req.query.app_id, user1, (err,results_getUserSettingsByUserId) => {
-									//choose random setting from user
-									let random_index = Math.floor(1 + Math.random() * results_getUserSettingsByUserId.length )
-									let settings_user_id;
-									if (social_type == 'SETTINGS_VIEW')
-										settings_user_id = user2;
-									else
-										user_account_id = 'null';
-									insertUserSettingView(req.query.app_id, JSON.parse(
-																			'{  "user_account_id":' + user_account_id + ',' + 
-																			`	"user_setting_id": ${results_getUserSettingsByUserId[random_index].id},
-																				"client_ip": null,
-																				"client_user_agent": null,
-																				"client_longitude": null,
-																				"client_latitude": null
-																				}`), (err,results_insertUserSettingView) => {
-										total_records++;
-										check_finished();
-									})
-								})
+				})
+			})
+		}
+		const create_user_account_follow = async (app_id, id, id_follow ) =>{
+			return new Promise((resolve, reject) => {
+				followUser(app_id, id, id_follow, (err,results) => {
+					if (err)
+						reject(err);
+					else{
+						if (results.affectedRows == 1)
+							records_user_account_follow++;
+						resolve(results);
+					}
+				})
+			})
+		}
+		const create_user_account_app_setting_like = async (app_id, user1, user2 ) =>{
+			return new Promise((resolve, reject) => {
+				getUserSettingsByUserId(app_id, user1, (err,results_settings) => {
+					if (err)
+						reject(err);
+					else{
+						let random_settings_index = Math.floor(1 + Math.random() * results_settings.length - 1 )
+						likeUserSetting(app_id, user2, results_settings[random_settings_index].id, (err,results) => {
+							if (err)
+								reject(err);
+							else{
+								if (results.affectedRows == 1)
+									records_user_account_setting_like++;
+								resolve(results);
 							}
 						})
-					})
+					}
 				})
-				*/
+			})
+		}
+		const create_user_account_app_setting_view = async (app_id, user1, user2 , social_type) =>{
+			return new Promise((resolve, reject) => {
+				getUserSettingsByUserId(app_id, user1, (err,results_settings) => {
+					if (err)
+						reject(err);
+					else{
+						//choose random setting from user
+						let random_index = Math.floor(1 + Math.random() * results_settings.length -1)
+						let user_account_id;
+						if (social_type == 'SETTINGS_VIEW')
+							user_account_id = user2;
+						else
+							user_account_id = 'null';
+						insertUserSettingView(app_id, JSON.parse(
+														'{  "user_account_id":' + user_account_id + ',' + 
+														`	"user_setting_id": ${results_settings[random_index].id},
+															"client_ip": null,
+															"client_user_agent": null,
+															"client_longitude": null,
+															"client_latitude": null
+															}`), (err,results) => {
+							if (err)
+								reject(err);
+							else{
+								if (results.affectedRows == 1)
+									records_user_account_setting_view++;
+								resolve(results);
+							}
+						})
+					}
+				})
+			})
+		}
+		for (let social_type of social_types){
+			//select new random sample for each social type
+			let random_users1 = [];
+			let random_users2 = [];
+			//loop until two groups both have 50% samples with unique users in each sample
+			let sample_amount = Math.floor(demo_users.length * 0.5);
+			while (random_users1.length < sample_amount || random_users2.length < sample_amount){
+				let random_array_index1 = Math.floor(1 + Math.random() * demo_users.length - 1 )
+				let random_array_index2 = Math.floor(1 + Math.random() * demo_users.length - 1 )
+				if (random_users1.length <sample_amount && !random_users1.includes(demo_users[random_array_index1].id) )
+					random_users1.push(demo_users[random_array_index1].id)
+				if (random_users2.length <sample_amount && !random_users2.includes(demo_users[random_array_index2].id))
+					random_users2.push(demo_users[random_array_index2].id)
+			}
+			let result_insert;
+			for (let user1 of random_users1){
+				for(let user2 of random_users2){
+					switch (social_type){
+						case 'LIKE':{
+							result_insert = await create_likeuser(req.query.app_id, user1, user2);
+							break;
+						}
+						case 'VIEW':{
+							result_insert = await create_user_account_view(req.query.app_id,JSON.parse(
+														`{  "user_account_id": ${user1},
+															"user_account_id_view": ${user2},
+															"client_ip": null,
+															"client_user_agent": null,
+															"client_longitude": null,
+															"client_latitude": null
+														}`));
+							break;
+						}
+						case 'VIEW_ANONYMOUS':{
+							result_insert = await create_user_account_view(req.query.app_id, JSON.parse(
+														`{  "user_account_id": null,
+															"user_account_id_view": ${user1},
+															"client_ip": null,
+															"client_user_agent": null,
+															"client_longitude": null,
+															"client_latitude": null
+														}`));
+							break;
+						}
+						case 'FOLLOWER':{
+							result_insert = await create_user_account_follow(req.query.app_id, user1, user2);
+							break;
+						}
+						case 'SETTINGS_LIKE':{
+							//pick a random user setting from the user and return the app_id
+							let user_settings = demo_users.filter(user=>user.id == user1)[0].settings;
+							let settings_app_id = user_settings[Math.floor(1 + Math.random() * user_settings.length - 1 )].app_id;
+							result_insert = await create_user_account_app_setting_like(settings_app_id, user1, user2);
+							break;
+						}
+						case 'SETTINGS_VIEW':
+						case 'SETTINGS_VIEW_ANONYMOUS':{
+							//pick a random user setting from the user and return the app_id
+							let user_settings = demo_users.filter(user=>user.id == user1)[0].settings;
+							let settings_app_id = user_settings[Math.floor(1 + Math.random() * user_settings.length - 1 )].app_id;
+							result_insert = await create_user_account_app_setting_view(settings_app_id, user1, user2 , social_type) ;
+							break;
+						}
+					}						
+				}
 			}
 		}
 		return res.status(200).json({
 			count_records_user_account: records_user_account,
 			count_records_user_account_app: records_user_account_app,
-			count_records_user_account_app_setting: records_user_account_app_setting
+			count_records_user_account_app_setting: records_user_account_app_setting,
+			count_records_user_account_like: records_user_account_like,
+			count_records_user_account_view: records_user_account_view,
+			count_records_user_account_follow: records_user_account_follow,
+			count_records_user_account_setting_like: records_user_account_setting_like,
+			count_records_user_account_setting_view: records_user_account_setting_view
 		});
 	} catch (error) {
 		import(`file://${process.cwd()}${ConfigGet(1, 'SERVER', 'REST_RESOURCE_SERVICE')}/db${ConfigGet(1, 'SERVICE_DB', 'REST_RESOURCE_SCHEMA')}/user_account/user_account.controller.js`).then(({checked_error}) =>{
@@ -306,23 +387,29 @@ const demo_delete = async (req, res)=> {
 			}
 			else{
 				let deleted_user = 0;
-				if (result_demo_users.length>0)
-					for (let user of result_demo_users){
-						deleteUser(req.query.app_id, user.id,  (err, result_deleteUser) =>{
-							if (err) {
-								return res.status(500).send({
-									data: err
-								});
-							}
-							else{
-								deleted_user++;
-								if (deleted_user == result_demo_users.length)
-									return res.status(200).json({
-										count_deleted: result_demo_users.length
-									});
+				if (result_demo_users.length>0){
+					const delete_user = async () => {
+						return new Promise((resolve, reject)=>{
+							for (let user of result_demo_users){
+								deleteUser(req.query.app_id, user.id,  (err, result_deleteUser) =>{
+									if (err) {
+										resolve(err);
+									}
+									else{
+										deleted_user++;
+										if (deleted_user == result_demo_users.length)
+											resolve();
+									}
+								})
 							}
 						})
 					}
+					delete_user().then(()=>{
+						return res.status(200).json({
+							count_deleted: result_demo_users.length
+						});
+					});
+				}
 				else
 					return res.status(200).json({
 						count_deleted: result_demo_users.length
