f2398314e85587143372432ce1457fdaa68e76ab
fapwork <fapwork@proton.me>
2023-04-30 01:17:22 +0200

    APB-491 moves /service/forms/controller.js into /apps/controller.js, renames getForm, getFormAdmin and getFormAdminSecure to getApp, getAppAdmin and getAppAdminSecure, renames old apps getApp and getAdmin to createApp and createAdmin, moves countries function to app2, updates documentation how to create new app. renames /apps/index.js to apps.service.js

--- apps/app2/server.js
+++ apps/app2/server.js
@@ -9,7 +9,7 @@ const server = (app) =>{
   //routes
   //app 2 pwa service worker, placed in root
   app.get("/sw.js",(req, res,next) => {
-    import(`file://${process.cwd()}/apps/index.js`).then(({ check_app_subdomain}) => {
+    import(`file://${process.cwd()}/apps/apps.service.js`).then(({ check_app_subdomain}) => {
       if (check_app_subdomain(APP2_ID, req.headers.host)) {
           res.type('application/javascript');
           res.setHeader('Service-Worker-Allowed', '/')
@@ -21,9 +21,9 @@ const server = (app) =>{
     })
   });
   app.get("/info/:info", (req, res, next) => {
-    import(`file://${process.cwd()}/apps/index.js`).then(({ check_app_subdomain}) => {
+    import(`file://${process.cwd()}/apps/apps.service.js`).then(({ check_app_subdomain}) => {
       if (check_app_subdomain(APP2_ID, req.headers.host)) {
-          import(`file://${process.cwd()}/apps/index.js`).then(({ getInfo}) => {
+          import(`file://${process.cwd()}/apps/apps.service.js`).then(({ getInfo}) => {
             switch (req.params.info){
               case 'about':
               case 'disclaimer':
@@ -50,7 +50,7 @@ const server = (app) =>{
   });
   //app 2 progressive webapp menifest
   app.get("/app2/manifest.json", (req, res, next) => {
-    import(`file://${process.cwd()}/apps/index.js`).then(({ check_app_subdomain}) => {
+    import(`file://${process.cwd()}/apps/apps.service.js`).then(({ check_app_subdomain}) => {
       if (check_app_subdomain(APP2_ID, req.headers.host)){
         import(`file://${process.cwd()}/server/server.service.js`).then(({ConfigGet}) => {
           import(`file://${process.cwd()}${ConfigGet(1, 'SERVER', 'REST_RESOURCE_SERVICE')}/db${ConfigGet(1, 'SERVICE_DB', 'REST_RESOURCE_SCHEMA')}/app_parameter/app_parameter.service.js`).then(({ getParameters }) => {
@@ -141,7 +141,7 @@ const server = (app) =>{
   });
   //app 2 show profile directly from url
   app.get('/:user', (req, res,next) => {
-    import(`file://${process.cwd()}/apps/index.js`).then(({ check_app_subdomain}) => {
+    import(`file://${process.cwd()}/apps/apps.service.js`).then(({ check_app_subdomain}) => {
       if (check_app_subdomain(APP2_ID, req.headers.host) &&
         req.params.user !== '' && 
         req.params.user!=='manifest.json' &&
@@ -150,8 +150,8 @@ const server = (app) =>{
         req.params.user!=='images' &&
         req.params.user!=='info' &&
         req.params.user!=='js') {
-        import(`file://${process.cwd()}${ConfigGet(1, 'SERVER', 'REST_RESOURCE_SERVICE')}/forms/forms.controller.js`).then(({ getForm}) => {
-          getForm(req, res, APP2_ID, req.params.user, (err, app_result)=>{
+        import(`file://${process.cwd()}/apps/apps.controller.js`).then(({ getApp}) => {
+          getApp(req, res, APP2_ID, req.params.user, (err, app_result)=>{
             //if app_result=0 means here redirect to /
             if (app_result==0)
               return res.redirect('/');
@@ -165,10 +165,10 @@ const server = (app) =>{
     })
   });
   app.get('/', (req, res, next) => {
-    import(`file://${process.cwd()}/apps/index.js`).then(({ check_app_subdomain}) => {
+    import(`file://${process.cwd()}/apps/apps.service.js`).then(({ check_app_subdomain}) => {
       if (check_app_subdomain(APP2_ID, req.headers.host)){
-        import(`file://${process.cwd()}${ConfigGet(1, 'SERVER', 'REST_RESOURCE_SERVICE')}/forms/forms.controller.js`).then(({ getForm}) => {
-          getForm(req, res, APP2_ID, null,(err, app_result)=>{
+        import(`file://${process.cwd()}/apps/apps.controller.js`).then(({ getApp}) => {
+          getApp(req, res, APP2_ID, null,(err, app_result)=>{
               return res.send(app_result);
           })
         })
