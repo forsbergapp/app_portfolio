42c0452216e84cbfcf2809de04428b99f1a796e7
fapwork <fapwork@proton.me>
2023-05-01 01:46:56 +0200

    APB-485 adds client_locale function in apps.controller that is used to get first language in 'accept-language' and client is only translating at startup if navigator.language is different to avoid to much network calls at app startup. removes ConfigGet declaration not used.

--- apps/admin/src/secure/index.js
+++ apps/admin/src/secure/index.js
@@ -1,6 +1,6 @@
 const { read_app_files, get_module_with_init} = await import(`file://${process.cwd()}/apps/apps.service.js`);
 
-const createAdminSecure = (app_id, system_admin, user_account_id, gps_lat, gps_long, gps_place) => {
+const createAdminSecure = (app_id, system_admin, user_account_id, gps_lat, gps_long, gps_place, locale) => {
     return new Promise((resolve, reject) => {
         const files = [
             ['APP', process.cwd() + '/apps/admin/src/secure/index.html'],
@@ -23,15 +23,16 @@ const createAdminSecure = (app_id, system_admin, user_account_id, gps_lat, gps_l
                 app = app.replace(  '/*<APP_STYLE_END/>*/',
                                     '</STYLE>');
                 get_module_with_init(app_id, 
-                                        system_admin,
-                                        user_account_id,
-                                    'app.admin_logoff_app',
-                                    true, //close eventsource and create new as logged in
-                                    false, //ui
-                                    gps_lat,
-                                    gps_long,
-                                    gps_place,
-                                    app, (err, app_init) =>{
+                                     locale,
+                                     system_admin,
+                                     user_account_id,
+                                     'app.admin_logoff_app',
+                                     true, //close eventsource and create new as logged in
+                                     false, //ui
+                                     gps_lat,
+                                     gps_long,
+                                     gps_place,
+                                     app, (err, app_init) =>{
                     if (err)
                         reject(err);
                     else{
