daaba971d8db8f97174045dd5df2e211d22d9579
fapwork <fapwork@proton.me>
2023-05-10 00:00:10 +0200

    APB-482 createMail replaces getMail and moves logic from mail.controller.js to apps.service.js, replaces logo with simple link without router using site logo not app1 logo, moves mail files to /common/src, updates sending user emails using createMail and BFF in user_account.controller.js, adds endpoint /mail (unverified) /mail/signup and /mail/access (forgot, change when logged in) to send email

--- service/mail/mail.service.js
+++ service/mail/mail.service.js
@@ -1,33 +1,47 @@
 const nodemailer = await import('nodemailer');
 
-const sendEmailService = (data, callBack) => {
-    let transporter = nodemailer.createTransport({
-        host: data.email_host,
-        port: data.email_port,
-        secure: data.email_secure,
-        auth: {
-            user: data.email_auth_user,
-            pass: data.email_auth_pass
-        },
-        debug: false,
-        logger: false
-    });
+const sendEmail = async (data) => {
+    return new Promise((resolve, reject)=>{
+        /*
+        {
+            "email_host":         [host],
+            "email_port":         [port],
+            "email_secure":       [secure],
+            "email_auth_user":    [user],
+            "email_auth_pass":    [password],
+            "from":               [email from ],
+            "to":                 [email to],
+            "subject":            [subject],
+            "html":               [html email]
+        };
+        */
+        const transporter = nodemailer.createTransport({
+            host: data.email_host,
+            port: data.email_port,
+            secure: data.email_secure,
+            auth: {
+                user: data.email_auth_user,
+                pass: data.email_auth_pass
+            },
+            debug: false,
+            logger: false
+        });
+        const message = {
+            from: `"${data.from}" <${data.email_auth_user}>`,
+            to: data.to,
+            subject: data.subject,
+            html: data.html,
+            encoding: 'utf-8',
+            textEncoding: 'quoted-printable'
+        };
 
-    const message = {
-        from: `"${data.from}" <${data.email_auth_user}>`,
-        to: data.to,
-        subject: data.subject,
-        html: data.html,
-        encoding: 'utf-8',
-        textEncoding: 'quoted-printable'
-    };
-
-    transporter.sendMail(message, (err, result) => {
-        if (err) {
-            return callBack(err, null);
-        } else {
-            return callBack(null, result);
-        }
-    });
+        transporter.sendMail(message, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    })
 }
-export{sendEmailService};
+export{sendEmail};
