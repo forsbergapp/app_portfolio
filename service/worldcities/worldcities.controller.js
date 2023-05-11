802d56c93b82d547c8b10ed85a7f4c538b176ddd
appportfolio <app.portfolio@protonmail.com>
2023-05-11 15:12:32 +0200

    APB-482 moves sort in controller to service for worldcities

--- service/worldcities/worldcities.controller.js
+++ service/worldcities/worldcities.controller.js
@@ -1,12 +1,11 @@
 const service = await import('./worldcities.service.js')
 const getCities = (req, res) => {
-	service.getService((err, cities) => {
+	service.getService(req.params.country,(err, cities) => {
 		if (err)
 			return res.status(500).json(
 				err
 			);
 		else{
-			cities = JSON.parse(cities).filter((item) => item.iso2 == req.params.country);	
 			return res.status(200).json(
 				cities
 			);
