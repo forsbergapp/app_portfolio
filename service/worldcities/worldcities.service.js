d235818af2bac1cced0a94af383e470a48e5f0d8
fapwork <fapwork@proton.me>
2023-05-11 15:12:32 +0200

    APB-482 moves sort in controller to service for worldcities

--- service/worldcities/worldcities.service.js
+++ service/worldcities/worldcities.service.js
@@ -1,10 +1,11 @@
-const getService = (callBack) => {
+const getService = async (country,callBack) => {
     import('node:fs').then((fs) =>{
         fs.readFile(process.cwd() + '/service/worldcities/worldcities.json', 'utf8', (error, fileBuffer) => {
             if (error)
                 callBack(error,null);
-            else
-                callBack(null,fileBuffer.toString());
+            else{
+                callBack(null,JSON.parse(fileBuffer.toString()).filter((item) => item.iso2 == country));
+            }
         });
     })
 }
