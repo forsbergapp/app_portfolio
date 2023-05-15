958cae1f0b3f3694299b753cb4c444d95cbfa274
fapwork <fapwork@proton.me>
2023-05-15 23:01:59 +0200

    APB-504 implements cache for geolocation, saves files as geodata_cache_ip.json and geodata_cache_place.json, for PLACE uses 2 decimals and search aproximate value +-0.07, adds exceptions in .gitignore

--- service/geolocation/geolocation.controller.js
+++ service/geolocation/geolocation.controller.js
@@ -1,81 +1,39 @@
 const service = await import('./geolocation.service.js')
 
-const geodata_empty = (geotype) => {
-	let geodata='';
-	switch (geotype){
-		case 'ip':{
-			//http://www.geoplugin.net/json.gp?ip=
-			//used geoplugin_city, geoplugin_regionName, geoplugin_countryName,geoplugin_latitude, geoplugin_longitude
-			return geodata = {
-				"geoplugin_request":"",
-				"geoplugin_status":null,
-				"geoplugin_delay":"",
-				"geoplugin_credit":"",
-				"geoplugin_city":"",
-				"geoplugin_region":"",
-				"geoplugin_regionCode":"",
-				"geoplugin_regionName":"",
-				"geoplugin_areaCode":"",
-				"geoplugin_dmaCode":"",
-				"geoplugin_countryCode":"",
-				"geoplugin_countryName":"",
-				"geoplugin_inEU":null,
-				"geoplugin_euVATrate":null,
-				"geoplugin_continentCode":"",
-				"geoplugin_continentName":"",
-				"geoplugin_latitude":"",
-				"geoplugin_longitude":"",
-				"geoplugin_locationAccuracyRadius":"",
-				"geoplugin_timezone":"",
-				"geoplugin_currencyCode":"",
-				"geoplugin_currencySymbol":"",
-				"geoplugin_currencySymbol_UTF8":"",
-				"geoplugin_currencyConverter":null
-			  };
-			break;
-		}
-		case 'place':{
-			//http://www.geoplugin.net/extras/location.gp?format=json&lat=[latitude]&lon=[longitude]
-			//used geoplugin_place. geoplugin_countryCode, geoplugin_region
-			return geodata = {
-				"geoplugin_place":"",
-				"geoplugin_countryCode":"",
-				"geoplugin_region":"",
-				"geoplugin_regionAbbreviated":"",
-				"geoplugin_county":"",
-				"geoplugin_latitude":"",
-				"geoplugin_longitude":"",
-				"geoplugin_distanceMiles":null,
-				"geoplugin_distanceKilometers":null
-			};
-			break;
-		}
-		default: return null;
-	}
-}
-
 const getPlace = async (req, res) => {
 	let geodata;
-	//service can return other formats, set json
-	const url = `http://www.geoplugin.net/extras/location.gp?format=json&lat=${req.query.latitude}&lon=${req.query.longitude}`;
-	geodata = await service.getService(url, req.headers["accept-language"]);
-	return res.status(200).send(
-		geodata
-	)
+	geodata = await service.getCacheGeodata('PLACE', null, req.query.latitude, req.query.longitude);
+	if (geodata != null)
+		return res.status(200).send(geodata);
+	else{
+		//service can return other formats, set json
+		const url = `http://www.geoplugin.net/extras/location.gp?format=json&lat=${req.query.latitude}&lon=${req.query.longitude}`;
+		geodata = await service.getGeodata(url, req.headers["accept-language"]);
+		if (geodata != '[[]]')
+			service.writeCacheGeodata('PLACE', geodata);
+		return res.status(200).send(
+			geodata
+		)
+	}
 }
-const getIp = async (req, res, callBack) => {
+const getIp = async (req, res) => {
 	let geodata;
 	let url;
-	if (req.query.ip == '::1' || req.query.ip == '::ffff:127.0.0.1')
-		url = `http://www.geoplugin.net/json.gp?ip=`;
-	else
-		url = `http://www.geoplugin.net/json.gp?ip=${req.query.ip}`;
-
-	geodata = await service.getService(url, req.headers["accept-language"]);
-	if (req.query.callback==1)
-		return callBack(null, geodata);
-	else
+	geodata = await service.getCacheGeodata('IP', req.query.ip, null, null);
+	if (geodata != null)
 		return res.status(200).send(geodata);
+	else{
+		if (req.query.ip == '::1' || req.query.ip == '::ffff:127.0.0.1'){
+			//create empty record with ip ::1 first time
+			service.writeCacheGeodata('IP', service.getGeodataEmpty('IP'));
+			url = `http://www.geoplugin.net/json.gp?ip=`;
+		}
+		else
+			url = `http://www.geoplugin.net/json.gp?ip=${req.query.ip}`;
+		geodata = await service.getGeodata(url, req.headers["accept-language"]);
+		service.writeCacheGeodata('IP', geodata);
+		return res.status(200).send(geodata);
+	}
 }
 const getTimezone = (req, res) => {
 	service.getTimezone(req.query.latitude, req.query.longitude, (err, result)=>{
