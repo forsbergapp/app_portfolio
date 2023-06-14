fceb370eea8e162108318ca51abab60061dcd83d
appportfolio <forsbergapp@protonmail.com>
2023-06-14 16:20:38 +0200

    APB-15 adds batch microservice on port 3003 that uses batch.json and /service/logs/YYYYMMDD_batch_log.json files and performs git pull in app_portfolio_test initially supporting both windows and ubuntu, uses cron expression except advanced expressions. Cancels pending or running jobs if batch server is restarted. Default enabled git pull is 8,12, 17 each day.

---
+++ service/batch/server.js
@@ -0,0 +1,34 @@
+const service = await import('./service.js');
+const fs = await import('node:fs');
+const https = await import('node:https');
+const {MICROSERVICE, IAM} = await import(`file://${process.cwd()}/service/service.service.js`);
+
+const startserver = () =>{
+	let options;
+	fs.readFile(process.cwd() + MICROSERVICE.filter(row=>row.SERVICE=='BATCH')[0].HTTPS_KEY, 'utf8', (error, fileBuffer) => {
+		let env_key = fileBuffer.toString();
+		fs.readFile(process.cwd() + MICROSERVICE.filter(row=>row.SERVICE=='BATCH')[0].HTTPS_CERT, 'utf8', (error, fileBuffer) => {
+			let env_cert = fileBuffer.toString();
+			options = {
+				key: env_key,
+				cert: env_cert
+			};
+			https.createServer(options, (req, res) => {
+				req.query = {};
+				req.params = {};
+            	res.setHeader('Access-Control-Allow-Methods', 'GET');
+				res.setHeader('Access-Control-Allow-Origin', '*');
+				res.setHeader('Content-Type',  'application/json; charset=utf-8');
+				
+			}).listen(MICROSERVICE.filter(row=>row.SERVICE=='BATCH')[0].PORT, ()=>{
+				console.log(`MICROSERVICE BATCH PORT ${MICROSERVICE.filter(row=>row.SERVICE=='BATCH')[0].PORT} `);
+			});
+			service.start_jobs();
+			process.on('uncaughtException', (err) =>{
+				console.log(err);
+			})
+		})
+	})
+}
+startserver();
+export {startserver}
