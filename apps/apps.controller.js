c12ab3c36b863c48fac66f93356fc81720c6b0f4
fapwork <fapwork@proton.me>
2023-09-02 21:22:43 +0200

    APB-556 app does not hang anymore if account in url does not exists

--- apps/apps.controller.js
+++ apps/apps.controller.js
@@ -82,49 +82,53 @@ const getApp = async (req, res, app_id, params, callBack) => {
             else{
                 import(`file://${process.cwd()}/apps/app${app_id}/src/app.js`).then(({ createApp }) => {
                     createApp(app_id, params,service.client_locale(req.headers['accept-language'])).then((app) => {
-                        //get translation data
-                        import(`file://${process.cwd()}/server/dbapi/app_portfolio/app_object/app_object.service.js`).then(({getObjects}) => {
-                            getObjects(app_id, service.client_locale(req.headers['accept-language']), 'APP_OBJECT_ITEM', 'COMMON', (err, result_objects) => {
-                                for (const row of result_objects){
-                                    app = app.replaceAll(
-                                        `<CommonTranslation${row.object_item_name.toUpperCase()}/>`,
-                                        `${row.text}`);
-                                }
-                                //get app with parameters
-                                service.get_module_with_init(app_id, 
-                                    service.client_locale(req.headers['accept-language']),
-                                    0,  //system_admin_only
-                                    true,  //ui
-                                    datatoken,
-                                    result_geodata.latitude,
-                                    result_geodata.longitude,
-                                    result_geodata.place,
-                                    app, (err, app_with_init) =>{
-                                    import(`file://${process.cwd()}/server/dbapi/app_portfolio/app_log/app_log.service.js`).then(({createLog}) => {
-                                        createLog(req.query.app_id,
-                                                    { app_id : app_id,
-                                                    app_module : 'APPS',
-                                                    app_module_type : 'APP',
-                                                    app_module_request : params,
-                                                    app_module_result : result_geodata.place,
-                                                    app_user_id : null,
-                                                    user_language : null,
-                                                    user_timezone : null,
-                                                    user_number_system : null,
-                                                    user_platform : null,
-                                                    server_remote_addr : req.ip,
-                                                    server_user_agent : req.headers['user-agent'],
-                                                    server_http_host : req.headers['host'],
-                                                    server_http_accept_language : req.headers['accept-language'],
-                                                    client_latitude : result_geodata.latitude,
-                                                    client_longitude : result_geodata.longitude
-                                                    }, ()  => {
-                                                        return callBack(null, app_with_init);
+                        if (app == 0)
+                            return callBack(null, app);
+                        else{
+                            //get translation data
+                            import(`file://${process.cwd()}/server/dbapi/app_portfolio/app_object/app_object.service.js`).then(({getObjects}) => {
+                                getObjects(app_id, service.client_locale(req.headers['accept-language']), 'APP_OBJECT_ITEM', 'COMMON', (err, result_objects) => {
+                                    for (const row of result_objects){
+                                        app = app.replaceAll(
+                                            `<CommonTranslation${row.object_item_name.toUpperCase()}/>`,
+                                            `${row.text}`);
+                                    }
+                                    //get app with parameters
+                                    service.get_module_with_init(app_id, 
+                                        service.client_locale(req.headers['accept-language']),
+                                        0,  //system_admin_only
+                                        true,  //ui
+                                        datatoken,
+                                        result_geodata.latitude,
+                                        result_geodata.longitude,
+                                        result_geodata.place,
+                                        app, (err, app_with_init) =>{
+                                        import(`file://${process.cwd()}/server/dbapi/app_portfolio/app_log/app_log.service.js`).then(({createLog}) => {
+                                            createLog(req.query.app_id,
+                                                        { app_id : app_id,
+                                                        app_module : 'APPS',
+                                                        app_module_type : 'APP',
+                                                        app_module_request : params,
+                                                        app_module_result : result_geodata.place,
+                                                        app_user_id : null,
+                                                        user_language : null,
+                                                        user_timezone : null,
+                                                        user_number_system : null,
+                                                        user_platform : null,
+                                                        server_remote_addr : req.ip,
+                                                        server_user_agent : req.headers['user-agent'],
+                                                        server_http_host : req.headers['host'],
+                                                        server_http_accept_language : req.headers['accept-language'],
+                                                        client_latitude : result_geodata.latitude,
+                                                        client_longitude : result_geodata.longitude
+                                                        }, ()  => {
+                                                            return callBack(null, app_with_init);
+                                            });
                                         });
                                     });
                                 });
                             });
-                        });
+                        }
                     });
                 });
             }
