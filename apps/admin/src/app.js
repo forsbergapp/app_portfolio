2d399ec6fea006c7731980744cb7a5ea109b54dd
fapwork <fapwork@proton.me>
2023-09-05 22:35:54 +0200

    APB-558 reorganizes server app html render and separates common and app render so common code are consolidated, adds read_common_files and render_common_html, splits body.html into body_dialogues.html and body_window_info.html

--- apps/admin/src/app.js
+++ apps/admin/src/app.js
@@ -1,45 +1,69 @@
 const { apps_start_ok } = await import(`file://${process.cwd()}/apps/apps.service.js`);
 const {ConfigGet} = await import(`file://${process.cwd()}/server/server.service.js`);
-const { read_app_files, getUserPreferences } = await import(`file://${process.cwd()}/apps/apps.service.js`);
+const { render_common_html, read_app_files, getUserPreferences } = await import(`file://${process.cwd()}/apps/apps.service.js`);
 
 const createAdmin = (app_id, locale) => {
     return new Promise((resolve, reject) => {
         const files = [
             ['APP', process.cwd() + '/apps/admin/src/index.html'],
             ['<AppHead/>', process.cwd() + '/apps/admin/src/head.html'],
-            ['<AppCommonHead/>', process.cwd() + '/apps/common/src/head.html'],
-            ['<AppCommonFonts/>', process.cwd() + '/apps/common/src/fonts.html'],
-            ['<AppCommonHeadMap/>', process.cwd() + '/apps/common/src/head_map.html'],
-            ['<AppCommonProfileSearch/>', process.cwd() + '/apps/common/src/profile_search.html'],
-            ['<AppCommonUserAccount/>', process.cwd() + '/apps/common/src/user_account.html'],
             ['<AppMenu/>', process.cwd() + '/apps/admin/src/menu.html'],
             ['<AppDialogues/>', process.cwd() + '/apps/admin/src/dialogues.html'],
-            ['<AppSecure/>', process.cwd() + '/apps/admin/src/secure.html'],
-            ['<AppThemes/>', process.cwd() + '/apps/common/src/app_themes.html'],
-            ['<AppCommonBody/>', process.cwd() + '/apps/common/src/body.html'],
-            ['<AppCommonBodyBroadcast/>', process.cwd() + '/apps/common/src/body_broadcast.html'],
-            ['<AppCommonProfileDetail/>', process.cwd() + '/apps/common/src/profile_detail.html'], //Profile tag in common body
-            ['<AppCommonProfileBtnTop/>', process.cwd() + '/apps/common/src/profile_btn_top.html']            
+            ['<AppSecure/>', process.cwd() + '/apps/admin/src/secure.html']
             ];
         if (ConfigGet(1, 'SERVICE_DB', 'START')=='1' && apps_start_ok()==true){
             getUserPreferences(app_id, locale).then((user_preferences) => {
-                read_app_files('', files, (err, app)=>{
+                read_app_files(files, (err, app)=>{
+                    render_common_html(app_id, app,	'FORM', true, '<AppUserAccount/>', true).then((app)=>{
+                        if (err)
+                            reject(err);
+                        else{
+                            //COMMON, set user preferences content
+                            app = app.replace(
+                                '<USER_LOCALE/>',
+                                `${user_preferences.user_locales}`);
+                            app = app.replace(
+                                '<USER_TIMEZONE/>',
+                                `${user_preferences.user_timezones}`);
+                            app = app.replace(
+                                '<USER_DIRECTION/>',
+                                `<option id='' value=''></option>${user_preferences.user_directions}`);
+                            app = app.replace(
+                                '<USER_ARABIC_SCRIPT/>',
+                                `<option id='' value=''></option>${user_preferences.user_arabic_scripts}`);
+                            //APP Profile tag not used in common body
+                            app = app.replace(
+                                '<AppProfileInfo/>',
+                                '');
+                            //APP Profile tag not used in common body
+                            app = app.replace(
+                                '<AppProfileTop/>',
+                                '');
+                            resolve(app);
+                        }
+                    });
+                });
+            });
+        }
+        else{
+            read_app_files(files, (err, app)=>{
+                render_common_html(app,	'FORM', true, '<AppUserAccount/>', true).then((app)=>{
                     if (err)
                         reject(err);
                     else{
                         //COMMON, set user preferences content
                         app = app.replace(
                             '<USER_LOCALE/>',
-                            `${user_preferences.user_locales}`);
+                            '');
                         app = app.replace(
                             '<USER_TIMEZONE/>',
-                            `${user_preferences.user_timezones}`);
+                            '');
                         app = app.replace(
                             '<USER_DIRECTION/>',
-                            `<option id='' value=''></option>${user_preferences.user_directions}`);
+                            '');
                         app = app.replace(
                             '<USER_ARABIC_SCRIPT/>',
-                            `<option id='' value=''></option>${user_preferences.user_arabic_scripts}`);
+                            '');
                         //APP Profile tag not used in common body
                         app = app.replace(
                             '<AppProfileInfo/>',
@@ -51,36 +75,6 @@ const createAdmin = (app_id, locale) => {
                         resolve(app);
                     }
                 });
-            });
-        }
-        else{
-            read_app_files('', files, (err, app)=>{
-                if (err)
-                    reject(err);
-                else{
-                    //COMMON, set user preferences content
-                    app = app.replace(
-                        '<USER_LOCALE/>',
-                        '');
-                    app = app.replace(
-                        '<USER_TIMEZONE/>',
-                        '');
-                    app = app.replace(
-                        '<USER_DIRECTION/>',
-                        '');
-                    app = app.replace(
-                        '<USER_ARABIC_SCRIPT/>',
-                        '');
-                    //APP Profile tag not used in common body
-                    app = app.replace(
-                        '<AppProfileInfo/>',
-                        '');
-                    //APP Profile tag not used in common body
-                    app = app.replace(
-                        '<AppProfileTop/>',
-                        '');
-                    resolve(app);
-                }
             });
         }
         
