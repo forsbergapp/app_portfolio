1b04f6b95d92db575d185daffeb183557701fce7
appportfolio <forsbergapp@protonmail.com>
2023-09-19 20:31:53 +0200

    APB-558 renames read_app_files to render_app_html

--- apps/admin/src/app.js
+++ apps/admin/src/app.js
@@ -1,6 +1,6 @@
 const { apps_start_ok } = await import(`file://${process.cwd()}/apps/apps.service.js`);
 const {ConfigGet} = await import(`file://${process.cwd()}/server/server.service.js`);
-const { render_common_html, read_app_files } = await import(`file://${process.cwd()}/apps/apps.service.js`);
+const { render_common_html, render_app_html } = await import(`file://${process.cwd()}/apps/apps.service.js`);
 
 const createAdmin = (app_id, locale) => {
     return new Promise((resolve, reject) => {
@@ -12,7 +12,7 @@ const createAdmin = (app_id, locale) => {
             ['<AppSecure/>', process.cwd() + '/apps/admin/src/secure.html']
             ];
         if (ConfigGet(1, 'SERVICE_DB', 'START')=='1' && apps_start_ok()==true){
-            read_app_files(files, (err, app)=>{
+            render_app_html(files, (err, app)=>{
                 render_common_html(app_id, app,	locale, 'FORM', true, '<AppUserAccount/>', true, true, true, true).then((app)=>{
                     if (err)
                         reject(err);
@@ -31,7 +31,7 @@ const createAdmin = (app_id, locale) => {
             });
         }
         else{
-            read_app_files(files, (err, app_files)=>{
+            render_app_html(files, (err, app_files)=>{
                 render_common_html(app_id, app_files, locale, 'FORM', true, '<AppUserAccount/>', true, false, false, false).then((app)=>{
                     if (err)
                         reject(err);
--- apps/app2/src/report/index.js
+++ apps/app2/src/report/index.js
@@ -1,4 +1,4 @@
-const { render_common_html, read_app_files } = await import(`file://${process.cwd()}/apps/apps.service.js`);
+const { render_common_html, render_app_html } = await import(`file://${process.cwd()}/apps/apps.service.js`);
 
 const createReport = (app_id, module) => {
     return new Promise((resolve, reject) => {
@@ -8,7 +8,7 @@ const createReport = (app_id, module) => {
             ['<AppCommonFonts/>', process.cwd() + '/apps/app2/src/fonts.html'],
             ['<ReportPaper/>', process.cwd() + '/apps/app2/src/report/paper.html']
         ];
-        read_app_files(files, (err, report)=>{
+        render_app_html(files, (err, report)=>{
             render_common_html(app_id, report,null, 'REPORT', false, null, false, false, false, false).then((report)=>{
                 if (err)
                     reject(err);
