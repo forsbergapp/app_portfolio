e7a7d76007d39750fdb8bdcba7a01a9169166953
fapwork <fapwork@proton.me>
2023-09-26 16:25:47 +0200

    APB-583 adds new router /worldcities/city/search/ and service to get cities using search, uses switch statement in worldcities router and adds getCitySearch, removes export in server.js

--- service/worldcities/service.js
+++ service/worldcities/service.js
@@ -30,4 +30,36 @@ const getCityRandom = async () => {
     return cities[Math.floor(Math.random() * cities.length - 1)];
 };
 
-export{getCities, getCityRandom};
+const getCitySearch = async (search, limit) => {
+    let cities = await getService();
+    //parse to object
+    cities = JSON.parse(cities);
+    //filter searched 
+    const match = (col, search) =>{
+        //compare without diacritics and use lower case
+        const col_check = col.normalize('NFD').replace(/\p{Diacritic}/gu, '').toLowerCase();
+        const search_check = search.normalize('NFD').replace(/\p{Diacritic}/gu, '').toLowerCase();
+        return col_check.search(search_check);
+    };
+    cities = cities.filter(city=>   match(city.city, search) >-1|| 
+                                    match(city.city_ascii, search) > -1|| 
+                                    match(city.country, search) > -1 ||
+                                    match(city.admin_name, search) > -1);
+    //sort
+    cities = cities.sort((first, second)=>{
+        const first_sort = first.country.toLowerCase() +  first.city.toLowerCase() + first.admin_name.toLowerCase();
+        const second_sort = second.country.toLowerCase() + second.city.toLowerCase() + second.admin_name.toLowerCase();
+        //using localeCompare as collation method
+        if (first_sort.localeCompare(second_sort)<0 )
+            return -1;
+        else if (first_sort.localeCompare(second_sort)>0 )
+            return 1;
+        else
+            return 0;
+    });
+    //limit records if used
+    if (limit>0)
+        cities = cities.filter((city, index)=>index<=limit);
+    return cities;
+};
+export{getCities, getCityRandom, getCitySearch};
