4e9a2784e9292eb1fe0b9ba3774c884bc75f382a
appportfolio <forsbergapp@protonmail.com>
2023-10-10 12:49:42 +0200

    APB-624 consolidates microservice start logic to MicroserviceServer(), uses ServiceConfig to read HTTPS_ENABLE, HTTPS_KEY and HTTPS_CERT instead of saving in MICROSERVICE variable, microservices start http if config does not exists or if https not enabled or microservices start https if enabled in config, returns statuscode 401 for all requests in batch, updates documentation, uses default for bcryptsjs in CreateSystemAdmin

--- service/worldcities/server.js
+++ service/worldcities/server.js
@@ -1,84 +1,72 @@
 const service = await import('./service.js');
-const fs = await import('node:fs');
-const https = await import('node:https');
-const {MICROSERVICE, IAM} = await import(`file://${process.cwd()}/service/service.service.js`);
+const { MicroserviceServer, IAM} = await import(`file://${process.cwd()}/service/service.service.js`);
 
-const startserver = () =>{
-	let options;
-	fs.readFile(process.cwd() + MICROSERVICE.filter(row=>row.SERVICE=='WORLDCITIES')[0].HTTPS_KEY, 'utf8', (error, fileBuffer) => {
-		const env_key = fileBuffer.toString();
-		fs.readFile(process.cwd() + MICROSERVICE.filter(row=>row.SERVICE=='WORLDCITIES')[0].HTTPS_CERT, 'utf8', (error, fileBuffer) => {
-			const env_cert = fileBuffer.toString();
-			options = {
-				key: env_key,
-				cert: env_cert
-			};
-			https.createServer(options, (req, res) => {
-				req.query = {};
-				req.params = {};
-				res.setHeader('Access-Control-Allow-Methods', 'GET');
-				res.setHeader('Access-Control-Allow-Origin', '*');
-				res.setHeader('Content-Type',  'application/json; charset=utf-8');
-				const params = new URLSearchParams(req.url.substring(req.url.indexOf('?')));
-				req.query.app_id = params.get('app_id');
-				switch (true){
-					case req.url.startsWith('/worldcities/city/search/'):{
-						req.params.search = req.url.substring('/worldcities/city/search/'.length, req.url.indexOf('?'));
-						if (params.get('limit'))
-							req.query.limit = Number(params.get('limit'));
-						else
-							req.query.limit = 0;
-						IAM(req.query.app_id, req.headers.authorization).then(result=>{
-							if (result == 1)
-								getCitySearch(req, res);
-							else
-							{
-								res.statusCode = 401;
-								res.write('⛔', 'utf-8');
-								res.end();
-							}
-						});
-						break;
-					}
-					case req.url.startsWith('/worldcities/city/random'):{
-						IAM(req.query.app_id, req.headers.authorization).then(result=>{
-							if (result == 1)
-								getCityRandom(req, res);
-							else
-							{
-								res.statusCode = 401;
-								res.write('⛔', 'utf-8');
-								res.end();
-							}
-						});
-						break;
-					}
-					case req.url.startsWith('/worldcities/country/'):{
-						req.params.country = req.url.substring('/worldcities/country/'.length, req.url.indexOf('?'));
-						IAM(req.query.app_id, req.headers.authorization).then(result=>{
-							if (result == 1)
-								getCities(req, res);
-							else
-							{
-								res.statusCode = 401;
-								res.write('⛔', 'utf-8');
-								res.end();
-							}
-						});
-						break;
-					}
-					default:
+const startserver = async () =>{
+	const request = await MicroserviceServer('WORLDCITIES');
+	request.server.createServer(request.options, (req, res) => {
+		req.query = {};
+		req.params = {};
+		res.setHeader('Access-Control-Allow-Methods', 'GET');
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Content-Type',  'application/json; charset=utf-8');
+		const params = new URLSearchParams(req.url.substring(req.url.indexOf('?')));
+		req.query.app_id = params.get('app_id');
+		switch (true){
+			case req.url.startsWith('/worldcities/city/search/'):{
+				req.params.search = req.url.substring('/worldcities/city/search/'.length, req.url.indexOf('?'));
+				if (params.get('limit'))
+					req.query.limit = Number(params.get('limit'));
+				else
+					req.query.limit = 0;
+				IAM(req.query.app_id, req.headers.authorization).then(result=>{
+					if (result == 1)
+						getCitySearch(req, res);
+					else
+					{
+						res.statusCode = 401;
+						res.write('⛔', 'utf-8');
 						res.end();
-						break;
-				}
-			}).listen(MICROSERVICE.filter(row=>row.SERVICE=='WORLDCITIES')[0].PORT, ()=>{
-				console.log(`MICROSERVICE WORLDCITIES PORT ${MICROSERVICE.filter(row=>row.SERVICE=='WORLDCITIES')[0].PORT} `);
-			});
+					}
+				});
+				break;
+			}
+			case req.url.startsWith('/worldcities/city/random'):{
+				IAM(req.query.app_id, req.headers.authorization).then(result=>{
+					if (result == 1)
+						getCityRandom(req, res);
+					else
+					{
+						res.statusCode = 401;
+						res.write('⛔', 'utf-8');
+						res.end();
+					}
+				});
+				break;
+			}
+			case req.url.startsWith('/worldcities/country/'):{
+				req.params.country = req.url.substring('/worldcities/country/'.length, req.url.indexOf('?'));
+				IAM(req.query.app_id, req.headers.authorization).then(result=>{
+					if (result == 1)
+						getCities(req, res);
+					else
+					{
+						res.statusCode = 401;
+						res.write('⛔', 'utf-8');
+						res.end();
+					}
+				});
+				break;
+			}
+			default:
+				res.end();
+				break;
+		}
+	}).listen(request.port, ()=>{
+		console.log(`MICROSERVICE WORLDCITIES PORT ${request.port} `);
+	});
 
-			process.on('uncaughtException', (err) =>{
-				console.log(err);
-			});
-		});
+	process.on('uncaughtException', (err) =>{
+		console.log(err);
 	});
 };
 const getCities = async (req, res) => {
