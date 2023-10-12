38542f7c13ae239c2ced9c6ca2474abb61c1267e
appportfolio <forsbergapp@protonmail.com>
2023-10-12 21:56:39 +0200

    APB-402 adds server/auth module, adds sql column alias in CheckLogin, updates types, removes return for res.status(), res.redirect() and next() function call in apps, server and server/auth modules

--- server/auth/admin/admin.controller.js
+++ server/auth/admin/admin.controller.js
@@ -1,12 +1,23 @@
+/** @module server/auth/admin */
+
+// eslint-disable-next-line no-unused-vars
+import * as Types from './../../../types.js';
+
 const {default:{sign, verify}} = await import('jsonwebtoken');
 
 const {CheckFirstTime, ConfigGet, ConfigGetUser, CreateSystemAdmin} = await import(`file://${process.cwd()}/server/server.service.js`);
 
+/**
+ * Middleware checks system admin token
+ * @param {Types.req} req
+ * @param {Types.res} res
+ * @param {function} next
+ */
 const checkSystemAdmin = (req, res, next) => {
     let token = req.get('authorization');
     if (token){
         token = token.slice(7);
-        verify(token, ConfigGet('SERVICE_AUTH', 'ADMIN_TOKEN_SECRET'), (err) => {
+        verify(token, ConfigGet('SERVICE_AUTH', 'ADMIN_TOKEN_SECRET'), (/**@type{Types.error}*/err) => {
             if (err){
                 res.status(401).send({
                     message: '⛔'
@@ -22,8 +33,13 @@ const checkSystemAdmin = (req, res, next) => {
         });
     }
 };
+/**
+ * Middleware checks system admin username and password
+ * @param {Types.req} req
+ * @param {Types.res} res
+ */
 const authSystemAdmin = (req, res) => {
-    const check_user = async (username, password) => {
+    const check_user = async (/**@type{string}*/username, /**@type{string}*/password) => {
         const { default: {compareSync} } = await import('bcryptjs');
         const config_username = ConfigGetUser('username');
         const config_password = ConfigGetUser('password');
@@ -31,16 +47,17 @@ const authSystemAdmin = (req, res) => {
             const jsontoken_at = sign ({tokentimstamp: Date.now()}, ConfigGet('SERVICE_AUTH', 'ADMIN_TOKEN_SECRET'), {
                                 expiresIn: ConfigGet('SERVICE_AUTH', 'ADMIN_TOKEN_EXPIRE_ACCESS')
                                 });
-            return res.status(200).json({ 
+            res.status(200).json({ 
                 token_at: jsontoken_at
             });
         }
         else{
-            return res.status(401).send('⛔');
+            res.status(401).send('⛔');
         }            
     };
-    if(req.headers.authorization){                
-        const userpass = new Buffer.from((req.headers.authorization || '').split(' ')[1] || '', 'base64').toString();
+    if(req.headers.authorization){       
+                 
+        const userpass =  Buffer.from((req.headers.authorization || '').split(' ')[1] || '', 'base64').toString();
         const username = userpass.split(':')[0];
         const password = userpass.split(':')[1];
         if (CheckFirstTime())
@@ -51,6 +68,6 @@ const authSystemAdmin = (req, res) => {
             check_user(username, password);
     }
     else
-        return res.status(401).send('⛔');
+        res.status(401).send('⛔');
 };
 export {checkSystemAdmin, authSystemAdmin};
