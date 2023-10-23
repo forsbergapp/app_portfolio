55273668360ddc2bc808367ab3a86bbb92f565c3
appportfolio <forsbergapp@protonmail.com>
2023-10-23 23:00:28 +0200

    APB-638 corrects datatype getUserAccountLogonAdmin

--- server/dbapi/app_portfolio/user_account_logon/user_account_logon.controller.js
+++ server/dbapi/app_portfolio/user_account_logon/user_account_logon.controller.js
@@ -3,7 +3,7 @@ const service = await import('./user_account_logon.service.js');
 const {getNumberValue} = await import(`file://${process.cwd()}/server/server.service.js`);
 
 const getUserAccountLogonAdmin = (req, res) => {
-	service.getUserAccountLogonAdmin(getNumberValue(req.query.app_id), getNumberValue(req.params.user_account_id), getNumberValue(req.params.app_id), (err, result) =>{
+	service.getUserAccountLogonAdmin(getNumberValue(req.query.app_id), getNumberValue(req.params.user_account_id), getNumberValue(req.params.app_id=='\'\''?'':req.params.app_id), (err, result) =>{
 		if (err) {
 			return res.status(500).send({
 				data: err
