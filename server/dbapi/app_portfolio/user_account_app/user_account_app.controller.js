2aeed97a5098f104f4b39ffb3d51349ecb06f62a
fapwork <fapwork@proton.me>
2023-10-30 20:46:22 +0100

    APB-402 updates types in dbapi/admin and dbapi/app_portfolio/user_account_app modules, corrects demo_add so password for users will be set, pool_start simplifies db 1+2 pool start and sets default value if any parameter is null, corrects pool parameter connectionLimit, updates array and uses app_id variable and updates comments in install_db_get_files, corrects checkAccessToken

--- server/dbapi/app_portfolio/user_account_app/user_account_app.controller.js
+++ server/dbapi/app_portfolio/user_account_app/user_account_app.controller.js
@@ -14,7 +14,7 @@ const {getNumberValue} = await import(`file://${process.cwd()}/server/server.ser
  */
 const createUserAccountApp = (req, res) => {
 	service.createUserAccountApp(getNumberValue(req.query.app_id), getNumberValue(req.body.user_account_id))
-	.then((result)=>{
+	.then((/**@type{Types.db_result_user_account_app_createUserAccountApp}*/result)=>{
 		res.status(200).json({
 			count: result.affectedRows,
 			items: Array(result)
@@ -33,7 +33,7 @@ const createUserAccountApp = (req, res) => {
  */
 const getUserAccountApps = (req, res) => {
 	service.getUserAccountApps(getNumberValue(req.query.app_id), getNumberValue(req.params.user_account_id))
-	.then((result)=>{
+	.then((/**@type{Types.db_result_user_account_app_getUserAccountApps[]}*/result)=>{
 		res.status(200).json({
 			count: result.length,
 			items: result
@@ -52,7 +52,7 @@ const getUserAccountApps = (req, res) => {
  */
 const getUserAccountApp = (req, res) => {
 	service.getUserAccountApp(getNumberValue(req.query.app_id), getNumberValue(req.params.user_account_id))
-	.then((result)=>{
+	.then((/**@type{Types.db_result_user_account_app_getUserAccountApp[]}*/result)=>{
 		res.status(200).json({
 			items: result
 		});
@@ -69,13 +69,14 @@ const getUserAccountApp = (req, res) => {
  * @param {Types.res} res 
  */
 const updateUserAccountApp = (req, res) => {
+	/**@type{Types.db_parameter_user_account_app_updateUserAccountApp} */
 	const data = {	setting_preference_direction_id: 	getNumberValue(req.body.setting_preference_direction_id),
 					setting_preference_arabic_script_id:getNumberValue(req.body.setting_preference_arabic_script_id),
 					setting_preference_timezone_id: 	getNumberValue(req.body.setting_preference_timezone_id),
 					preference_locale:					req.body.preference_locale
 				};
 	service.updateUserAccountApp(getNumberValue(req.query.app_id), getNumberValue(req.params.user_account_id), data)
-	.then((result)=>{
+	.then((/**@type{Types.db_result_user_account_app_updateUserAccountApp}*/result)=>{
 		res.status(200).json({
 			items: result
 		});
@@ -93,7 +94,7 @@ const updateUserAccountApp = (req, res) => {
  */
 const deleteUserAccountApps = (req, res) => {
 	service.deleteUserAccountApps(getNumberValue(req.query.app_id), getNumberValue(req.params.user_account_id), getNumberValue(req.params.app_id))
-	.then((result)=>{
+	.then((/**@type{Types.db_result_user_account_app_deleteUserAccountApps}*/result)=>{
 		res.status(200).send(
 			result
 		);
