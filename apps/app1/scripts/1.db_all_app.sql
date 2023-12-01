2f4e4f2bfe07d3a68d3f9584f30ec5ff098a85f0
fapwork <fapwork@proton.me>
2023-12-01 11:56:26 -0500

    APB-573 removes COMMON_GLOBAL.app_url, adds app_logo in get_module_with_init, sets app_logo in set_app_service_parameters, removes logo in css for admin and app2 and sets logo in init_app, removes app_name, url and logo from table app, updates data model

--- apps/app1/scripts/1.db_all_app.sql
+++ apps/app1/scripts/1.db_all_app.sql
@@ -2,7 +2,7 @@
 --
 -- app
 --
-INSERT INTO app_portfolio.app (id, app_name, url, logo, enabled, app_category_id) VALUES (1,'App Portfolio', 'https://localhost','/app1/images/logo.png',1, 1);
+INSERT INTO app_portfolio.app (id, enabled, app_category_id) VALUES (1, 1, 1);
 --
 -- setting_type
 --
