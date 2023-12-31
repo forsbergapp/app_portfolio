704452c64e2abbbd2544a633436562c56f39c1f6
fapwork <fapwork@proton.me>
2023-12-31 03:20:32 -0500

    APB-746 replaces microservice/logs/ with microservice/data/, moves MESSAGE_QUEUE_PUBLISH, MESSAGE_QUEUE_CONSUME and MESSAGE_QUEUE_ERROR from microservice/config.json to FILE_DB, file_get_log and file_append_log changes filesuffix parameter logic and adds sample for IAM, LOG and MESSAGE_QUEUE, adds getFilesuffix(), changes batchlog name to PATH_DATA/[SERVICENAME]_[DATE].log, renames geolocation cache files to PATH_DATA/GEOLOCATION_geodata_cache_ip.log and PATH_DATA/GEOLOCATION_geodata_cache_place.log

--- microservice/geolocation/service.js
+++ microservice/geolocation/service.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 import * as Types from './../../types.js';
 
-const {ConfigServices} = await import(`file://${process.cwd()}/microservice/microservice.service.js`);
+const {CONFIG, ConfigServices} = await import(`file://${process.cwd()}/microservice/microservice.service.js`);
 
 /**
  * 
@@ -71,12 +71,12 @@ const getGeodataEmpty = (geotype) => {
 const getCacheGeodata = async (cachetype, ip, latitude, longitude) =>{
     const fs = await import('node:fs');
     /**@type{Types.microservice_config_service_record}*/
-    const config = ConfigServices('GEOLOCATION');
+    const config_service = ConfigServices('GEOLOCATION');
     let geodata_cache;
     try {
         switch (cachetype){
             case 'IP':{
-                geodata_cache = await fs.promises.readFile(`${process.cwd()}${config.PATH}geodata_cache_ip.json`, 'utf8');
+                geodata_cache = await fs.promises.readFile(`${process.cwd()}${CONFIG.PATH_DATA}${config_service.NAME}_geodata_cache_ip.log`, 'utf8');
                 geodata_cache = geodata_cache.split('\r\n');
                 for (const row of geodata_cache){
                     const row_obj = JSON.parse(row);
@@ -88,7 +88,7 @@ const getCacheGeodata = async (cachetype, ip, latitude, longitude) =>{
                 return null;
             }
             case 'PLACE':{
-                geodata_cache = await fs.promises.readFile(`${process.cwd()}${config.PATH}geodata_cache_place.json`, 'utf8');
+                geodata_cache = await fs.promises.readFile(`${process.cwd()}${CONFIG.PATH_DATA}${config_service.NAME}_geodata_cache_place.log`, 'utf8');
                 geodata_cache =  geodata_cache.split('\r\n');
                 /**
                  * 
@@ -142,15 +142,15 @@ const getCacheGeodata = async (cachetype, ip, latitude, longitude) =>{
 const writeCacheGeodata = async (cachetype, geodata) =>{
     const fs = await import('node:fs');
     /**@type{Types.microservice_config_service_record}*/
-    const config = ConfigServices('GEOLOCATION');
+    const config_service = ConfigServices('GEOLOCATION');
     switch (cachetype){
         case 'IP':{
-            await fs.promises.appendFile(`${process.cwd()}${config.PATH}geodata_cache_ip.json`, 
+            await fs.promises.appendFile(`${process.cwd()}${CONFIG.PATH_DATA}${config_service.NAME}_geodata_cache_ip.log`, 
                                                           JSON.stringify(JSON.parse(geodata)) +'\r\n', 'utf8');
             break;
         }
         case 'PLACE':{
-            await fs.promises.appendFile(`${process.cwd()}${config.PATH}geodata_cache_place.json`, 
+            await fs.promises.appendFile(`${process.cwd()}${CONFIG.PATH_DATA}${config_service.NAME}_geodata_cache_place.log`, 
                                                              JSON.stringify(JSON.parse(geodata)) +'\r\n', 'utf8');
             break;
         }
