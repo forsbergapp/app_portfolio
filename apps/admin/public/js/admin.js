3fdec45ae551c7858b58206e19a049d6521a2a42
fapwork <fapwork@proton.me>
2024-01-20 23:24:53 -0500

    APB-769 uses system_admin and admin classes on select_broadcast_type and menu_secure and defines available menuitems in css

--- apps/admin/public/js/admin.js
+++ apps/admin/public/js/admin.js
@@ -4,8 +4,8 @@ const app_secure = await import('app_secure');
 const admin_logoff_app = () => {
     const clear_common = () => {
         app_secure.delete_globals();
-        document.querySelector('#select_broadcast_type').classList.remove('system_admin');
-        
+        document.querySelector('#select_broadcast_type').classList.remove('admin','system_admin');
+        document.querySelector('#menu_secure').classList.remove('admin','system_admin');
         document.querySelectorAll('.main_content').forEach(content => {
             content.innerHTML = '';
         });
