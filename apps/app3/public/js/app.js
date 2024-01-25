bbd564c725ea536a4ddebb2bb11d7a535d9cb2ec
fapwork <fapwork@proton.me>
2024-01-25 10:16:43 -0500

    APB-671 adds typescript to app3-6 and default templates, adds missing return void in admin, replaces event function with commonly with null in mount_app in app4, moves types first in files, excludes thirdparty modules and apps/default in tsconfig.json

--- apps/app3/public/js/app.js
+++ apps/app3/public/js/app.js
@@ -1,4 +1,30 @@
+/**@type{{body:{className:string, classList:{add:function}},
+ *        querySelector:function}} */
+ const AppDocument = document;
+
+ /**
+ * @typedef {object}        AppEvent
+ * @property {string}       code
+ * @property {function}     preventDefault
+ * @property {function}     stopPropagation
+ * @property {{ id:                 string,
+  *              innerHTML:          string,
+  *              value:              string,
+  *              parentNode:         {nextElementSibling:{querySelector:function}},
+  *              nextElementSibling: {dispatchEvent:function},
+  *              focus:              function,
+  *              blur:               function,
+  *              getAttribute:       function,
+  *              setAttribute:       function,
+  *              dispatchEvent:      function,
+  *              classList:          {contains:function}
+  *              className:          string
+  *            }}  target
+  */
+
+/**@ts-ignore */
 const common = await import('common');
+
 const APP_GLOBAL = {
                     'docs':[{'id':1,
                             'doc_title':'Diagram',
@@ -15,11 +41,22 @@ const APP_GLOBAL = {
                             ]
 };
 Object.seal(APP_GLOBAL);
+/**
+ * App exception function
+ * @param {Error} error 
+ * @returns {void}
+ */
 const app_exception = (error) => {
     common.show_message('EXCEPTION', null, null, null, error);
 };
+ 
+/**
+ * Get docs
+ * @param {*} docid 
+ * @returns {void}
+ */
 const getdocs = (docid = null) => {
-    document.querySelector('#doc_list').classList.add('css_spinner');
+    AppDocument.querySelector('#doc_list').classList.add('css_spinner');
     let html ='';
     for (const doc of APP_GLOBAL.docs) {
         if (docid== doc.id || docid==null)
@@ -28,13 +65,17 @@ const getdocs = (docid = null) => {
                         <div class='doc_list_item_title'>${doc.doc_title}</div>
                     </div>`;
     }
-    document.querySelector('#doc_list').classList.remove('css_spinner');
-    document.querySelector('#doc_list').innerHTML = html;
+    AppDocument.querySelector('#doc_list').classList.remove('css_spinner');
+    AppDocument.querySelector('#doc_list').innerHTML = html;
 };
+/**
+ * App event click
+ * @param {AppEvent} event 
+ * @returns {void}
+ */
 const app_event_click = event => {
     if (event==null){
-        //javascript framework
-        document.querySelector('#app').addEventListener('click',(event) => {
+        AppDocument.querySelector('#app').addEventListener('click',(/**@type{AppEvent}*/event) => {
             app_event_click(event);
         });
     }
@@ -56,7 +97,7 @@ const app_event_click = event => {
                     break;
                 }
                 case 'common_window_info_btn_close':{
-                    document.querySelector('#dialogue_documents').style.visibility = 'visible';
+                    AppDocument.querySelector('#dialogue_documents').style.visibility = 'visible';
                     break;
                 }
                 case 'doc_list':{
@@ -69,7 +110,12 @@ const app_event_click = event => {
         });
     }
 };
-const mount_app_app = async framework => {
+/**
+ * Mount app
+ * @param {string|null} framework 
+ * @returns {Promise.<void>}
+ */
+const mount_app_app = async (framework=null) => {
     await common.mount_app(framework,
         {   Click: app_event_click,
             Change: null,
@@ -81,17 +127,28 @@ const mount_app_app = async framework => {
         getdocs();
         const docid = window.location.pathname.substring(1);
         if (docid!=''){
-            document.querySelector('#dialogue_documents').style.visibility = 'hidden';
-            common.show_window_info(0, document.querySelector(`#doc_${docid}`).getAttribute('full_size'));
+            AppDocument.querySelector('#dialogue_documents').style.visibility = 'hidden';
+            common.show_window_info(0, AppDocument.querySelector(`#doc_${docid}`).getAttribute('full_size'));
         }
         else
-            document.querySelector('#dialogue_documents').style.visibility = 'visible';
+            AppDocument.querySelector('#dialogue_documents').style.visibility = 'visible';
     });
 };
+/**
+ * Init app
+ * @returns {Promise.<void>}
+ */
 const init_app = async () => {
     mount_app_app();
 };
-const init = (parameters) => {
+/**
+ * Init common
+ * @param {{app:{   parameter_name:string, 
+ *                  parameter_value:string}[],
+ *          app_service:{system_admin_only:number, first_time:number}}} parameters 
+ * @returns {void}
+ */
+const init = parameters => {
     common.COMMON_GLOBAL.exception_app_function = app_exception;
     common.init_common(parameters).then(()=>{
         init_app();
