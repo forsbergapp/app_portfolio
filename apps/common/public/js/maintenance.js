af06eeb5e66e6a66745f7eaeaa52268b8d9b9595
appportfolio <forsbergapp@protonmail.com>
2024-01-25 10:43:33 -0500

    APB-671 adds typescript to regional.js and maintenance.js, adds common_app_id in parameters and modules to importmap for maintenance

--- apps/common/public/js/maintenance.js
+++ apps/common/public/js/maintenance.js
@@ -1,14 +1,29 @@
+/**@type{{body:{className:string, classList:{add:function}},
+ *        querySelector:function,
+ *        title: string}} */
+ const AppDocument = document;
+
+/**@ts-ignore */
 const common = await import('common');
+/**
+ * App exception function
+ * @returns {void}
+ */
 const app_exception = () => {
     null;
 };
+/**
+ * Init common
+ * @param {{app_id:number,
+ *          common_app_id:number,
+ *          rest_resource_bff:string}} parameters 
+ * @returns {void}
+ */
 const init = (parameters) => {
+    AppDocument.title = '⚒';
+    AppDocument.querySelector('#common_maintenance_footer').innerHTML= '';
     
-
-    document.title = '⚒';
-    document.querySelector('#common_maintenance_footer').innerHTML= '';
-    
-    common.COMMON_GLOBAL.common_app_id= parseInt(parameters.common_app_id);
+    common.COMMON_GLOBAL.common_app_id= parameters.common_app_id;
     common.COMMON_GLOBAL.app_id = parameters.app_id;
     common.COMMON_GLOBAL.exception_app_function = app_exception; 
     common.COMMON_GLOBAL.rest_resource_bff = parameters.rest_resource_bff;
