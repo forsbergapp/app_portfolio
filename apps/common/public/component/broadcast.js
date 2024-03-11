ab51602e6e38843f8887615176feea09f04a5e17
fapwork <fapwork@proton.me>
2024-03-11 18:53:15 -0500

    APB-576 adds ComponentRender and ComponentRemove in common.js, removes show_broadcast_alert, removes visibilty attribute for common_broadcast_info in css, replaces broadcast html with component /common/component/broadcast.js with template, method and export default syntax, adds broadcast_close event in maintenance.js, removes component innerHTML when closed

---
+++ apps/common/public/component/broadcast.js
@@ -0,0 +1,44 @@
+const template =`<div id='common_broadcast_info'>
+                    <div id='common_broadcast_banner'>
+                        <div id='common_broadcast_header'>
+                            <div id='common_broadcast_info_message'>
+                                <div id='common_broadcast_info_message_item'><MESSAGE/></div>
+                            </div>    
+                        </div>
+                        <div id='common_broadcast_footer'>
+                            <div id='common_broadcast_info_logo'></div>
+                            <div id='common_broadcast_info_title' class='common_icon'></div>
+                            <div id='common_broadcast_close' class='common_icon'></div>
+                        </div>
+                    </div>
+                </div>`;
+/**
+ * 
+ * @param {*} props 
+ */
+const method = (props) => {
+    switch (props.common_framework){
+        case 2:{
+            //Vue
+            //Use tempmount div to be able to return pure HTML
+            //props.common_document.querySelector(`#${props.common_mountdiv}`).innerHTML = `<div id='tempmount'></div>`;
+            //Vue.createApp(...
+            //return props.common_document.querySelector('#tempmount').innerHTML;
+            return template.replace('<MESSAGE/>',props.message,)
+        }
+        case 3:{
+            //React
+            //Use tempmount div to be able to return pure HTML
+            //props.document.querySelector(`#${props.common_mountdiv}`).innerHTML = `<div id='tempmount'></div>`;
+            //ReactDOM.createRoot(div... .render( App()
+            //return props.common_document.querySelector('#tempmount').innerHTML;
+            return template.replace('<MESSAGE/>',props.message,)
+        }
+        case 1:
+        default:{
+            //Default Javascript
+            return template.replace('<MESSAGE/>',props.message,)
+        }
+    }
+}
+export default method;
