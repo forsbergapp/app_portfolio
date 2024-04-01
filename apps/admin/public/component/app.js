f2ecbca5dc99f809b67e75851a5d1ad36ab7b9ba
fapwork <fapwork@proton.me>
2024-04-01 16:53:11 -0500

    APB-804 creates admin_secure component and mounts when logged in, mounts dialogue_send_broadcast component when needed and adds system_admin and apps props, replaces old apps dialogue class names with common_dialogue class names

--- apps/admin/public/component/app.js
+++ apps/admin/public/component/app.js
@@ -1,40 +1,6 @@
 /**@type{{querySelector:function}} */
 const AppDocument = document;
-const template =`   <div id='dialogues'>
-                        <div id='dialogue_send_broadcast' class='dialogue'></div>
-                    </div>
-                    <div id='menu_open' class='common_dialogue_button common_icon'></div>
-                    <div id='menu'>
-                        <div id='app_user_account'></div>
-                        <div id='menu_secure'>
-                            <div id='menu_close' class='common_dialogue_button common_icon'></div>
-                            <div id='menu_1' class='menuitem common_icon'></div>
-                            <div id='menu_2' class='menuitem common_icon'></div>
-                            <div id='menu_3' class='menuitem common_icon'></div>
-                            <div id='menu_4' class='menuitem common_icon'></div>
-                            <div id='menu_5' class='menuitem common_icon'></div>
-                            <div id='menu_6' class='menuitem common_icon'></div>
-                            <div id='menu_7' class='menuitem common_icon'></div>
-                            <div id='menu_8' class='menuitem common_icon'></div>
-                            <div id='menu_9' class='menuitem common_icon'></div>
-                            <div id='menu_10' class='menuitem common_icon'></div>
-                            <div id='menu_11' class='menuitem common_icon'></div>
-                        </div>
-                    </div>
-                    <div id='admin_secure'>
-                        <div id='main'>
-                            <div id='menu_1_content' class='main_content'></div>
-                            <div id='menu_2_content' class='main_content'></div>
-                            <div id='menu_3_content' class='main_content'></div>
-                            <div id='menu_4_content' class='main_content'></div>
-                            <div id='menu_5_content' class='main_content'></div>
-                            <div id='menu_6_content' class='main_content'></div>
-                            <div id='menu_7_content' class='main_content'></div>
-                            <div id='menu_8_content' class='main_content'></div>
-                            <div id='menu_9_content' class='main_content'></div>
-                            <div id='menu_10_content' class='main_content'></div>
-                        </div>
-                    </div>`;
+const template =`   <div id='admin_secure'></div>`;
 /**
  * 
  * @param {{common_document:AppDocument,
