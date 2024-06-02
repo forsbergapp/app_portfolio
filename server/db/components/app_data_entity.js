d76e3e5c912502c477d82101dccb5477e0791346
fapwork <fapwork@proton.me>
2024-06-02 13:46:35 +0200

    APB-892 adds resource_id_string in endpoints for app_data_resource_master, app_data_resource_detail and app_data_resource_detail_data and GET method

--- server/db/components/app_data_entity.js
+++ server/db/components/app_data_entity.js
@@ -13,27 +13,29 @@ const {getNumberValue} = await import(`file://${process.cwd()}/server/server.ser
 /**
  * 
  * @param {number} app_id 
+ * @param {number|null} resource_id
  * @param {*} query
  * @returns {Promise.<import('../../../types.js').db_result_app_data_entity_get[]>}
  */
-const getEntity = (app_id, query) => service.get(   app_id, 
-                                                    getNumberValue(query.get('id')), 
-                                                    getNumberValue(query.get('data_app_id')), 
-                                                    query.get('lang_code'))
-                                                .catch((/**@type{import('../../../types.js').error}*/error)=>{throw error;});
+const getEntity = (app_id, resource_id, query) => service.get(  app_id, 
+                                                                resource_id, 
+                                                                getNumberValue(query.get('data_app_id')), 
+                                                                query.get('lang_code'))
+                                                    .catch((/**@type{import('../../../types.js').error}*/error)=>{throw error;});
 
 /**
  * 
  * @param {number} app_id 
+ * @param {number|null} resource_id
  * @param {*} query
  * @returns {Promise.<import('../../../types.js').db_result_app_data_entity_resource_get[]>}
  */
-const getEntityResource = (app_id, query) => app_data_entity_resource.get(  app_id, 
-                                                                            getNumberValue(query.get('id')), 
-                                                                            getNumberValue(query.get('data_app_id')), 
-                                                                            query.get('resource_name'), 
-                                                                            getNumberValue(query.get('entity_id')), 
-                                                                            query.get('lang_code'))
-                                                .catch((/**@type{import('../../../types.js').error}*/error)=>{throw error;});
+const getEntityResource = (app_id, resource_id, query) => app_data_entity_resource.get( app_id, 
+                                                                                        resource_id, 
+                                                                                        getNumberValue(query.get('data_app_id')), 
+                                                                                        query.get('resource_name'), 
+                                                                                        getNumberValue(query.get('entity_id')), 
+                                                                                        query.get('lang_code'))
+                                                            .catch((/**@type{import('../../../types.js').error}*/error)=>{throw error;});
 
 export{getEntity, getEntityResource};
