a2ce0a0dd6e65601e46413c944a3b92e5a92e53c
fapwork <fapwork@proton.me>
2024-06-03 16:52:32 +0200

    APB-886 implements fields functionality in BFF that limits columns for rows or keys for a returned object if fields query parameters is used, deletes fields logic in getTransacions

--- apps/app5/src/functions/account_transactions.js
+++ apps/app5/src/functions/account_transactions.js
@@ -11,21 +11,6 @@ const getTransacions = async (app_id, data, locale) =>{
     /**@type{import('../../../../server/db/sql/app_data_resource_detail_data.service.js')} */
     const {get} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_detail_data.service.js`);
 
-    const transactions = await get(app_id, null, data.user_account_id, data.data_app_id, 'ACCOUNT', null, null, null, false);
-    
-    if (data.fields){
-        const transaction_fields = transactions.rows.map((/**@type{*}*/row)=>{
-            let transaction_new = {};
-            for (const field of data.fields.split(',')){
-                /**@ts-ignore */
-                transaction_new[field] = row[field];
-            }
-            return transaction_new;
-        })
-        return {list_header: transactions.list_header,
-                rows: transaction_fields};
-    }
-    else
-        return transactions;
+    return await get(app_id, null, data.user_account_id, data.data_app_id, 'ACCOUNT', null, null, null, false);
 } 
 export default getTransacions;
