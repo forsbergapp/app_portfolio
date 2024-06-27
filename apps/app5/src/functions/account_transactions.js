eb8b53c54657a6ba28137f18097ef7ce97834ebf
fapwork <fapwork@proton.me>
2024-06-27 23:06:55 +0200

    APB-773 updates get() in app_data_resource_detail_data.service.js with more parameters and joins in SQL, adds try catch and removes \r\n in json_data in db_execute

--- apps/app5/src/functions/account_transactions.js
+++ apps/app5/src/functions/account_transactions.js
@@ -11,6 +11,9 @@ const getTransacions = async (app_id, data, locale) =>{
     /**@type{import('../../../../server/db/sql/app_data_resource_detail_data.service.js')} */
     const {get} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_detail_data.service.js`);
 
-    return await get(app_id, null, data.user_account_id, data.data_app_id, 'ACCOUNT', null, null, null, false);
+    return await get(app_id, null, null, data.user_account_id, data.data_app_id, 
+                    'RESOURCE_TYPE', 'ACCOUNT', 
+                    'RESOURCE_TYPE', 'CUSTOMER', 
+                    null, null, null, null, false);
 }; 
 export default getTransacions;
