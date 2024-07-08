02f837084558d0686e43c99bc2aa213d9ce8f807
fapwork <fapwork@proton.me>
2024-07-09 01:11:11 +0200

    APB-773 adds PAYMENT_REQUEST_EXPIRE parameter for app 6, adds RESOURCE_TYPE CURRENCY and resource CURRENCY with APPEURO as currency for app 5 and app6, adds type for account_create.js, removes common_dialogue_user_menu_token_countdown_icon, adds payment_request_create.js, payment_request() validates VPA and calls PAYMENT_REQUEST_CREATE with temporary data, PRODUCT_GET returns currency info and app adjusts css, adds ip parameter to app-function and functions used

--- apps/app5/src/functions/customer_metadata.js
+++ apps/app5/src/functions/customer_metadata.js
@@ -2,13 +2,14 @@
 /**
  * @param {number} app_id
  * @param {*} data
+ * @param {string} ip
  * @param {string} locale
  * @returns {Promise.<import('../../../../types.js').db_result_app_data_resource_master_get[]>}
  */
-const customer_metadata = async (app_id, data, locale) =>{
+const customer_metadata = async (app_id, data, ip, locale) =>{
     /**@type{import('../../../../server/db/sql/app_data_resource_master.service.js')} */
     const {get} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_master.service.js`);
     
-    return await get(app_id, data.resource_id, data.user_account_id, data.data_app_id, 'CUSTOMER', data.entity_id, data.locale, true);
+    return await get(app_id, data.resource_id, data.user_account_id, data.data_app_id, 'CUSTOMER', data.entity_id, locale, true);
 };
 export default customer_metadata;
