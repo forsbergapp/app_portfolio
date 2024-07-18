92adb8acd24aecd655717413d337c2413c26f50c
forsbergapp <forsbergapp@protonmail.com>
2024-07-19 00:10:02 +0200

    APB-773 replaces PAYMENT_ACCEPT and PAYMENT_CANCEL with PAYMENT_REQUEST_UPDATE and adds payment_request_update.js, renames PAYMENT_REQUEST_GET to PAYMENT_REQUEST_GET_STATUS including filenames and adds PAYMENT_REQUEST_GET with payment_request_get.js for APP_ACCESS in app5

--- apps/app5/src/functions/payment_request_get.js
+++ apps/app5/src/functions/payment_request_get.js
@@ -1,56 +1,39 @@
 /** @module apps/app5 */
 /**
  * @param {number} app_id
- * @param {{id:string,
-*          message:string}} data
-* @param {string} user_agent
-* @param {string} ip
-* @param {string} locale
-* @param {import('../../../../types.js').res} res
-* @returns {Promise.<{message:string}[]>}
-*/
+ * @param {{data_app_id:number,
+ *          user_account_id:number,
+ *          payment_request_id: string}} data
+ * @param {string} user_agent
+ * @param {string} ip
+ * @param {string} locale
+ * @param {import('../../../../types.js').res} res
+ * @returns {Promise.<import('../../../../types.js').db_result_app_data_resource_master_get[]>}
+ */
 const payment_request_get = async (app_id, data, user_agent, ip, locale, res) =>{
-     
-   /**@type{import('../../../../server/db/sql/app_data_resource_master.service.js')} */
-   const {get:MasterGet} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_master.service.js`);
+    /**@type{import('../../../../server/db/sql/app_data_resource_master.service.js')} */
+    const {get:MasterGet} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_master.service.js`);
+    
+    /**@type{import('../../../../server/db/sql/app_data_resource_detail.service.js')} */
+    const {get:DetailGet} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_detail.service.js`);
 
-   /**@type{import('../../../../server/security.service')} */
-   const {PrivateDecrypt, PublicEncrypt} = await import(`file://${process.cwd()}/server/security.service.js`);
-
-   const merchant = await MasterGet(app_id, null, null, app_id, 'MERCHANT', null, locale, false)
-                           .then(result=>result.map(merchant=>JSON.parse(merchant.json_data)).filter(merchant=>merchant.merchant_id==data.id)[0]);
-   if (merchant){
-        /** 
-        * @type {{ api_secret:           string,
-        *          payment_request_id:   string}}
-        */
-        const  body_decrypted = JSON.parse(PrivateDecrypt(merchant.merchant_private_key, data.message));
-        if (merchant.merchant_api_secret==body_decrypted.api_secret){
-            const payment_request = await MasterGet(app_id, null, null, app_id, 'PAYMENT_REQUEST', null, locale, true)
-                                            .then(result=>result.map(payment_request=>JSON.parse(payment_request.json_data)).filter(payment_request=>payment_request.payment_request_id==body_decrypted.payment_request_id)[0]);
-            
-            if (payment_request){
-                    /**
-                     * @type {{ status:string}}
-                     */
-                    const data_return = {   status:                 payment_request.status
-                    };
-                    const data_encrypted = PublicEncrypt(merchant.merchant_public_key, JSON.stringify(data_return));
-                    return [{message:data_encrypted}];
-            }
-            else{
-                    res.statusCode = 404;
-                    throw '⛔';
-                }
-        }
+    const payment_request = await MasterGet(app_id, null, null, data.data_app_id, 'PAYMENT_REQUEST', null, locale, true)
+                                    /**@ts-ignore */
+                                    .then(result=>result.filter(payment_request=>payment_request.payment_request_id==data.payment_request_id)[0]);
+    if (payment_request){
+        const account_payer =  await DetailGet(app_id, null, null, data.user_account_id, data.data_app_id, 'ACCOUNT', null, locale, false)
+                                        /**@ts-ignore */
+                                        .then(result=>result.filter(result=>result.bank_account_vpa == payment_request.payerid)[0]);
+        if (account_payer)
+            return  [payment_request];
         else{
             res.statusCode = 404;
             throw '⛔';
         }
-   }
-   else{
-       res.statusCode = 404;
-       throw '⛔';
-   }
+    }
+    else{
+        res.statusCode = 404;
+        throw '⛔';
+    }
 };
 export default payment_request_get;
---
+++ apps/app5/src/functions/payment_request_update.js
@@ -0,0 +1,110 @@
+/** @module apps/app5 */
+/**
+ * @param {number} app_id
+ * @param {{data_app_id:number,
+ *          user_account_id:number,
+ *          payment_request_id:string,
+ *          status:1|0}} data
+ * @param {string} user_agent
+ * @param {string} ip
+ * @param {string} locale
+ * @param {import('../../../../types.js').res} res
+ * @returns {Promise.<{status:string}[]>}
+ */
+const payment_request_update = async (app_id, data, user_agent, ip, locale, res) =>{
+
+    /**@type{import('../../../../server/server.service.js')} */
+    const {getNumberValue} = await import(`file://${process.cwd()}/server/server.service.js`);
+
+    /**@type{import('../../../../server/db/sql/app_data_resource_master.service.js')} */
+    const {get:MasterGet, update:MasterUpdate} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_master.service.js`);
+
+    /**@type{import('../../../../server/db/sql/app_data_resource_detail.service.js')} */
+    const {get:DetailGet} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_detail.service.js`);
+
+    /**@type{import('../../../../server/db/sql/app_data_resource_detail_data.service.js')} */
+    const {get:DetailDataGet, post:DetailDataPost} = await import(`file://${process.cwd()}/server/db/sql/app_data_resource_detail_data.service.js`);
+
+    const customer = await MasterGet(app_id, null, data.user_account_id, data.data_app_id, 'CUSTOMER', null, locale, false)
+                            .then(result=>result[0]);
+    const payment_request = await MasterGet(app_id, null, null, data.data_app_id, 'PAYMENT_REQUEST', null, locale, true)
+                            .then(result=>result
+                                            /**@ts-ignore */
+                                            .filter(payment_request=>payment_request.payment_request_id==data.payment_request_id)[0]);
+
+    if (customer && payment_request && (getNumberValue(data.status)==1 || getNumberValue(data.status)==0)){
+        let status ='PENDING';
+        if (getNumberValue(data.status)==1)
+            try {
+                const account_payer         =  await DetailGet(app_id, null, customer.id, data.user_account_id, data.data_app_id, 
+                                                                'ACCOUNT', null, locale, false).then(result=>result[0]);
+                const account_payer_saldo   =  await DetailDataGet(app_id, null, account_payer.id, data.user_account_id, data.data_app_id, 
+                                                                    'RESOURCE_TYPE', 'ACCOUNT', 'RESOURCE_TYPE', 'CUSTOMER', null, null, null, locale, false)
+                                                                    .then(result=>result.reduce((balance, current_row)=>balance += 
+                                                                                                    /**@ts-ignore */
+                                                                                                    (current_row.amount_deposit ?? current_row.amount_withdrawal) ?? 0,0));
+                /**@ts-ignore */                                                
+                if ((account_payer_saldo - payment_request.amount) <0){
+                    status='NO FUNDS';
+                }
+                else{
+                    const data_debit = {json_data                               : { timestamp:new Date().toISOString(),
+                                                                                    logo:'',
+                                                                                    /**@ts-ignore */
+                                                                                    origin:payment_request.reference,
+                                                                                    amount_deposit:null,
+                                                                                    /**@ts-ignore */
+                                                                                    amount_withdrawal:payment_request.amount},
+                                        user_account_id                         : data.user_account_id,
+                                        data_app_id                             : data.data_app_id,
+                                        app_data_resource_detail_id             : account_payer.id,
+                                        app_data_resource_master_attribute_id   : null
+                                        };
+                    //create DEBIT transaction PAYERID resource TRANSACTION
+                    await DetailDataPost(app_id, data_debit);
+
+                    const account_payee         =  await DetailGet(app_id, null, null, null, data.data_app_id, 
+                                                                    'ACCOUNT', null, locale, false)
+                                                            /**@ts-ignore */
+                                                            .then(result=>result.filter(account=>account.bank_account_vpa == payment_request.payeeid)[0]);
+                    const data_credit = {   json_data                               : { timestamp:new Date().toISOString(),
+                                                                                        logo:'',
+                                                                                        /**@ts-ignore */
+                                                                                        origin:payment_request.reference,
+                                                                                        /**@ts-ignore */
+                                                                                        amount_deposit:payment_request.amount,
+                                                                                        amount_withdrawal:null},
+                                                                                    
+                                            user_account_id                         : account_payee.user_account_app_user_account_id,
+                                            data_app_id                             : data.data_app_id,
+                                            app_data_resource_detail_id             : account_payer.id,
+                                            app_data_resource_master_attribute_id   : null
+                                            };
+                    //create CREDIT transaction PAYEEID resource TRANSACTION
+                    await DetailDataPost(app_id, data_credit);
+                    status = 'PAID';
+                }
+            } catch (error) {
+                status = 'FAILED';
+            }
+        else
+            status='CANCELLED';
+
+        const data_payment_request = {  json_data                                       : JSON.parse(payment_request.json_data),
+                                        user_account_id                                 : data.user_account_id,
+                                        data_app_id                                     : data.data_app_id,
+                                        app_data_entity_resource_app_data_entity_id     : payment_request.app_data_entity_resource_app_data_entity_id,
+                                        app_data_entity_resource_id                     : payment_request.app_data_entity_resource_id
+                                        };
+        //update status in json_data column
+        data_payment_request.json_data.status = status;
+        //update payment request
+        await MasterUpdate(app_id, payment_request.id, data_payment_request);
+        return [{status:status}];
+   }
+   else{
+       res.statusCode = 404;
+       throw '⛔';
+   }
+};
+export default payment_request_update;
