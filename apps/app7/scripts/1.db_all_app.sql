569a7ecbea1877c329ba99fff4782a8ad479d97c
fapwork <fapwork@proton.me>
2024-08-13 20:55:44 +0200

    APB-936 adds new cube app 7 using default images from template, adds Other to framework_set in common sets default null for event in common_event

---
+++ apps/app7/scripts/1.db_all_app.sql
@@ -0,0 +1,5 @@
+-- COMMON
+--
+-- app
+--
+INSERT INTO <DB_SCHEMA/>.app (id, app_category_id) VALUES (<APP_ID/>, 8);
---
+++ apps/app7/scripts/2.db_all_app_hi.sql
@@ -0,0 +1,4 @@
+--
+-- app_translation app
+--
+INSERT INTO <DB_SCHEMA/>.app_translation (app_id, language_id, json_data) VALUES (<APP_ID/>,201,'{"name":"क्यूब", "description":"क्यूब"}');
