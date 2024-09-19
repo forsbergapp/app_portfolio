31948fa110c929123236d6d919520bde6dc9a0eb
forsbergapp <forsbergapp@protonmail.com>
2024-09-19 16:16:50 +0200

    APB-973 profile_detail calls new component profile_detail (previous unused code was copied select component) or sets empty common_profile_detail_list if showing other app specific, adjusts css in profile stat and replaces hear divs to icon within profile settings links, displays private profile for current logged in user, removes fetch_detail parameter not used anymore

--- apps/common/public/component/profile_detail.js
+++ apps/common/public/component/profile_detail.js
@@ -4,83 +4,152 @@
 /**
  * Displays profile detail
  * @param {{spinner:string,
-*           default_data_value:string,
-*           default_value:string,
-*           options:[{value:string, text:string}]|[],
-*           column_value:string,
-*           column_text:string}} props
-*/
-const template = props => ` <div class='common_select_dropdown'>
-                               <div class='common_select_dropdown_value' data-value='${props.default_data_value}'>${props.default_value}</div>
-                               <div class='common_select_dropdown_icon common_icon'></div>
-                           </div>
-                           <div class='common_select_options ${props.spinner}'>
-                               ${props.options.map(row=>
-                                   /**@ts-ignore */
-                                   `<div class='common_select_option' data-value='${row[props.column_value]}'>${row[props.column_text]}</div>`).join('')
-                               }
-                           </div>` ;
+ *          user_account_id:number,
+ *          user_account_id_profile:number,
+ *          detailchoice:number,
+ *          list:[{ id:number|null, 
+ *                  APP_ID:number, 
+ *                  PROTOCOL:string, 
+ *                  SUBDOMAIN:string, 
+ *                  HOST:string, 
+ *                  PORT:string,
+ *                  LOGO:string,
+ *                  NAME:string,
+ *                  date_created:string,
+ *                  avatar:string,
+ *                  provider_image:string,
+ *                  username:string}]|[]}} props
+ */
+const template = props => ` ${props.spinner==''?
+                                `${props.list.map(row=>
+                                    `${props.detailchoice==5 && typeof row.id =='undefined'?
+                                        `<div data-app_id='${row.APP_ID}' data-url='${row.PROTOCOL}${row.SUBDOMAIN}.${row.HOST}:${row.PORT}' class='common_profile_detail_list_row common_row'>
+                                            <div class='common_profile_detail_list_col'>
+                                                <div class='common_profile_detail_list_app_id'>${row.APP_ID}</div>
+                                            </div>
+                                            <div class='common_profile_detail_list_col'>
+                                                <div class='common_image common_image_avatar_list' style='background-image:url("${row.LOGO}");'></div>
+                                            </div>
+                                            <div class='common_profile_detail_list_col'>
+                                                <div class='common_profile_detail_list_app_name common_wide_list_column common_link'>
+                                                    ${row.NAME}
+                                                </div>
+                                            </div>
+                                            <div class='common_profile_detail_list_col'>
+                                                ${props.user_account_id==props.user_account_id_profile?
+                                                    '<div class=\'common_profile_detail_list_app_delete common_icon\'></div>':
+                                                    ''
+                                                }
+                                            </div>
+                                            <div class='common_profile_detail_list_col'>
+                                                <div class='common_profile_detail_list_date_created'>${row.date_created}</div>
+                                            </div>
+                                        </div>`:
+                                        `<div data-user_account_id='${row.id}' class='common_profile_detail_list_row common_row'>
+                                            <div class='common_profile_detail_list_col'>
+                                                <div class='common_profile_detail_list_user_account_id'>${row.id}</div>
+                                            </div>
+                                            <div class='common_profile_detail_list_col'>
+                                                <div class='common_image common_image_avatar_list' style='background-image:url("${row.avatar ?? row.provider_image}");'></div>
+                                            </div>
+                                            <div class='common_profile_detail_list_col'>
+                                                <div class='common_profile_detail_list_username common_wide_list_column common_link'>
+                                                    ${row.username}
+                                                </div>
+                                            </div>
+                                        </div>`
+                                    }`
+                                ).join('')
+                                }`:
+                                `<div class='${props.spinner}'</div>`
+                            }
+                            ` ;
 /**
 * 
 * @param {{common_document:import('../../../common_types.js').CommonAppDocument,
 *          common_mountdiv:string,
-*          default_data_value:string,
-*          default_value:string,
-*          options:[{value:string, text:string}],
-*          path:string,
-*          query:string,
-*          method:string,
-*          authorization_type:string,
-*          column_value:string,
-*          column_text:string,
+*          user_account_id:number,
+*          user_account_id_profile:number,
+*          detailchoice:number,
+*          function_show_common_dialogue:function,
+*          function_click:function,
 *          function_FFB:function}} props 
-* @returns {Promise.<{ props:{function_post:function}, 
+* @returns {Promise.<{ props:{function_post:function|null}, 
 *                      data:null, 
 *                      template:string}>}
 */
 const component = async props => {
 
+    if (!props.user_account_id)
+        props.function_show_common_dialogue('LOGIN');
    const post_component = async () =>{
-       // add first static option first if any then add fetched options
-       const ffb_options = props.path?await props.function_FFB(props.path, props.query, props.method, props.authorization_type, null)
-                                   .then((/**@type{string}*/result)=>JSON.parse(result).rows):[];
-       /**@type{[{value:string, text:string}]|[]} */
-       const options = props.options?props.options.concat(ffb_options):ffb_options;
-       props.common_document.querySelector(`#${props.common_mountdiv}`).classList.add('common_select');
-       props.common_document.querySelector(`#${props.common_mountdiv}`).innerHTML = render_template({  spinner:'',
-                                                                                                       default_data_value:props.default_data_value,
-                                                                                                       default_value:props.default_value,
-                                                                                                       options:options,
-                                                                                                       column_value:props.column_value,
-                                                                                                       column_text:props.column_text
-                                                                                                   });
+        let path;
+        switch (props.detailchoice){
+            case 1:
+            case 2:
+            case 3:
+            case 4:{
+                /*detailchoice 1,2,3, 4: user_account*/
+                path = '/server-db/user_account-profile-detail';
+                break;
+            }
+            case 5:{
+                /* detailchoice 5, apps, returns same columns*/
+                path = '/server-db/user_account_app-apps';
+                break;
+            }
+            case 6:
+            case 7:{
+                /*detailchoice 6, 7: app specific */
+                path = '/server-db/user_account_app_data_post-profile-detail';
+                break;
+            }    
+        }
+       const list = await props.function_FFB(`${path}/${props.user_account_id_profile}`, `detailchoice=${props.detailchoice}`, 'GET', 'APP_ACCESS', null)
+                                   .then((/**@type{string}*/result)=>JSON.parse(result));
+
+       props.common_document.querySelector(`#${props.common_mountdiv}`).innerHTML = render_template({   spinner:'',
+                                                                                                        user_account_id:props.user_account_id,
+                                                                                                        user_account_id_profile:props.user_account_id_profile,
+                                                                                                        detailchoice:props.detailchoice,
+                                                                                                        list:list
+                                                                                                    });
+        props.common_document.querySelector('#common_profile_detail_list')['data-function'] = props.function_click;
   };
   /**
-   * @param {{ spinner:string,
-   *           default_data_value:string,
-   *           default_value:string,
-   *           options:[{value:string, text:string}]|[],
-   *           column_value:string,
-   *           column_text:string}} template_props
+   * @param {{  spinner:string,
+   *            user_account_id:number,
+   *            user_account_id_profile:number,
+   *            detailchoice:number,
+   *            list:[{ id:number|null, 
+   *                    APP_ID:number, 
+   *                    PROTOCOL:string, 
+   *                    SUBDOMAIN:string, 
+   *                    HOST:string, 
+   *                    PORT:string,
+   *                    LOGO:string,
+   *                    NAME:string,
+   *                    date_created:string,
+   *                    avatar:string,
+   *                    provider_image:string,
+   *                    username:string}]|[]}} template_props
    */
   const render_template = template_props =>{
-      return template({    spinner:template_props.spinner,
-                           default_data_value:template_props.default_data_value ?? '',
-                           default_value:template_props.default_value ?? '',
-                           options:template_props.options,
-                           column_value:template_props.column_value,
-                           column_text:template_props.column_text
+      return template({ spinner:template_props.spinner,
+                        user_account_id:template_props.user_account_id,
+                        user_account_id_profile:template_props.user_account_id_profile,
+                        detailchoice:template_props.detailchoice,
+                        list:template_props.list
       });
   };
   return {
-      props:  {function_post:post_component},
+      props:  {function_post:props.user_account_id?post_component:null},
       data:   null,
-      template: render_template({  spinner:'css_spinner',
-                                   default_data_value:props.default_data_value,
-                                   default_value:props.default_value,
-                                   options:[],
-                                   column_value:props.column_value,
-                                   column_text:props.column_text
+      template: render_template({   spinner:'css_spinner',
+                                    user_account_id:props.user_account_id,
+                                    user_account_id_profile:props.user_account_id_profile,
+                                    detailchoice:props.detailchoice,
+                                    list:[]
       })
   };
 };
