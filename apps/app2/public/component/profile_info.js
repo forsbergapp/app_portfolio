72320879ed63cbd2fbd36175dd34f4660bf1e325
fapwork <fapwork@proton.me>
2024-09-26 17:07:47 +0200

    APB-752 refactors profile_info.js and converts select to div, removes nastaliq css for timetable month header, adjusts css for common select so long text is truncated

--- apps/app2/public/component/profile_info.js
+++ apps/app2/public/component/profile_info.js
@@ -1,53 +1,151 @@
 /**
  * @module apps/app2/component/profile_info
  */
-
-const template = () => `<div id='profile_info_user_settings'>
-                            <div id='profile_main_btn_user_settings' class='common_link common_icon'></div>
-                        </div>
-                        <div id='profile_info_user_setting_likes'>
-                            <div id='profile_main_btn_user_setting_likes'>
-                                <div id='profile_main_btn_user_setting_likes_user_setting' class='common_link common_icon'></div>
-                            </div>
-                            <div id='profile_info_user_setting_likes_count'></div>
-                        </div>
-                        <div id='profile_info_user_setting_liked'>
-                            <div id='profile_main_btn_user_setting_liked'>
-                                <div id='profile_main_btn_user_setting_liked_user_setting' class='common_link common_icon'></div>
-                            </div>
-                            <div id='profile_info_user_setting_liked_count'></div>
-                        </div>
-                        <div id='profile_user_settings_row'>
-                            <select id='profile_select_user_settings'>
-                            </select>
-                            <div id='profile_user_settings_detail'>
-                                <div id='profile_user_settings_day' class='common_icon'></div>
-                                <div id='profile_user_settings_month' class='common_icon'></div>
-                                <div id='profile_user_settings_year' class='common_icon'></div>
-                                <div id='profile_user_settings_like'>
-                                    <div class='common_icon common_unlike common_link'></div>
-                                    <div class='common_icon common_like common_link'></div>
+/**
+ * @param {{spinner:string}} props
+ */
+const template = props => ` ${props.spinner==''?
+                                `<div id='profile_info_user_settings'>
+                                    <div id='profile_main_btn_user_settings' class='common_link common_icon'></div>
                                 </div>
-                                <div id='profile_user_settings_info_likes' class='common_icon'></div>
-                                <div id='profile_user_settings_info_likes_count'></div>
-                                <div id='profile_user_settings_info_views' class='common_icon'></div>
-                                <div id='profile_user_settings_info_views_count'></div>
-                            </div>
-                        </div>`;
+                                <div id='profile_info_user_setting_likes'>
+                                    <div id='profile_main_btn_user_setting_likes'>
+                                        <div id='profile_main_btn_user_setting_likes_user_setting' class='common_link common_icon'></div>
+                                    </div>
+                                    <div id='profile_info_user_setting_likes_count'></div>
+                                </div>
+                                <div id='profile_info_user_setting_liked'>
+                                    <div id='profile_main_btn_user_setting_liked'>
+                                        <div id='profile_main_btn_user_setting_liked_user_setting' class='common_link common_icon'></div>
+                                    </div>
+                                    <div id='profile_info_user_setting_liked_count'></div>
+                                </div>
+                                <div id='profile_user_settings_row'>
+                                    <div id='profile_select_user_settings'></div>
+                                    <div id='profile_user_settings_detail'>
+                                        <div id='profile_user_settings_day' class='common_icon'></div>
+                                        <div id='profile_user_settings_month' class='common_icon'></div>
+                                        <div id='profile_user_settings_year' class='common_icon'></div>
+                                        <div id='profile_user_settings_like'>
+                                            <div class='common_icon common_unlike common_link'></div>
+                                            <div class='common_icon common_like common_link'></div>
+                                        </div>
+                                        <div id='profile_user_settings_info_likes' class='common_icon'></div>
+                                        <div id='profile_user_settings_info_likes_count'></div>
+                                        <div id='profile_user_settings_info_views' class='common_icon'></div>
+                                        <div id='profile_user_settings_info_views_count'></div>
+                                    </div>
+                                </div>`:
+                                `<div id='profile_info_user_settings' class='${props.spinner}'>
+                                 </div>`
+                            }
+                            `;
 /**
  * 
  * @param {{common_document:import('../../../common_types.js').CommonAppDocument,
- *          common_mountdiv:string}} props 
- * @returns {Promise.<{ props:{function_post:null}, 
- *                      data:null, 
+ *          common_mountdiv:string,
+ *          user_account_id:number|null,
+ *          profile_id:number,
+ *          function_ComponentRender:function,
+ *          function_FFB:function}} props 
+ * @returns {Promise.<{ props:{function_post:post_component}, 
+ *                      data:{  function_profile_user_setting_update:       function,
+ *                              function_profile_show_user_setting_detail:  function,
+ *                              function_profile_user_setting_stat:         function},
  *                      template:string}>}
  */
 const method = async props => {
-    props;
+    /**
+     * Profile show user setting detail
+     * @param {number} liked 
+     * @param {number} count_likes 
+     * @param {number} count_views 
+     * @returns {void}
+     */
+    const profile_show_user_setting_detail = (liked, count_likes, count_views) => {
+        
+        props.common_document.querySelector('#profile_user_settings_like').children[0].style.display = `${liked == 1?'none':'block'}`;
+        props.common_document.querySelector('#profile_user_settings_like').children[1].style.display = `${liked == 1?'block':'none'}`;
+
+        props.common_document.querySelector('#profile_user_settings_info_likes_count').innerHTML = count_likes;
+        props.common_document.querySelector('#profile_user_settings_info_views_count').innerHTML = count_views;
+    };
+
+    /**
+     * Profile user setting stat
+     * @param {number} id
+     * @returns {void}
+     */
+    const profile_user_setting_stat = id => {
+        props.function_FFB(`/server-db/user_account_app_data_post-profile-stat-like/${id}`, null, 'GET', 'APP_DATA', null)
+        .then((/**@type{string}*/result)=>{
+            props.common_document.querySelector('#profile_info_user_setting_likes_count').innerHTML = JSON.parse(result)[0].count_user_post_likes;
+            props.common_document.querySelector('#profile_info_user_setting_liked_count').innerHTML = JSON.parse(result)[0].count_user_post_liked;
+        })
+        .catch(()=>null);
+    };
+    /**
+     * @param {number} profile_id
+     * @param {number|null} sid
+     */
+    const profile_user_setting_update = async (profile_id, sid=null) =>{
+        /**@type{{value:string, text:string}[]}*/
+        const user_settings = await props.function_FFB(  `/server-db/user_account_app_data_post-profile/${profile_id}`, 
+                                                        `id_current_user=${props.user_account_id??''}`, 
+                                                        'GET', 
+                                                        'APP_DATA', null)
+                                    .then((/**@type{string}*/result)=>
+                                            JSON.parse(result)
+                                            .map((/**@type{{id:number, 
+                                                            user_account_app_user_account_id:number, 
+                                                            liked:number, 
+                                                            count_likes:number, 
+                                                            count_views:number,
+                                                            design_paper_size:string, 
+                                                            description:string}}}*/setting)=>{return {  value:JSON.stringify({   
+                                                                                                                sid:setting.id, 
+                                                                                                                user_account_id:setting.user_account_app_user_account_id, 
+                                                                                                                liked:setting.liked,
+                                                                                                                count_likes:setting.count_likes,
+                                                                                                                count_views:setting.count_views,
+                                                                                                                paper_size:setting.design_paper_size,
+                                                                                                                description:setting.description}), 
+                                                                                                        text:setting.description};}));
+
+        props.common_document.querySelector(`#${props.common_mountdiv}`).innerHTML = template({spinner:''});
+        //show current setting or first setting if showing first time
+        await props.function_ComponentRender('profile_select_user_settings', 
+            {
+              default_data_value:   sid?user_settings.filter(setting=>JSON.parse(setting.value).sid == sid)[0].value:user_settings[0].value,
+              default_value:        sid?user_settings.filter(setting=>JSON.parse(setting.value).sid == sid)[0].text:user_settings[0].text,
+              options: user_settings,
+              path:null,
+              query:null,
+              method:null,
+              authorization_type:null,
+              column_value:'value',
+              column_text:'text',
+              function_FFB:props.function_FFB
+            }, '/common/component/select.js');
+        
+        const profile_select_user_settings = props.common_document.querySelector('#profile_select_user_settings .common_select_dropdown_value').getAttribute('data-value');
+        profile_show_user_setting_detail(   JSON.parse(profile_select_user_settings).liked,
+                                            JSON.parse(profile_select_user_settings).count_likes,
+                                            JSON.parse(profile_select_user_settings).count_views);
+        profile_user_setting_stat(profile_id);
+    };
+
+    const post_component = async ()=>{
+        await profile_user_setting_update(props.profile_id);
+    };
+
     return {
-        props:  {function_post:null},
-        data:   null,
-        template: template()
+        props:  {function_post:post_component},
+        data:   {function_profile_user_setting_update:profile_user_setting_update,
+                 function_profile_show_user_setting_detail:profile_show_user_setting_detail,
+                 function_profile_user_setting_stat:profile_user_setting_stat
+        },
+        template: template({spinner:'css_spinner'})
     };
 };
 export default method;
