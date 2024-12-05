335f6555796b7f0c4b4f59bae64180ffe7f68e44
fapwork <fapwork@proton.me>
2024-12-05 23:30:54 +0100

    APB-401 updates documentation, updates SSL installation instructions and terraform server_setup.sh script adding cd before npm install and installing ufw, adds pm2 documentation, info about HOST parameter first time and how to see progress on server after terraform apply

--- server/install/scripts/server_setup.sh
+++ server/install/scripts/server_setup.sh
@@ -9,10 +9,13 @@ sudo curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install nodejs -y
 sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/node
 sudo snap install --classic certbot
-cd /home/ubuntu
+sudo apt-get install ufw -y
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw --force enable
 sudo -i -u ubuntu git clone https://github.com/forsbergapp/app_portfolio.git app_portfolio
-cd /home/ubuntu/app_portfolio
-sudo -i -u ubuntu npm install --omit=dev
+sudo -i -u ubuntu bash -c "cd /home/ubuntu/app_portfolio && npm install --omit=dev"
 sudo npm install -g pm2
 sudo -i -u ubuntu pm2 start /home/ubuntu/app_portfolio/server/init.js --cwd /home/ubuntu/app_portfolio --name app_portfolio -o "/dev/null" -e "/dev/null" --watch --ignore-watch=".git .vscode .well-known data node_modules microservice .gitignore .eslintignore .eslintrc.js jsdoc.json README.md tsconfig.json"
 sudo -i -u ubuntu pm2 start /home/ubuntu/app_portfolio/microservice/batch/server.js --cwd /home/ubuntu/app_portfolio --name batch --watch="microservice" --ignore-watch="microservice/geolocation microservice/mail microservice/worldcities" --watch-delay 10 --no-autorestart --silent
