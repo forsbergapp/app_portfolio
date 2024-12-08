4c805bb32e502ece49ce44d5af470aabbffe2179
fapwork <fapwork@proton.me>
2024-12-09 00:06:50 +0100

    APB-1022 adds unique control for IAM_APP_TOKEN.token in fileModelAppToken.js, updates IAM_APP_TOKEN and IAM_USER_LOGIN in ER model with UK for token and NOT NULL columns, adds modified to IAM_USER_LOGIN, adds modified in fileModelIamUserlogin.js and update() and checks that more than 1 key will be updated

--- server/db/fileModelIamAppToken.js
+++ server/db/fileModelIamAppToken.js
@@ -28,27 +28,32 @@ const post = async (app_id, data) => {
         data.res != null &&
         data.token != null &&
         data.ip != null){
-        /**@type{server_db_file_iam_app_token} */
-        const data_new = {};
-        //required
-        data_new.app_id = data.app_id;
-        data_new.res = data.res;
-        data_new.token = data.token;
-        data_new.ip = data.ip;
-        //optional
-        if (data.ua!=null)
-            data_new.ua = data.ua;
-        if (data.long!=null)
-            data_new.long = data.long;
-        if (data.lat!=null)
-            data_new.lat = data.lat;
-        data_new.created = new Date().toISOString();
-        return fileFsDBLogPost(app_id, 'IAM_APP_TOKEN',data_new, '').then((result)=>{
-            if (result.affectedRows>0)
-                return result;
-            else
-                throw fileCommonRecordNotFound(null);
-        });
+        //security check that token is not used already
+        if (await fileFsDBLogGet(app_id, 'IAM_APP_TOKEN', null, null,'').then(result=>result.filter((/**@type{server_db_file_iam_app_token} */row)=>row.token==data.token).length==0)){
+            /**@type{server_db_file_iam_app_token} */
+            const data_new = {};
+            //required
+            data_new.app_id = data.app_id;
+            data_new.res = data.res;
+            data_new.token = data.token;
+            data_new.ip = data.ip;
+            //optional
+            if (data.ua!=null)
+                data_new.ua = data.ua;
+            if (data.long!=null)
+                data_new.long = data.long;
+            if (data.lat!=null)
+                data_new.lat = data.lat;
+            data_new.created = new Date().toISOString();
+            return fileFsDBLogPost(app_id, 'IAM_APP_TOKEN',data_new, '').then((result)=>{
+                if (result.affectedRows>0)
+                    return result;
+                else
+                    throw fileCommonRecordNotFound(null);
+            });
+        }
+        else
+            throw '⛔';    
     }
     else
         throw '⛔';
