7e891bd4e4a5c582631b96c19d96161d76603613
fapwork <fapwork@proton.me>
2024-12-18 18:30:11 +0100

    APB-1030 moves markdown_image event in app.js in doc app to common_markdown_iamge event in common.js, replaces commonMiscMarkdownParse with component common_markdown.js, updates common_markdown_code_inline to use lighter background and adds border, adds common_dialogue_button class to menu_close and removes default black button bolors, removes common_window_info_btn_close event in app.js, updates JSDoc documentation

--- apps/common/public/js/common.js
+++ apps/common/public/js/common.js
@@ -547,109 +547,7 @@ const commonMiscListKeyEvent = (event, module, event_function=null) => {
         }            
     }
 };
-/**
- * Converts given markdown file and mounts to given div id to supported div tags without any semantic HTML
- * Converts only sections, headings, code and images
- * Images should be clickable and displayed in windows info using event delegation
- * headings:
- * must start at first position on a row
- *                                  div class       comment
- * #                                title_h1
- * ##                               title_h2
- * ###                              title_h3
- * code:
- * code block must start as first position on a row, no support for inline code
- * ```                              code            code block
- * images:
- * [![text](small img)](full size img)  markdown_image  alt text should be used here as text below image
- *                                                      hover text is not supported
- * not supported:
- * ![alt text ](img "hover text")
- *         
- * @param {string} markdown
- * @returns {string}
- */
-const commonMiscMarkdownParse = markdown =>{
-    //remove all '\r' in '\r\n'
-    markdown = markdown.replaceAll('\r\n','\n');
-    //convert headings to section
-    let current_section = -1;
-    let old_section = -1;
-    markdown = markdown.split('\n').map((row, /**@type{number}*/index)=>{
-        let div = '';
-        if (row.indexOf('#')==0){
-            current_section = row.split('#').length;
-            if (index==0)
-                div = '<div class=\'common_markdown_section\'>' + '\n' + row;
-            else
-                switch (true){
-                    case current_section == old_section:{
-                        div =   '</div>' + '\n' +
-                                '<div class=\'common_markdown_section\'>' + '\n' + row;
-                        break;
-                    }
-                    case current_section < old_section:{
-                        div +='</div>' + '\n';
-                        for (let i=1;i<=(old_section - current_section);i++){
-                            div +='</div>' + '\n';
-                        }
-                        div +=  '<div class=\'common_markdown_section\'>' + '\n' + row;
-                        break;
-                    }
-                    case current_section > old_section:{
-                        div =   '\n' +
-                                '<div class=\'common_markdown_section\'>' + '\n' + row;
-                        break;
-                    }
-                }   
-            old_section = row.split('#').length;
-            return div;
-        }
-        else
-            return row;
-    }).join('\n') + '</div>';
-                                        
-    //convert headings #, ## and ##
-    //correct syntax 
-    //#[1 space character] []text,   ex # heading 1
-    //##[1 space character] []text,  ex ## heading 2
-    //###[1 space character] []text, ex ### heading 3
-    //# must be first character in the row or it is not part of markdown parsing
-    markdown = markdown.split('\n').map(row=>row.indexOf('#####')==0?`<div class='common_markdown_title_h5'>${row.replace('#####','')}</div>`:row).join('\n');
-    markdown = markdown.split('\n').map(row=>row.indexOf('####')==0?`<div class='common_markdown_title_h4'>${row.replace('####','')}</div>`:row).join('\n');
-    markdown = markdown.split('\n').map(row=>row.indexOf('###')==0?`<div class='common_markdown_title_h3'>${row.replace('###','')}</div>`:row).join('\n');
-    markdown = markdown.split('\n').map(row=>row.indexOf('##')==0?`<div class='common_markdown_title_h2'>${row.replace('##','')}</div>`:row).join('\n');
-    markdown = markdown.split('\n').map(row=>row.indexOf('#')==0?`<div class='common_markdown_title_h1'>${row.replace('#','')}</div>`:row).join('\n');
-    //convert code
-    //regexp for code blocks
-    const regexp_code = /```([\s\S]*?)```/g;
-    let match_code;
-    while ((match_code = regexp_code.exec(markdown)) !==null){
-        markdown = markdown.replace(match_code[0], `<div class='common_markdown_code'>${match_code[1]}</div>`);
-    }
-    //convert code inline
-    //regexp for code blocks
-    const regexp_code_inline = /`([\s\S]*?)`/g;
-    let match_code_inline;
-    while ((match_code_inline = regexp_code_inline.exec(markdown)) !==null){
-        markdown = markdown.replace(match_code_inline[0], `<div class='common_markdown_code_inline'>${match_code_inline[1]}</div>`);
-    }
-    //convert image tags
-    //regexp for [![text](small img)](full size img)
-    const regexp = /\[!\[([^)]+)\]\(([^)]+)\)\]\(([^)]+)\)/g;
-    let match;
-    while ((match = regexp.exec(markdown)) !==null){
-        markdown = markdown.replace(match[0], 
-                                    `<div 	class='common_markdown_image' 
-                                            style='background-image:url("${match[2]}")' 
-                                            data-url='${match[3]}'></div><div class='common_markdown_image_text'>${match[1]}</div>`);
-    }
-    return `<div class='common_markdown_header' style='background-image:url("${COMMON_GLOBAL.app_logo}")'>${COMMON_DOCUMENT.title}</div>
-                <div class='common_markdown_article'>
-                    ${markdown}
-                </div>
-            <div class='common_markdown_footer'>${COMMON_GLOBAL.app_copyright}</div>`;
-};
+
 /**
  * 
  * Check if mobile
@@ -3146,6 +3044,22 @@ const commonEvent = async (event_type,event=null) =>{
                         case 'common_lov_list':{
                             COMMON_DOCUMENT.querySelector('#common_lov_list')['data-function'](event);
                             break;
+                        }
+                        //markdown document tags
+                        case event.target.classList.contains('common_markdown_image')?event_target_id:'':{
+                            if (event.target.getAttribute('data-url'))
+                                commonComponentRender({
+                                    mountDiv:   'common_window_info',
+                                    data:       {
+                                                //show IMAGE type 0 
+                                                info:0,
+                                                url:event.target.getAttribute('data-url'),
+                                                content_type:null, 
+                                                iframe_content:null
+                                                },
+                                    methods:    {commonWindowSetTimeout:commonWindowSetTimeout},
+                                    path:       '/common/component/common_window_info.js'});
+                            break;
                         }        
                         default:{
                             if (event.target.classList.contains('leaflet-control-zoom-in') || event.target.parentNode.classList.contains('leaflet-control-zoom-in')){
@@ -3733,7 +3647,6 @@ export{/* GLOBALS*/
        commonMiscInputControl,
        commonMiscImportmap,
        commonMiscListKeyEvent,
-       commonMiscMarkdownParse,
        commonMiscMobile,
        commonMiscPreferencesUpdateBodyClassFromPreferences,
        commonMiscPreferencesPostMount,
