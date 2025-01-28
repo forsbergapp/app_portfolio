a2841c1cd5cdc204ceb0ee7ece1bf6ca89fbecf2
fapwork <fapwork@proton.me>
2025-01-28 13:27:23 +0100

    APB-987 removes jsdoc.json reference in server_setup.sh

--- server/install/scripts/server_setup.sh
+++ server/install/scripts/server_setup.sh
@@ -17,7 +17,7 @@ sudo ufw --force enable
 sudo -i -u ubuntu git clone https://github.com/forsbergapp/app_portfolio.git app_portfolio
 sudo -i -u ubuntu bash -c "cd /home/ubuntu/app_portfolio && npm install --omit=dev"
 sudo npm install -g pm2
-sudo -i -u ubuntu pm2 start /home/ubuntu/app_portfolio/server/init.js --cwd /home/ubuntu/app_portfolio --name app_portfolio -o "/dev/null" -e "/dev/null" --watch --ignore-watch=".git .vscode .well-known data node_modules microservice .gitignore .eslintignore .eslintrc.js jsdoc.json README.md tsconfig.json"
+sudo -i -u ubuntu pm2 start /home/ubuntu/app_portfolio/server/init.js --cwd /home/ubuntu/app_portfolio --name app_portfolio -o "/dev/null" -e "/dev/null" --watch --ignore-watch=".git .vscode .well-known data node_modules microservice .gitignore .eslintignore .eslintrc.js README.md tsconfig.json"
 sudo -i -u ubuntu pm2 start /home/ubuntu/app_portfolio/microservice/batch/server.js --cwd /home/ubuntu/app_portfolio --name batch --watch="microservice" --ignore-watch="microservice/geolocation microservice/mail" --watch-delay 10 --no-autorestart --silent
 sudo -i -u ubuntu pm2 start /home/ubuntu/app_portfolio/microservice/mail/server.js --cwd /home/ubuntu/app_portfolio --name mail --watch="microservice" --ignore-watch="microservice/batch microservice/geolocation" --no-autorestart --silent
 sudo -i -u ubuntu pm2 start /home/ubuntu/app_portfolio/microservice/geolocation/server.js --cwd /home/ubuntu/app_portfolio --name geolocation --watch="microservice" --ignore-watch="microservice/batch microservice/mail"  --no-autorestart --silent
