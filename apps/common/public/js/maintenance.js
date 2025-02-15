ad15768b6ba5b35bc3e742b9ac5551d303b5211d
fapwork <fapwork@proton.me>
2025-02-15 02:50:11 +0100

    APB-945 refactors dbInstall() separating data scripts with datamodel scripts so apps and users can be installed before data scripts using executeDatabase() and throws error if result key not returned from dbCommonExecute() , commonAppStart() checks that all apps has db credentials, maintenance.js reads COMMON_GLOBAL.iam_user_username, adds server_app_id parameter to db info menu rest api, dbStart() reads app_secret db credentials from each app, set dba parameter for dbModelApp.post and dbModelApp.deleteRecord, dbUserCreate()uses Promise.all and corrrects return type

--- apps/common/public/js/maintenance.js
+++ apps/common/public/js/maintenance.js
@@ -38,7 +38,7 @@ const appCommonInit = parameters => {
     common.COMMON_GLOBAL.rest_resource_bff = decoded_parameters.rest_resource_bff;
     common.COMMON_GLOBAL.user_account_id = null;
     common.COMMON_GLOBAL.iam_user_id = null;
-    common.COMMON_GLOBAL.iam_user_name = null;
+    common.COMMON_GLOBAL.iam_user_username = null;
     common.COMMON_GLOBAL.token_dt = decoded_parameters.app_idtoken;
     common.commonSocketConnectOnline();    
     common.commonSocketMaintenanceShow(null,1);
