ecbbe306307ee0c374ea1ee031ff027ec9fd1219
fapwork <fapwork@proton.me>
2025-02-16 18:09:19 +0100

    APB-945 removes "/bff/admin/v1/server-db/app_data_stat-log-stat", sql APP_DATA_STAT_SELECT_UNIQUE_VISITORS and dbModelAppDataStat.getStatUniqueVisitor not used

--- server/db/dbModelAppDataStat.js
+++ server/db/dbModelAppDataStat.js
@@ -9,9 +9,6 @@
 /**@type{import('./dbSql.js')} */
 const dbSql = await import(`file://${process.cwd()}/server/db/dbSql.js`);
 
-/**@type{import('./fileModelConfig.js')} */
-const fileModelConfig = await import(`file://${process.cwd()}/server/db/fileModelConfig.js`);
-
 /**@type{import('../server.js')} */
 const {serverUtilNumberValue} = await import(`file://${process.cwd()}/server/server.js`);
 
@@ -112,82 +109,6 @@ const getLog = parameters =>
                 return result;
         });
 /**
- * @name getStatUniqueVisitor
- * @description Get stat unique visitors
- * @function
- * @memberof ROUTE_REST_API
- * @param {{app_id:number,
- *          data:{  select_app_id?:string|null,
- *                  year?:string|null,
- *                  month?:string|null}}} parameters
- * @returns {Promise.<server_server_response & {result?:server_db_sql_result_app_data_stat_getStatUniqueVisitor[] }>}
- */
-const getStatUniqueVisitor = parameters =>{
-    /**
-     * Convert to array with object
-     * @param {*} log
-     * @param {boolean} log_amount
-     * @returns {*}
-     */
-     const to_object = (log, log_amount) => {
-        /**@type{*} */
-        const log_array_with_object = [];
-        log.forEach((/**@type{*}*/log_row)=>
-                {
-                    const log_split = log_row.split(';');
-                    log_array_with_object.push({chart:Number(log_split[0]),
-                                                app_id:log_split[1]=='null'?null:Number(log_split[1]),
-                                                year:Number(log_split[2]),
-                                                month:Number(log_split[3]),
-                                                day:log_split[4]=='null'?null:Number(log_split[4]),
-                                                amount:log_amount?Number(log_split[5]):null});
-                }
-        );
-        return log_array_with_object;
-    };
-    return new Promise((resolve)=>{
-        import(`file://${process.cwd()}/server/db/common.js`).then((/**@type{import('./common.js')} */{dbCommonExecute})=>
-            dbCommonExecute(parameters.app_id, 
-                            dbSql.APP_DATA_STAT_SELECT_UNIQUE_VISITORS, 
-                            {   app_id_log: serverUtilNumberValue(parameters.data.select_app_id),
-                                year_log: serverUtilNumberValue(parameters.data.year),
-                                month_log: serverUtilNumberValue(parameters.data.month),
-                                app_data_entity_resource_id: 0,
-                                app_data_entity_resource_app_data_entity_app_id: serverUtilNumberValue(fileModelConfig.get('CONFIG_SERVER','SERVER', 'APP_COMMON_APP_ID')),
-                                app_data_entity_resource_app_data_entity_id : 0}))
-            .then(result_logs =>{
-                if (result_logs.result)
-                    if (result_logs.result.length>0){
-                        //use SQL group by and count() in javascript
-                        //save unique server_remote_addr in a set
-                        const log_unique_ip = new Set();
-                        for (const log of result_logs.result){
-                            log_unique_ip.add( `${log.chart};${log.app_id};${log.year};${log.month};${log.day};${JSON.parse(log.json_data).server_remote_addr}`);
-                        }
-                        //convert to array with objects
-                        const log_unique = to_object(log_unique_ip, false);
-                        //save amount in unique server_remote_addr list in a set
-                        const log_unique_with_amount = new Set();
-                        log_unique.forEach((/**@type{*}*/log)=>{
-                            log_unique_with_amount.add( `${log.chart};${log.app_id};${log.year};${log.month};${log.day};${log_unique.filter((/**@type{*}*/log_amount)=>   
-                                                                                                                                log_amount.chart==log.chart && 
-                                                                                                                                log_amount.app_id == log.app_id &&
-                                                                                                                                log_amount.year == log.year &&
-                                                                                                                                log_amount.month == log.month &&
-                                                                                                                                log_amount.day == log.day).length}`);
-                        });
-                        //convert to array with objects
-                        const result_getStatUniqueVisitorAdmin = to_object(log_unique_with_amount, true);
-                        result_logs.result = result_getStatUniqueVisitorAdmin;
-                    }
-                    else{
-                        result_logs.result = [];
-                    }
-                resolve(result_logs);
-            });
-    });
-};
-/**
  * @name post
  * @description Create stat record
  * @function
@@ -210,4 +131,4 @@ const post = async (app_id, data) =>
                             app_data_entity_resource_app_data_entity_id:        data.app_data_entity_resource_app_data_entity_id
                             }));
 
-export{get, getLog, getStatUniqueVisitor, post};
+export{get, getLog, post};
