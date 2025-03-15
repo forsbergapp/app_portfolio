d5c0732a52dc6754f13c2b8f0537ab94a2007527
fapwork <fapwork@proton.me>
2025-03-15 19:45:56 +0100

    AP-4 adds app otp with app id 9 including new app records and adds new app9_logo.svg, replaces regexp in securityOTPKeyValidate with Array.from logic, securityTOTPGenerate returns totp_value and expire in milliseconds, adds server function otp.js, updates default app comments in app.js and removes sw.js

---
+++ apps/app9/src/functions/totp.js
@@ -0,0 +1,31 @@
+/**
+ * @module apps/app9/src/functions/totp
+ */
+/**
+ * @import {server_server_response} from '../../../../server/types.js'
+ */
+/**
+ * @name getTOTP
+ * @description Get TOTP
+ * @function
+ * @param {{app_id:number,
+*          data:{  otp_key:string},
+*          user_agent:string,
+*          ip:string,
+*          host:string,
+*          idToken:string,
+*          authorization:string,
+*          locale:string}} parameters
+* @returns {Promise.<server_server_response & {result?:{totp_value?:string, expire?:number}|null[]}>}
+*/
+const getTOTP = async parameters =>{
+   /**@type{import('../../../../server/security.js')} */
+   const Security = await import(`file://${process.cwd()}/server/security.js`);
+
+   const result_totp = await Security.securityTOTPGenerate(parameters.data.otp_key);
+   return {result: [{totp_value:result_totp?.totp_value,
+                    expire:result_totp?.expire
+                    }],
+            type:'JSON'};
+};
+export default getTOTP;
