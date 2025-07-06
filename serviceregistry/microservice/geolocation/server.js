cd6c3b9ec0aaad789a7100459cf1efcdf10e2245
fapwork <fapwork@proton.me>
2025-07-07 00:40:23 +0200

    AP-53 adds /server/install/default/microservice/common.js for microservices with api to main server and to request url, copies microservice common.js in postConfigDefault() and replaces public_key and private key with uuid and secret in installation.js and microservice configuration and ServiceRegistry.json, updates BATCH and GEOLOCATION microservice to use new common.js file

--- serviceregistry/microservice/geolocation/server.js
+++ serviceregistry/microservice/geolocation/server.js
@@ -4,7 +4,7 @@
  */
 
 /**
- * @import {config, request, response, geolocation_data} from './types.js'
+ * @import {common, config, request, response, geolocation_data} from './types.js'
  */
 
 /**
@@ -30,144 +30,23 @@ const serverProcess = new ClassServerProcess();
  */
 const serverStart = async () =>{
     const service = await import('./service.js');
-    const fs = await import('node:fs');
-    const Crypto = await import('node:crypto');
 
-    /**
-     * @name serverReturn
-     * @description Return result
-     * @function
-     * @param {number} code 
-     * @param {string|null} error 
-     * @param {*} result 
-     * @param {response} res
-     * @returns {void}
-     */
-    const serverReturn = (code, error, result, res)=>{
-        res.statusCode = code;
-        if (error){
-            log('MICROSERVICE_ERROR', error);
-            //ISO20022 error format
-            const message = JSON.stringify({error:{
-                                            http:code, 
-                                            code:'MICROSERVICE',
-                                            text:error, 
-                                            developer_text:null, 
-                                            more_info:null}});
-            res.write(message, 'utf8');
-        }
-        else{
-            res.setHeader('Content-Type',  'application/json; charset=utf-8');
-            res.write(JSON.stringify(result), 'utf8');
-        }
-        res.end();
-    };
-
-    /**
-     * @description log
-     * @param {'MICROSERVICE_LOG'|'MICROSERVICE_ERROR'} type
-     * @param {string} message
-     */
-    const log = (type, message) =>{
-        service.requestUrl({url:Config.message_queue_url, 
-            method:Config.message_queue_method, 
-            body:{data:Buffer.from(
-                            JSON.stringify({IAM_service:'GEOLOCATION', 
-                                            type: type, 
-                                            message:message})).toString('base64')
-                },
-            authorization:'Bearer ' + jwt_data.token,
-            language:'en'})
-        .catch(error=>
-            console.log(`MICROSERVICE ERROR ${error}`)
-        );
-    };
     
-    /**
-     * @param 
-     * @param {{id:number|null,
-     *          message:{}}} message
-     * @returns {string}
-     */
-    const encryptMessage = message => 
-        Buffer.from(JSON.stringify(
-            {
-            id:message.id,
-            message:Crypto.publicEncrypt(  Config.public_key,
-                    Buffer.from(JSON.stringify(message.message))).toString('base64')
-            })).toString('base64');
-            
-    /**
-     * @description Authenticates app using IAM and sends query encoded with base64
-     * @param {number} app_id
-     * @param {string} authorization
-     * @returns {Promise.<boolean>}
-     */
-    const iamAuthenticateApp = async (app_id, authorization) =>{
-        const [auth_client_id, auth_client_secret] = Buffer.from((authorization.replace('Basic ','') || ''), 'base64').toString().split(':');
-        return await service.requestUrl({   url:Config.iam_auth_app_url, 
-                                            method:Config.iam_auth_app_method, 
-                                            body:{data:encryptMessage({ id:null, 
-                                                                        message: {
-                                                                                    app_id:app_id, 
-                                                                                    client_id:auth_client_id,
-                                                                                    client_secret:auth_client_secret
-                                                                                }})
-                                                },
-                                            authorization:'Bearer ' + jwt_data.token,
-                                            language:'en'})
-                    .then(result=>
-                        result.error?false:true
-                    )
-                    .catch(()=>
-                        false
-                    );
-    };
+    /**@type{common} */
+    const common = await import(serverProcess.cwd() + '/data/microservice/common.js');
     /**
      * @description Get config
      * @type{config} 
      */
-    const Config = JSON.parse(await fs.promises.readFile(serverProcess.cwd() + '/data/microservice/GEOLOCATION.json', 'utf8'));
-
-    const auth = async attempt =>{
-        const attempts = 10;
-        /**
-         * @type{{  token:string,
-         *           exp:number,
-         *           iat:number}}
-         */
-        const jwt_data = await service.requestUrl({url:Config.service_registry_auth_url, 
-                                method:Config.service_registry_auth_method, 
-                                body:{data:encryptMessage({id:null, message: {message:null}})
-                                    },
-                                language:'en'})
-                                .catch(()=>null);
-        if (jwt_data == null && ((attempt??1) <=attempts) )
-            await new Promise ((resolve)=>{setTimeout(()=>{auth((attempt??1) +1).then(()=>resolve(null));}, 5000);});
-        else
-            return jwt_data;
-    };
-    //wait 5 seconds first time
-    await new Promise ((resolve)=>{setTimeout(()=>resolve(null), 5000);});
-    const jwt_data = await auth();
-    if (jwt_data == null)
-        throw '⛔';
-
-    /**
-     * @type{{  server: import('node:http') ,
-     *          port:   number,
-     *          options:{key?:string, cert?:string}}}
-     */
-    const request =     {
-                            server  :   await import(`node:${Config.server_protocol}`),
-                            port	: Config.server_port,
-                            options : Config.server_protocol=='https'?{
-                                            key: await fs.promises.readFile(serverProcess.cwd() + Config.server_https_key, 'utf8'),
-                                            cert: await fs.promises.readFile(serverProcess.cwd() + Config.server_https_cert, 'utf8')
-                                        }:{}
-                        };
+    const Config = await common.commonConfig('GEOLOCATION');
     
-	request.server.createServer(request.options, (/**@type{request}*/req, /**@type{response}*/res) => {
+    const auth = await common.commonAuth({service_registry_auth_method:Config.service_registry_auth_method,
+                                    service_registry_auth_url:Config.service_registry_auth_url,
+                                    uuid:Config.uuid,
+                                    secret:Config.secret
+    });
+
+	Config.server.createServer(Config.options, (/**@type{request}*/req, /**@type{response}*/res) => {
 		res.setHeader('Access-Control-Allow-Methods', 'GET');
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		const URI_query = Buffer.from(req.url.substring(req.url.indexOf('?')), 'base64').toString('utf-8');
@@ -182,41 +61,138 @@ const serverStart = async () =>{
                                         null:
                                             Number(app_query.get('app_id')),
 						data:	    data};
-		iamAuthenticateApp(req.query.app_id, req.headers.authorization).then((/**@type{boolean}*/authenticate)=>{
+                        
+		common.commonIamAuthenticateApp({app_id:req.query.app_id,
+                                        token:auth.token,
+                                        iam_auth_app_url:Config.iam_auth_app_url,
+                                        iam_auth_app_method:Config.iam_auth_app_method,
+                                        uuid:Config.uuid,
+                                        secret:Config.secret,
+                                        'app-id':req.query.app_id,
+                                        'app-signature':''}).then((/**@type{boolean}*/authenticate)=>{
 			if (authenticate){
 				switch (true){
 					case URI_path == '/api/v1' && req.query.service == 'PLACE' && req.method =='GET':{
 						if(	(req.query.data.latitude !=null && req.query.data.latitude!='') ||
 							(req.query.data.longitude !=null && req.query.data.longitude!='')){
-							service.getPlace(Config, req.query.data.latitude, req.query.data.longitude, req.headers['accept-language'])
-							.then(result=>result?.length>0?serverReturn(200, null, JSON.parse(result), res):'')
-							.catch(error =>serverReturn(500, error, null, res));
+							service.getPlace(common, Config, req.query.data.latitude, req.query.data.longitude, req.headers['accept-language'])
+                            
+							.then(result=>result?.length>0?
+                                            common.commonServerReturn({
+                                                            service: 'GEOLOCATION',
+                                                            token: auth.token,
+                                                            uuid: Config.uuid,
+                                                            secret: Config.secret,
+                                                            message_queue_url: Config.message_queue_url,
+                                                            message_queue_method: Config.message_queue_method,
+                                                            code: 200,
+                                                            error: null,
+                                                            result: JSON.parse(result),
+                                                            res:res}):
+                                                '')
+							.catch(error =>common.commonServerReturn({
+                                service: 'GEOLOCATION',
+                                token: auth.token,
+                                uuid: Config.uuid,
+                                secret: Config.secret,
+                                message_queue_url: Config.message_queue_url,
+                                message_queue_method: Config.message_queue_method,
+                                code: 500,
+                                error: error,
+                                result: null,
+                                res:res}));
 						}
 						else
-							serverReturn(400, '⛔', null, res);
+                            common.commonServerReturn({
+                                service: 'GEOLOCATION',
+                                token: auth.token,
+                                uuid: Config.uuid,
+                                secret: Config.secret,
+                                message_queue_url: Config.message_queue_url,
+                                message_queue_method: Config.message_queue_method,
+                                code: 400,
+                                error: '⛔',
+                                result: null,
+                                res:res});
 						break;
 					}
                     case URI_path == '/api/v1' && req.query.service == 'IP' && req.method =='GET':{
 						//no v6 support
-						service.getIp(Config, req.query.data.ip.replace('::ffff:',''), req.headers['accept-language'])
-						.then(result=>serverReturn(200, null, JSON.parse(result), res))
-						.catch(error =>serverReturn(500, error, null, res));
+						service.getIp(common, Config, req.query.data.ip.replace('::ffff:',''), req.headers['accept-language'])
+						.then(result=>common.commonServerReturn({
+                            service: 'GEOLOCATION',
+                            token: auth.token,
+                            uuid: Config.uuid,
+                            secret: Config.secret,
+                            message_queue_url: Config.message_queue_url,
+                            message_queue_method: Config.message_queue_method,
+                            code: 200,
+                            error: null,
+                            result: JSON.parse(result),
+                            res:res}))
+						.catch(error =>common.commonServerReturn({
+                            service: 'GEOLOCATION',
+                            token: auth.token,
+                            uuid: Config.uuid,
+                            secret: Config.secret,
+                            message_queue_url: Config.message_queue_url,
+                            message_queue_method: Config.message_queue_method,
+                            code: 500,
+                            error: error,
+                            result: null,
+                            res:res}));
 						break;
 					}
 					default:{
-						serverReturn(401, '⛔', null, res);
+						common.commonServerReturn({
+                            service: 'GEOLOCATION',
+                            token: auth.token,
+                            uuid: Config.uuid,
+                            secret: Config.secret,
+                            message_queue_url: Config.message_queue_url,
+                            message_queue_method: Config.message_queue_method,
+                            code: 401,
+                            error: '⛔',
+                            result: null,
+                            res:res});
 					}
 				}
 			}
 			else
-				serverReturn(401, '⛔', null, res);
+                common.commonServerReturn({
+                    service: 'GEOLOCATION',
+                    token: auth.token,
+                    uuid: Config.uuid,
+                    secret: Config.secret,
+                    message_queue_url: Config.message_queue_url,
+                    message_queue_method: Config.message_queue_method,
+                    code: 401,
+                    error: '⛔',
+                    result: null,
+                    res:res});
 		});
-	}).listen(request.port, ()=>{
-        log('MICROSERVICE_LOG', `MICROSERVICE START PORT ${request.port}`);
+	}).listen(Config.port, ()=>{
+        common.commonLog({
+            type:'MICROSERVICE_LOG',
+            service:'GEOLOCATION',
+            message:`MICROSERVICE START PORT ${Config.port}`,
+            token:auth.token,
+            message_queue_url:Config.message_queue_url,
+            message_queue_method:Config.message_queue_method,
+            uuid:Config.uuid,
+            secret:Config.secret});
 	});
 
-	serverProcess.on('uncaughtException', (err) =>{
-        log('MICROSERVICE_ERROR', err);
+	serverProcess.on('uncaughtException', err =>{
+        common.commonLog({
+            type:'MICROSERVICE_ERROR',
+            service:'GEOLOCATION',
+            message:err,
+            token:auth.token,
+            message_queue_url:Config.message_queue_url,
+            message_queue_method:Config.message_queue_method,
+            uuid:Config.uuid,
+            secret:Config.secret});
 	});
 };
 serverStart();
