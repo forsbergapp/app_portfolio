e51aec3f6637fabfbb891f662e799a42ca95d488
fapwork <fapwork@proton.me>
2025-08-20 14:56:39 +0200

    AP-100 replaces removed common_link_common_css link with reading ui fonts css and root css in adopted stylesheet from document in print.js, adds comments for empty lines in fonts.css, adds COMMON_GLOBAL.app_fonts_loaded and commonMiscPrint() copies loaded fonts from COMMON_GLOBAL.app_fonts_loaded to iframe using FontFace() in common.js

--- apps/app4/public/component/print.js
+++ apps/app4/public/component/print.js
@@ -12,7 +12,7 @@
  * @description Template
  * @function
  * @param {{app_link_app_report_css:string|void,
- *          common_link_common_css:string|void,
+ *          app_fonts_css:string|void,
  *          html:string}} props
  * @returns {string}
  */
@@ -21,8 +21,10 @@ const template = props =>`  <!DOCTYPE html>
                            <head>
                                <meta charset='UTF-8'>
                                <title></title>
+                               <style>
+                                    ${props.app_fonts_css} 
+                               </style>
                                <link rel='stylesheet' type='text/css' href='${props.app_link_app_report_css}' />
-                               <link rel='stylesheet' type='text/css' href='${props.common_link_common_css}' />
                             </head>
                             <body id="printbody">
                                 ${props.html}
@@ -47,14 +49,27 @@ const template = props =>`  <!DOCTYPE html>
  *                      template:string}>}
  */
 const component = async props => {
-    
+    //get css with variables in :root
+    let cssRoot ='';
+    props.methods.COMMON_DOCUMENT.adoptedStyleSheets.forEach(sheet => { 
+        for (const i in sheet.cssRules) { 
+            //load css with base64 strings and variables in :root
+            if (sheet.cssRules[i].cssText?.indexOf('data:font/woff2;') ||
+                sheet.cssRules[i].cssText?.startsWith(':root')){ 
+                cssRoot += sheet.cssRules[i].cssText + '\n'; 
+            }
+        }
+    });
+
+    const templateRendered =  template({  
+                                app_link_app_report_css:props.methods.COMMON_DOCUMENT.querySelector('#app_link_app_report_css').attributes['href'].textContent,
+                                app_fonts_css:cssRoot,
+                                html: props.data.appHtml});
     return {
         lifecycle:  null,
         data:       null,
         methods:    null,
-        template:   template({  app_link_app_report_css:props.methods.COMMON_DOCUMENT.querySelector('#app_link_app_report_css').attributes['href'].textContent,
-                                common_link_common_css:props.methods.COMMON_DOCUMENT.querySelector('#common_link_common_css').attributes['href'].textContent,
-                                html: props.data.appHtml})
+        template:   templateRendered
     };
 };
 export default component;
