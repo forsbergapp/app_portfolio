5c27b040d16ed3b9271495b0fdd0f8941b42d5cb
fapwork <fapwork@proton.me>
2025-09-08 12:06:29 +0200

    AP-110 moves html to common_map_tile.js component, adds commonGeoJSONTile type, calls common_map_tile.js component from drawTiles() and changes to async, adds id to tile element in common_map.js

---
+++ apps/common/public/component/common_map_tile.js
@@ -0,0 +1,56 @@
+/**
+ * @module apps/common/component/common_map_tile
+ */
+
+/**
+ * @import {COMMON_DOCUMENT, CommonComponentLifecycle, commonGeoJSONTile}  from '../../../common_types.js'
+ */
+
+/**
+ * @name template
+ * @description Template
+ * @function
+ * @param {{id:string,
+ *          left:number,
+ *          top:number,
+ *          tileSize:number,
+ *          url:string}} props 
+ * @returns {string}
+ */
+const template = props => ` <div id='${props.id}' 
+                                 class='common_map_tile' 
+                                 style='left:${(props.left)}px;top:${(props.top)}px;width:${props.tileSize}px;height:${props.tileSize}px;background-image:url(${props.url})'>
+                            </div`;
+/**
+* @name component
+* @description Component
+* @function
+* @param {{data:        {
+*                       commonMountdiv:string,
+*                       geoJSON:commonGeoJSONTile
+*                       },
+*          methods:     {
+*                       COMMON_DOCUMENT:COMMON_DOCUMENT,
+*                       project:function,
+*                       }}} props
+* @returns {Promise.<{ lifecycle:CommonComponentLifecycle, 
+*                      data:   null,
+*                      methods:null,
+*                      template:string}>}
+*/
+const component = async props => {
+   
+    return {
+        lifecycle:  null,
+        data:       null,
+        methods:    null,
+        template:   template({
+                        id:     props.data.geoJSON.id,
+                        left:   props.data.geoJSON.properties.left,
+                        top:    props.data.geoJSON.properties.top,
+                        tileSize:  props.data.geoJSON.properties.tileSize,
+                        url:  props.data.geoJSON.properties.url
+                    })
+    };
+};
+export default component;
